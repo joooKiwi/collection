7e54338b1d3bbbea54e3b6f0209d33ad262bd219
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-07-23 00:48:45 -0400

    Removal of some unused types

--- javascript/src/general type.ts
+++ javascript/src/general type.ts
@@ -5,6 +5,8 @@
  * All the right is reserved to the author of this project.                   *
  ******************************************************************************/
 
+//TODO move in a separate project
+
 /**
  * A simple encapsulation type toward a specified type to be the value, <b>null</b>
  *
@@ -26,7 +28,3 @@ export type Nullable<T, > = | T | null | undefined
 
 /** A simple type for the {@link Number} as itself or included in a {@link String} directly */
 export type NumberOrNumberInString = | number | `${number}`
-/** A simple type for either a {@link String primitive string} or a {@link Number primitive number} */
-export type StringOrNumber = | string | number
-/** A simple type for either a {@link String primitive string} or a {@link Symbol primitive symbol} */
-export type StringOrSymbol = | string | symbol
