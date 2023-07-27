cfc72ff6de0fb8ec7070f78597ed22c2fe5c25a4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-07-27 16:37:14 -0400

    Change on the iterator tests to handle the changes

--- javascript/test/CollectionIterator.test.ts
+++ javascript/test/CollectionIterator.test.ts
@@ -5,6 +5,7 @@
  * All the right is reserved to the author of this project.                   *
  ******************************************************************************/
 
+import {CollectionConstants}       from "../src/CollectionConstants"
 import {EmptyCollectionHolder}     from "../src/EmptyCollectionHolder"
 import {GenericCollectionIterator} from "../src/iterator/GenericCollectionIterator"
 
@@ -39,13 +40,13 @@ describe("CollectionIteratorTest", () => {
                 const it = instance.next()
 
                 test("done", () => expect(it.done,).toBeTrue(),)
-                test("value", () => expect(() => it.value,).toThrow(ReferenceError,),)
+                test("value", () => expect(it.value,).toBe(CollectionConstants.AFTER_LAST_VALUE_IN_ITERATOR_SYMBOL,),)
             },)
             describe("previous", () => {
                 const it = instance.previous()
 
                 test("done", () => expect(it.done,).toBeTrue(),)
-                test("value", () => expect(() => it.value,).toThrow(ReferenceError,),)
+                test("value", () => expect(it.value,).toBe(CollectionConstants.BEFORE_FIRST_VALUE_IN_ITERATOR_SYMBOL,),)
             },)
         },)
         describe("forEach", () => {
@@ -68,7 +69,7 @@ describe("CollectionIteratorTest", () => {
     describe("GenericCollectionIterator", () => {
         const EMPTY_CALLBACK = () => {}
 
-        describe.each(everyInstances,)("%s", ({value: collectionInstance,},) => { describe.each(iterableCreation,)("%s", ({value: iterableCreation,},) => {
+        describe.each(everyInstances,)("%s", ({value: collectionInstance,},) => {describe.each(iterableCreation,)("%s", ({value: iterableCreation,},) => {
             const maxSize = ABCDEFGHIJ.length,
                 newInstance = () => new GenericCollectionIterator<typeof ABCDEFGHIJ[number]>(newCollectionInstance(collectionInstance, iterableCreation, ABCDEFGHIJ,))
 
@@ -419,14 +420,14 @@ describe("CollectionIteratorTest", () => {
                     },)
                     describe("at the end", () => {
                         const it = newInstance().forEach(EMPTY_CALLBACK,).next()
-                        test("value", () => expect(() => it.value,).toThrow(ReferenceError,),)
+                        test("value", () => expect(it.value,).toBe(CollectionConstants.AFTER_LAST_VALUE_IN_ITERATOR_SYMBOL,),)
                         test("done", () => expect(it.done,).toBeTrue(),)
                     },)
                 },)
                 describe("previous", () => {
                     describe("at the start", () => {
                         const it = newInstance().previous()
-                        test("value", () => expect(() => it.value,).toThrow(ReferenceError,),)
+                        test("value", () => expect(it.value,).toBe(CollectionConstants.BEFORE_FIRST_VALUE_IN_ITERATOR_SYMBOL,),)
                         test("done", () => expect(it.done,).toBeTrue(),)
                     },)
                     describe("at the end", () => {
