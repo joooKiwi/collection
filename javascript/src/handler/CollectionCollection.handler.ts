f12b6bfcf472b1193a36b8effbc5db6e46ef70b6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-03 11:17:06 -0400

    Change on the "CollectionCollectionHandler" to be similar to the "ArrayCollectionHandler"

--- javascript/src/handler/CollectionCollection.handler.ts
+++ javascript/src/handler/CollectionCollection.handler.ts
@@ -14,9 +14,19 @@ import {ValueHolder}               from "./ValueHolder"
 export class CollectionCollectionHandler<out T = unknown, const REFERENCE extends CollectionHolder<T> = CollectionHolder<T>, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >
     extends AbstractCollectionHandler<T, REFERENCE, COLLECTION> {
 
+    //#region -------------------- Fields --------------------
+
+    #amountOfElementRetrieved?: number
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
     public constructor(collection: COLLECTION, reference: REFERENCE,) {
         super(collection, reference,)
     }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Getter methods --------------------
 
     public get isEmpty(): REFERENCE["isEmpty"] {
         return this._reference.isEmpty
@@ -26,26 +36,38 @@ export class CollectionCollectionHandler<out T = unknown, const REFERENCE extend
         return this._reference.size
     }
 
-    public get(index: number,): ValueHolder<T> {
-        const reference = this._reference,
-            value = reference.getOrNull(index,)
-        if (value == null) {
-            if ((index < 0 ? this.size + index : index) in reference)
-                return { value: value as T, cause: null, }
-            const $this = this
-            return { value: null, get cause() { return new ReferenceError(`The value doesn't exist in the collection "${$this._collection.constructor.name}" at the index ${index}.`,) }, }
-        }
-
-        const indexOfValue = reference.indexOf(value,)
-        if (indexOfValue == null) {
-            const $this = this
-            return { value: null, get cause() { return new ReferenceError(`The index doesn't exist in the collection "${$this._collection.constructor.name}" at the index ${index}.`,) }, }
-        }
-
-        return {value: this._collection[indexOfValue] = value, cause: null,}
+    /** The amount of element that was retrieved so far */
+    protected get _amountOfElementRetrieved(): number {
+        return this.#amountOfElementRetrieved ?? 0
     }
 
+    /** Set the amount of element that was retrieved so far */
+    protected set _amountOfElementRetrieved(value: number,) {
+        this.#amountOfElementRetrieved = value
+    }
 
+    //#endregion -------------------- Getter methods --------------------
 
+    public get(index: number,): ValueHolder<T> {
+        const collection = this._collection
+        if (index in collection)
+            return { value: collection[index] as T, cause: null, }
+
+        const size = this.size,
+            indexToRetrieve = index < 0 ? size + index : index
+        if (indexToRetrieve < 0)
+            return { value: null, get cause() { return new ReferenceError(`The index ${index}${index === indexToRetrieve ? '' : ` (${indexToRetrieve} after calculation)`} is under 0.`,) }, }
+        if (indexToRetrieve > size)
+            return { value: null, get cause() { return new ReferenceError(`The index ${index}${index === indexToRetrieve ? '' : ` (${indexToRetrieve} after calculation)`} is over the size of the collection (${size}).`,) }, }
+
+        if (this._hasFinished)
+            return { value: collection[indexToRetrieve] as T, cause: null, }
+        if (indexToRetrieve in collection)
+            return { value: collection[indexToRetrieve] as T, cause: null, }
+
+        if (size - 1 === this._amountOfElementRetrieved++)
+            this._hasFinished = true
+        return { value: collection[indexToRetrieve] = this._reference.get(indexToRetrieve,), cause: null, }
+    }
 
 }
