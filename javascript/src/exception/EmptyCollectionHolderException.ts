559045a304aed128815a79b8890ac2cb4ab40a18
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 18:11:08 -0500

    Simple change to the documentation of the exceptions

--- javascript/src/exception/EmptyCollectionHolderException.ts
+++ javascript/src/exception/EmptyCollectionHolderException.ts
@@ -9,7 +9,10 @@ import type {Nullable, NullOr, PossibleNumeric} from "../general type"
 
 import {CollectionHolderIndexOutOfBoundsException} from "./CollectionHolderIndexOutOfBoundsException"
 
-/** An exception made to tell that the collection was empty while not being expected to be empty */
+/**
+ * An exception made to tell that the {@link CollectionHolder collection}
+ * was empty while not being expected to be empty
+ */
 export class EmptyCollectionHolderException<const T extends NullOr<PossibleNumeric> = null,
     const out CAUSE extends Error = never, >
     extends CollectionHolderIndexOutOfBoundsException<T, CAUSE> {
