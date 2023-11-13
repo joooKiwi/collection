f7ccebf2e189c6a86e6fcde2ea0fb6cc2fa6db84
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 22:45:51 -0500

    Addition of specific callback variants on the "find(predicate)" extension method

--- javascript/src/method/find.ts
+++ javascript/src/method/find.ts
@@ -7,7 +7,10 @@
 
 import type {CollectionHolder}                           from "../CollectionHolder"
 import type {BooleanCallback, RestrainedBooleanCallback} from "../CollectionHolder.types"
+import type {NonEmptyCollectionHolder}                   from "../NonEmptyCollectionHolder"
 import type {Nullable, NullOr}                           from "../general type"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Get the first element found or <b>null</b> if nothing was found
@@ -17,6 +20,7 @@ import type {Nullable, NullOr}                           from "../general type"
  * @see ReadonlyArray.find
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/find.html Kotlin find(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
+ * @typescriptDefinition
  * @extensionFunction
  */
 export function find<const T, const S extends T, >(collection: Nullable<CollectionHolder<T>>, predicate: RestrainedBooleanCallback<T, S>,): NullOr<S>
@@ -37,6 +41,37 @@ export function find<const T, const S extends T, >(collection: Nullable<Collecti
     if (collection.isEmpty)
         return null
 
+    if (predicate.length === 1)
+        return __with1Argument(collection as NonEmptyCollectionHolder<T>, predicate as (value: T,) => boolean,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection as NonEmptyCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyCollectionHolder<T>, predicate as () => boolean,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __with0Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: () => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return collection.get(index,)
+    return null
+}
+
+function __with1Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size) {
+        const value = collection.get(index,)
+        if (predicate(value,))
+            return value
+    }
+    return null
+}
+
+function __with2Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
     const size = collection.size
     let index = -1
     while (++index < size) {
@@ -46,3 +81,5 @@ export function find<const T, const S extends T, >(collection: Nullable<Collecti
     }
     return null
 }
+
+//#endregion -------------------- Loop methods --------------------
