23cd0a24f485112cb0bcf061e887a80baf8646ab
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 23:44:03 -0500

    Addition of specific callback variants on the "forEachIndexed(action)" extension method

--- javascript/src/method/forEachIndexed.ts
+++ javascript/src/method/forEachIndexed.ts
@@ -8,12 +8,14 @@
 import type {CollectionHolder}   from "../CollectionHolder"
 import type {IndexValueCallback} from "../CollectionHolder.types"
 
+//#region -------------------- Facade method --------------------
+
 /**
  * Perform a given {@link action} on each element
  * and return the {@link collection} afterwards
  *
  * @param collection The {@link CollectionHolder collection}
- * @param action The given action
+ * @param action     The given action
  * @see ReadonlyArray.forEach
  * @see ReadonlySet.forEach
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/on-each.html Kotlin onEach(action)
@@ -21,9 +23,37 @@ import type {IndexValueCallback} from "../CollectionHolder.types"
  * @extensionFunction
  */
 export function forEachIndexed<const T, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >(collection: COLLECTION, action: IndexValueCallback<T>,): COLLECTION {
+    if (action.length === 1)
+        return __with1Argument(collection, action as (index: number,) => void,)
+    if (action.length >= 2)
+        return __with2Argument(collection, action,)
+    return __with0Argument(collection, action as () => void,)
+}
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __with0Argument<const T, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >(collection: COLLECTION, action: () => void,) {
     const size = collection.size
     let index = -1
     while (++index < size)
-        action(index, collection.get(index,),)
+        action()
     return collection
 }
+
+function __with1Argument<const T, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >(collection: COLLECTION, action: (index: number,) => void,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        action(index,)
+    return collection
+}
+
+function __with2Argument<const T, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >(collection: COLLECTION, action: (index: number,value: T, ) => void,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        action(index,collection.get(index,), )
+    return collection
+}
+
+//#endregion -------------------- Loop methods --------------------
