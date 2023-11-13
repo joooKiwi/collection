e5f562e7c905dbe96995e4ddabe8289320ff0403
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 22:15:42 -0500

    Addition of specific callback variants on the "any(predicate?)" extension method

--- javascript/src/method/any.ts
+++ javascript/src/method/any.ts
@@ -5,9 +5,12 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import type {CollectionHolder} from "../CollectionHolder"
-import type {BooleanCallback}  from "../CollectionHolder.types"
-import type {Nullable}         from "../general type"
+import type {CollectionHolder}         from "../CollectionHolder"
+import type {BooleanCallback}          from "../CollectionHolder.types"
+import type {NonEmptyCollectionHolder} from "../NonEmptyCollectionHolder"
+import type {Nullable}                 from "../general type"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Tell if the {@link collection} {@link CollectionHolder.isNotEmpty is not empty}
@@ -24,7 +27,7 @@ export function any<const T, const COLLECTION extends CollectionHolder<T> = Coll
  * match the given {@link predicate}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The matching predicate
+ * @param predicate  The matching predicate
  * @returns {boolean} <b>true</b> if at least one {@link predicate} is true on a value of the {@link collection}
  * @see ReadonlyArray.some
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/any.html Kotlin any(predicate)
@@ -40,6 +43,36 @@ export function any<const T, >(collection: Nullable<CollectionHolder<T>>, predic
     if (collection.isEmpty)
         return false
 
+    if (predicate.length === 1)
+        return __with1Argument(collection as NonEmptyCollectionHolder<T>, predicate as (value: T,) => boolean,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection as NonEmptyCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyCollectionHolder<T>, predicate as () => boolean,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __with0Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: () => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return true
+    return false
+}
+
+function __with1Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        // @ts-ignore: We have only 1 argument in the predicate
+        if (predicate(collection.get(index,),))
+            return true
+    return false
+}
+
+function __with2Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
     const size = collection.size
     let index = -1
     while (++index < size)
@@ -47,3 +80,5 @@ export function any<const T, >(collection: Nullable<CollectionHolder<T>>, predic
             return true
     return false
 }
+
+//#endregion -------------------- Loop methods --------------------
