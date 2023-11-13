d69556cad51b02c08d5ac2c6664397471d13a639
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 22:47:54 -0500

    Addition of specific callback variants on the "findLast(predicate)" extension method

--- javascript/src/method/findLast.ts
+++ javascript/src/method/findLast.ts
@@ -7,7 +7,10 @@
 
 import type {CollectionHolder}                           from "../CollectionHolder"
 import type {BooleanCallback, RestrainedBooleanCallback} from "../CollectionHolder.types"
+import type {NonEmptyCollectionHolder}                   from "../NonEmptyCollectionHolder"
 import type {Nullable, NullOr}                           from "../general type"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Get the last element found or <b>null</b> if nothing was found
@@ -25,7 +28,7 @@ export function findLast<const T, const S extends T, >(collection: Nullable<Coll
  * Get the last element found or <b>null</b> if nothing was found
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The given predicate
+ * @param predicate  The given predicate
  * @see ReadonlyArray.findLast
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/find-last.html Kotlin findLast(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.lastordefault C# LastOrDefault(predicate)
@@ -38,6 +41,35 @@ export function findLast<const T, const S extends T, >(collection: Nullable<Coll
     if (collection.isEmpty)
         return null
 
+    if (predicate.length === 1)
+        return __with1Argument(collection as NonEmptyCollectionHolder<T>, predicate as (value: T,) => boolean,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection as NonEmptyCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyCollectionHolder<T>, predicate as () => boolean,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __with0Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: () => boolean,) {
+    let index = collection.size
+    while (index-- > 0)
+        if (predicate())
+            return collection.get(index,)
+    return null
+}
+
+function __with1Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+    let index = collection.size
+    while (index-- > 0) {
+        const value = collection.get(index,)
+        if (predicate(value,))
+            return value
+    }
+    return null
+}
+
+function __with2Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
     let index = collection.size
     while (index-- > 0) {
         const value = collection.get(index,)
@@ -46,3 +78,5 @@ export function findLast<const T, const S extends T, >(collection: Nullable<Coll
     }
     return null
 }
+
+//#endregion -------------------- Loop methods --------------------
