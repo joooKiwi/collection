f7271db9e62c9e7dec2209e901d97c93da3fa2f7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-12 23:15:13 -0500

    Addition of specific callback variants on the "firstOrNull(predicate?)" extension method

--- javascript/src/method/firstOrNull.ts
+++ javascript/src/method/firstOrNull.ts
@@ -7,7 +7,10 @@
 
 import type {CollectionHolder}                           from "../CollectionHolder"
 import type {BooleanCallback, RestrainedBooleanCallback} from "../CollectionHolder.types"
+import type {NonEmptyCollectionHolder}                   from "../NonEmptyCollectionHolder"
 import type {Nullable, NullOr}                           from "../general type"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Get the first element in the {@link collection}
@@ -25,7 +28,7 @@ export function firstOrNull<const T, >(collection: Nullable<CollectionHolder<T>>
  * or <b>null</b> if the {@link collection} {@link CollectionHolder.isEmpty is empty}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The matching predicate
+ * @param predicate  The matching predicate
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/first-or-null.html Kotlin firstOrNull(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
  * @extensionFunction
@@ -37,7 +40,7 @@ export function firstOrNull<const T, const S extends T, >(collection: Nullable<C
  * or <b>null</b> if the {@link collection} {@link CollectionHolder.isEmpty is empty}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The matching predicate
+ * @param predicate  The matching predicate
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/first-or-null.html Kotlin firstOrNull(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
  * @extensionFunction
@@ -48,8 +51,52 @@ export function firstOrNull<const T, const S extends T, >(collection: Nullable<C
         return null
     if (collection.isEmpty)
         return null
-    if (predicate != null)
-        return collection.find(predicate,)
 
+    if (predicate == null)
+        return __withNoPredicate(collection as NonEmptyCollectionHolder<T>,)
+    if (predicate.length === 1)
+        return __with1Argument(collection as NonEmptyCollectionHolder<T>, predicate as (value: T,) => boolean,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection as NonEmptyCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyCollectionHolder<T>, predicate as () => boolean,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __withNoPredicate<const T, >(collection: NonEmptyCollectionHolder<T>,) {
     return collection.getOrNull(0,)
 }
+
+function __with0Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: () => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return collection.get(index,)
+    return null
+}
+
+function __with1Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size) {
+        const value = collection.get(index,)
+        if (predicate(value,))
+            return value
+    }
+    return null
+}
+
+function __with2Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size) {
+        const value = collection.get(index,)
+        if (predicate(value, index,))
+            return value
+    }
+    return null
+}
+
+//#endregion -------------------- Loop methods --------------------
