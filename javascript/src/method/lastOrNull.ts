4fb17213d614b033ed5e9bc508e2a33311061940
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-11-16 18:26:16 -0500

    Addition of specific callback variants on the "lastOrNull(predicate?)" extension method

--- javascript/src/method/lastOrNull.ts
+++ javascript/src/method/lastOrNull.ts
@@ -7,7 +7,10 @@
 
 import type {Nullable, NullOr}                           from "../general type"
 import type {CollectionHolder}                           from "../CollectionHolder"
+import type {NonEmptyCollectionHolder}                   from "../NonEmptyCollectionHolder"
 import type {BooleanCallback, RestrainedBooleanCallback} from "../CollectionHolder.types"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Get the last element in the current {@link collection}
@@ -25,7 +28,7 @@ export function lastOrNull<const T, >(collection: Nullable<CollectionHolder<T>>,
  * or <b>null</b> if the {@link collection} {@link CollectionHolder.isEmpty is empty}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The matching predicate
+ * @param predicate  The matching predicate
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/last-or-null.html Kotlin lastOrNull(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.lastordefault C# LastOrDefault(predicate)
  * @extensionFunction
@@ -37,7 +40,7 @@ export function lastOrNull<const T, const S extends T, >(collection: Nullable<Co
  * or <b>null</b> if the {@link collection} {@link CollectionHolder.isEmpty is empty}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @param predicate The matching predicate
+ * @param predicate  The matching predicate
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/last-or-null.html Kotlin lastOrNull(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.lastordefault C# LastOrDefault(predicate)
  * @extensionFunction
@@ -48,8 +51,53 @@ export function lastOrNull<const T, const S extends T, >(collection: Nullable<Co
         return null
     if (collection.isEmpty)
         return null
-    if (predicate != null)
-        return collection.findLast(predicate,)
 
-    return collection.getOrNull(-1,)
+    if (predicate == null)
+        return __withNoPredicate(collection as NonEmptyCollectionHolder<T>,)
+    if (predicate.length === 1)
+        return __with1Argument(collection as NonEmptyCollectionHolder<T>, predicate as (value: T,) => boolean,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection as NonEmptyCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyCollectionHolder<T>, predicate as () => boolean,)
 }
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __withNoPredicate<const T, >(collection: NonEmptyCollectionHolder<T>,) {
+    const lastIndex = collection.size - 1
+    if (lastIndex in collection)
+        return collection[lastIndex] as T
+    return collection.getOrNull(lastIndex,)
+}
+
+function __with0Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: () => boolean,) {
+    const size = collection.size
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return collection.get(index,)
+    return null
+}
+
+function __with1Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+    let index = collection.size
+    while (index-- > 0) {
+        const value = collection.get(index,)
+        if (predicate(value,))
+            return value
+    }
+    return null
+}
+
+function __with2Argument<const T, >(collection: NonEmptyCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
+    let index = collection.size
+    while (index-- > 0) {
+        const value = collection.get(index,)
+        if (predicate(value, index,))
+            return value
+    }
+    return null
+}
+
+//#endregion -------------------- Loop methods --------------------
