6fdf6470cdbd6ac7a639f1593d1c59fb8ade228a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-01 11:43:15 -0500

    Adaptation of the last changes for the minimalist implementation for the any(predicate?) extension method

--- javascript/src/method/any.ts
+++ javascript/src/method/any.ts
@@ -5,28 +5,41 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}                   from "../CollectionHolder"
 import type {BooleanCallback}                    from "../CollectionHolder.types"
 import type {Nullable}                           from "../general type"
-import type {NonEmptySimplisticCollectionHolder} from "../NonEmptySimplisticCollectionHolder"
-import type {SimplisticCollectionHolder}         from "../SimplisticCollectionHolder"
+import type {MinimalistCollectionHolder}         from "../MinimalistCollectionHolder"
+import type {NonEmptyMinimalistCollectionHolder} from "../NonEmptyMinimalistCollectionHolder"
+
+import {isCollectionHolder} from "./isCollectionHolder"
 
 //#region -------------------- Facade method --------------------
 
 /**
- * Tell if the {@link collection} {@link SimplisticCollectionHolder.isNotEmpty is not empty}
+ * Tell if the {@link collection} {@link CollectionHolder.isNotEmpty is not empty}
  *
  * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
- * @return {boolean} <b>false</b> if null is received or !{@link SimplisticCollectionHolder.isEmpty isEmpty} otherwise
+ * @return {boolean} <b>false</b> if null is received or {@link CollectionHolder.isNotEmpty isNotEmpty} otherwise
  * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/any.html Kotlin any()
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.any C# Any()
  * @extensionFunction
  */
-export function any<const T, const COLLECTION extends SimplisticCollectionHolder<T> = SimplisticCollectionHolder<T>, >(collection: Nullable<COLLECTION>,): COLLECTION["isEmpty"] extends true ? false : COLLECTION["isEmpty"] extends false ? true : boolean
+export function any<const T, const COLLECTION extends CollectionHolder<T> = CollectionHolder<T>, >(collection: Nullable<COLLECTION>,): COLLECTION["isNotEmpty"]
+/**
+ * Tell if the {@link collection} {@link MinimalistCollectionHolder.isEmpty is (not) empty}
+ *
+ * @param collection The {@link Nullable nullable} {@link MinimalistCollectionHolder collection}
+ * @return {boolean} <b>false</b> if null is received or !{@link MinimalistCollectionHolder.isEmpty isEmpty} otherwise
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/any.html Kotlin any()
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.any C# Any()
+ * @extensionFunction
+ */
+export function any<const T, const COLLECTION extends MinimalistCollectionHolder<T> = MinimalistCollectionHolder<T>, >(collection: Nullable<COLLECTION>,): COLLECTION["isEmpty"] extends true ? false : COLLECTION["isEmpty"] extends false ? true : boolean
 /**
  * Check if <b>one</b> of the elements in the {@link collection}
  * match the given {@link predicate}
  *
- * @param collection The {@link Nullable nullable} {@link SimplisticCollectionHolder collection}
+ * @param collection The {@link Nullable nullable} {@link MinimalistCollectionHolder collection}
  * @param predicate  The matching predicate
  * @return {boolean} <b>true</b> if at least one {@link predicate} is true on a value of the {@link collection}
  * @see ReadonlyArray.some
@@ -34,26 +47,29 @@ export function any<const T, const COLLECTION extends SimplisticCollectionHolder
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.any C# Any(predicate)
  * @extensionFunction
  */
-export function any<const T, >(collection: Nullable<SimplisticCollectionHolder<T>>, predicate: Nullable<BooleanCallback<T>>,): boolean
-export function any<const T, >(collection: Nullable<SimplisticCollectionHolder<T>>, predicate?: Nullable<BooleanCallback<T>>,): boolean {
+export function any<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: Nullable<BooleanCallback<T>>,): boolean
+export function any<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate?: Nullable<BooleanCallback<T>>,): boolean {
     if (collection == null)
         return false
     if (predicate == null)
-        return !collection.isEmpty
+        if (isCollectionHolder(collection,))
+            return collection.isNotEmpty
+        else
+            return !collection.isEmpty
     if (collection.isEmpty)
         return false
 
     if (predicate.length == 1)
-        return __with1Argument(collection as NonEmptySimplisticCollectionHolder<T>, predicate as (value: T,) => boolean,)
+        return __with1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (value: T,) => boolean,)
     if (predicate.length >= 2)
-        return __with2Argument(collection as NonEmptySimplisticCollectionHolder<T>, predicate,)
-    return __with0Argument(collection as NonEmptySimplisticCollectionHolder<T>, predicate as () => boolean,)
+        return __with2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate,)
+    return __with0Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as () => boolean,)
 }
 
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
-function __with0Argument<const T, >(collection: NonEmptySimplisticCollectionHolder<T>, predicate: () => boolean,) {
+function __with0Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: () => boolean,) {
     let index = collection.size
     while (index-- > 0)
         if (predicate())
@@ -61,7 +77,7 @@ function __with0Argument<const T, >(collection: NonEmptySimplisticCollectionHold
     return false
 }
 
-function __with1Argument<const T, >(collection: NonEmptySimplisticCollectionHolder<T>, predicate: (value: T,) => boolean,) {
+function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T,) => boolean,) {
     const size = collection.size
     let index = -1
     while (++index < size)
@@ -70,7 +86,7 @@ function __with1Argument<const T, >(collection: NonEmptySimplisticCollectionHold
     return false
 }
 
-function __with2Argument<const T, >(collection: NonEmptySimplisticCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
+function __with2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
     const size = collection.size
     let index = -1
     while (++index < size)
