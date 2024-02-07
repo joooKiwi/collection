a2dc13c45583d5c175e333a8bac76a0a7891649d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-07 10:43:27 -0500

    Addition of a type to give the iterable

--- javascript/src/iterable/types.ts
+++ javascript/src/iterable/types.ts
@@ -12,9 +12,21 @@ import type {IterableWithLength}       from "./IterableWithLength"
 import type {IterableWithPossibleSize} from "./IterableWithPossibleSize"
 import type {IterableWithSize}         from "./IterableWithSize"
 
-/** Every possible type of {@link Iterable} in combination with the size field (as size, length or count) */
+/**
+ * Every possible type of {@link Iterable} with the size field (size, length or count)
+ *
+ * @see PossibleIterableWithSize
+ */
 export type PossibleIterable<T, > = | Iterable<T>
-                                    | IterableWithPossibleSize<T>
-                                    | IterableWithSize<T>
-                                    | IterableWithLength<T>
-                                    | IterableWithCount<T>
+                                    | PossibleIterableWithSize<T>
+
+/**
+ * Every possible type of {@link Iterable} with a required (or close to it)
+ * size field (size, length or count)
+ *
+ * @see PossibleIterable
+ */
+export type PossibleIterableWithSize<T, > = | IterableWithPossibleSize<T>
+                                            | IterableWithSize<T>
+                                            | IterableWithLength<T>
+                                            | IterableWithCount<T>
