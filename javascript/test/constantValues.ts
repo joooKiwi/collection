c26f8c5a9a2b86a25ffb05ba84d14f923ed5b3eb
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-07 12:36:14 -0500

    Separation of the constants to use the "export const" instead of all being linked to the first exportation

--- javascript/test/constantValues.ts
+++ javascript/test/constantValues.ts
@@ -18,55 +18,55 @@ import {GenericCollectionIterator}         from "../src/iterator/GenericCollecti
 import {Holder} from "./Holder"
 
 export const sizeValues = () => [
-        new Holder({array: [], size: 0,}, '0',),
-        new Holder({array: [Symbol(),], size: 1,}, '1',),
-        new Holder({array: [Symbol(), Symbol(),], size: 2,}, '2',),
-    ] as const,
-    TEMPLATE_ITEMS = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
-        1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n, 0n,
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-        'à', 'â', 'á', 'ä', 'è', 'ê', 'é', 'ë', 'í', 'î', 'ì', 'ï', 'ó', 'ô', 'ò', 'ö', 'œ', 'ú', 'û', 'ù', 'ü', 'ç',
-        'À', 'Â', 'Á', 'Ä', 'È', 'Ê', 'É', 'Ë', 'Í', 'Î', 'Ì', 'Ï', 'Ó', 'Ô', 'Ò', 'Ö', 'Œ', 'Ú', 'Û', 'Ù', 'Ü', 'Ç',
-        true, false,
-    ] as const,
-    everySimplisticInstances = [
-        new Holder(GenericMinimalistCollectionHolder, "minimalist",),
-        new Holder(GenericCollectionHolder, "normal",),
-        new Holder(LazyGenericCollectionHolder, "lazy (normal)",),
-    ] as const,
-    everyInstances = [
-        new Holder(GenericCollectionHolder, "GenericCollectionHolder",),
-        new Holder(LazyGenericCollectionHolder, "LazyGenericCollectionHolder",),
-    ] as const,
-    iterableCreation = [
-        new Holder(<T>(array: readonly T[],) => Array.from(array,), "array",),
-        new Holder(<T>(array: readonly T[],) => new Set(array,), "set",),
-        new Holder(<T>(array: readonly T[],) => ({
-            [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
-        }) as Iterable<T>, "iterable",),
-        new Holder(<T>(array: readonly T[],) => ({
-            [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
-            size: array.length,
-        }) as IterableWithSize<T>, "iterable with size",),
-        new Holder(<T>(array: readonly T[],) => ({
-            [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
-            length: array.length,
-        }) as IterableWithLength<T>, "iterable with length",),
-        new Holder(<T>(array: readonly T[],) => ({
-            [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
-            count: array.length,
-        }) as IterableWithCount<T>, "iterable with count",),
-        new Holder(<T>(array: readonly T[],) => ({
-            [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
-            size: null,
-            length: null,
-            count: null,
-        }) as IterableWithPossibleSize<T>, "iterable with null size, length and count",),
-        new Holder(<T>(array: readonly T[],) => new GenericCollectionIterator(new GenericCollectionHolder(array,),), "collection iterator + generic collection",),
-        new Holder(<T>(array: readonly T[],) => new GenericCollectionIterator(new LazyGenericCollectionHolder(array,),), "collection iterator + lazy collection",),
-        new Holder(<T>(array: readonly T[],) => new GenericCollectionHolder(array,), "generic collection",),
-        new Holder(<T>(array: readonly T[],) => new LazyGenericCollectionHolder(array,), "lazy collection",),
-    ],
-    nonPresentItem = Symbol()
+    new Holder({array: [], size: 0,}, "0",),
+    new Holder({array: [Symbol(),], size: 1,}, "1",),
+    new Holder({array: [Symbol(), Symbol(),], size: 2,}, "2",),
+] as const
+export const TEMPLATE_ITEMS = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+    1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n, 0n,
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'à', 'â', 'á', 'ä', 'è', 'ê', 'é', 'ë', 'í', 'î', 'ì', 'ï', 'ó', 'ô', 'ò', 'ö', 'œ', 'ú', 'û', 'ù', 'ü', 'ç',
+    'À', 'Â', 'Á', 'Ä', 'È', 'Ê', 'É', 'Ë', 'Í', 'Î', 'Ì', 'Ï', 'Ó', 'Ô', 'Ò', 'Ö', 'Œ', 'Ú', 'Û', 'Ù', 'Ü', 'Ç',
+    true, false,
+] as const
+export const everySimplisticInstances = [
+    new Holder(GenericMinimalistCollectionHolder, "minimalist",),
+    new Holder(GenericCollectionHolder, "normal",),
+    new Holder(LazyGenericCollectionHolder, "lazy (normal)",),
+] as const
+export const everyInstances = [
+    new Holder(GenericCollectionHolder, "GenericCollectionHolder",),
+    new Holder(LazyGenericCollectionHolder, "LazyGenericCollectionHolder",),
+] as const
+export const iterableCreation = [
+    new Holder(<T>(array: readonly T[],) => Array.from(array,), "array",),
+    new Holder(<T>(array: readonly T[],) => new Set(array,), "set",),
+    new Holder(<T>(array: readonly T[],) => ({
+        [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
+    }) as Iterable<T>, "iterable",),
+    new Holder(<T>(array: readonly T[],) => ({
+        [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
+        size: array.length,
+    }) as IterableWithSize<T>, "iterable with size",),
+    new Holder(<T>(array: readonly T[],) => ({
+        [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
+        length: array.length,
+    }) as IterableWithLength<T>, "iterable with length",),
+    new Holder(<T>(array: readonly T[],) => ({
+        [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
+        count: array.length,
+    }) as IterableWithCount<T>, "iterable with count",),
+    new Holder(<T>(array: readonly T[],) => ({
+        [Symbol.iterator](): IterableIterator<T> { return array[Symbol.iterator]() },
+        size: null,
+        length: null,
+        count: null,
+    }) as IterableWithPossibleSize<T>, "iterable with null size, length and count",),
+    new Holder(<T>(array: readonly T[],) => new GenericCollectionIterator(new GenericCollectionHolder(array,),), "collection iterator + generic collection",),
+    new Holder(<T>(array: readonly T[],) => new GenericCollectionIterator(new LazyGenericCollectionHolder(array,),), "collection iterator + lazy collection",),
+    new Holder(<T>(array: readonly T[],) => new GenericCollectionHolder(array,), "generic collection",),
+    new Holder(<T>(array: readonly T[],) => new LazyGenericCollectionHolder(array,), "lazy collection",),
+]
+export const nonPresentItem = Symbol()
