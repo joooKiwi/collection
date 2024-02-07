b4de8e01be20c10865d401bd2d8c24b4da26e271
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-07 12:52:24 -0500

    Separation of the regular tests to the alias methods tests for the "size"

--- javascript/test/CollectionHolder.02 - size.test.ts
+++ javascript/test/CollectionHolder.02 - size.test.ts
@@ -5,21 +5,87 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import {everySimplisticInstances, sizeValues}                   from "./constantValues"
-import {newCollectionInstance, newMinimalistCollectionInstance} from "./newCollectionInstance"
-import {GenericMinimalistCollectionHolder}                      from "../src/GenericMinimalistCollectionHolder"
+import {everySimplisticInstances, sizeValues} from "./constantValues"
+import {newCollectionInstance}                from "./newCollectionInstance"
+import {GenericMinimalistCollectionHolder}    from "../src/GenericMinimalistCollectionHolder"
+import {LazyGenericCollectionHolder}          from "../src/LazyGenericCollectionHolder"
+import {GenericCollectionHolder}              from "../src/GenericCollectionHolder"
 
-describe("CollectionHolderTest (size)", () =>
-describe.each(everySimplisticInstances,)("%s", ({value: instance,},) =>
-describe.each(sizeValues(),)("%s", ({value: {array, size,},},) => {
-    if (instance === GenericMinimalistCollectionHolder) {
-        test("size",       () => expect(newMinimalistCollectionInstance(instance, array,).size,).toEqual(size,),)
-        test("isEmpty",    () => expect(newMinimalistCollectionInstance(instance, array,).isEmpty,).toBe(size == 0,),)
-        return
-    }
-    test("size",       () => expect(newCollectionInstance(instance, array,).size,).toEqual(size,),)
-    test("length",     () => expect(newCollectionInstance(instance, array,).length,).toEqual(size,),)
-    test("count",      () => expect(newCollectionInstance(instance, array,).count,).toEqual(size,),)
-    test("isEmpty",    () => expect(newCollectionInstance(instance, array,).isEmpty,).toBe(size == 0,),)
-    test("isNotEmpty", () => expect(newCollectionInstance(instance, array,).isNotEmpty,).toBe(size != 0,),)
-},),),)
+describe("CollectionHolderTest (size)", () => {
+
+    describe("aliases", () => {
+        describe("GenericCollectionHolder", () => {
+            test("length", () => {
+                const instance = new class GenericCollectionHolder_LengthTest
+                    extends GenericCollectionHolder {
+
+                    public amountOfCall = 0
+
+                    public override get length(): this["size"] {
+                        this.amountOfCall++
+                        return super.length
+                    }
+
+                }([],)
+                instance.length
+                expect(instance.amountOfCall,).toBe(1,)
+            },)
+            test("count", () => {
+                const instance = new class GenericCollectionHolder_CountTest
+                    extends GenericCollectionHolder {
+
+                    public amountOfCall = 0
+
+                    public override get count(): this["size"] {
+                        this.amountOfCall++
+                        return super.count
+                    }
+
+                }([],)
+                instance.count
+                expect(instance.amountOfCall,).toBe(1,)
+            },)
+        },)
+        describe("LazyGenericCollectionHolder", () => {
+            test("length", () => {
+                const instance = new class LazyGenericCollectionHolder_LengthTest
+                    extends LazyGenericCollectionHolder {
+
+                    public amountOfCall = 0
+
+                    public override get length(): this["size"] {
+                        this.amountOfCall++
+                        return super.length
+                    }
+
+                }([],)
+                instance.length
+                expect(instance.amountOfCall,).toBe(1,)
+            },)
+            test("count", () => {
+                const instance = new class LazyGenericCollectionHolder_CountTest
+                    extends LazyGenericCollectionHolder {
+
+                    public amountOfCall = 0
+
+                    public override get count(): this["size"] {
+                        this.amountOfCall++
+                        return super.count
+                    }
+
+                }([],)
+                instance.count
+                expect(instance.amountOfCall,).toBe(1,)
+            },)
+        },)
+    },)
+
+    describe.each(everySimplisticInstances,)("%s", ({value: instance,},) =>
+        describe.each(sizeValues(),)("%s", ({value: {array, size,},},) => {
+            test("size", () => expect(newCollectionInstance(instance, array,).size,).toEqual(size,),)
+            test("isEmpty", () => expect(newCollectionInstance(instance, array,).isEmpty,).toBe(size == 0,),)
+            if (instance !== GenericMinimalistCollectionHolder)
+                test("isNotEmpty", () => expect(newCollectionInstance(instance, array,).isNotEmpty,).toBe(size != 0,),)
+        },),)
+
+},)
