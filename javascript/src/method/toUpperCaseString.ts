5b6d9b478a0ef96b3421d77e2853d25163220228
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:13:18 -0500

    Change and addition of handling on the "toUpperCaseString" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toUpperCaseString.ts
+++ javascript/src/method/toUpperCaseString.ts
@@ -5,6 +5,7 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
@@ -23,15 +24,39 @@ import {asUpperCaseString} from "./asString"
 export function toUpperCaseString<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): string {
     if (collection == null)
         return "[]"
+
+    const size = collection.size
+    if (size == 0)
+        return "[]"
+    return __toString(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to a {@link String} on every value
+ * by calling its "<i>{@link String.toUpperCase toUpperCase()}</i>" method
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @see String.toUpperCase
+ * @extensionFunction
+ */
+export function toUpperCaseStringByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): string {
+    if (collection == null)
+        return "[]"
     if (collection.isEmpty)
         return "[]"
+    return __toString(collection, collection.size,)
+}
 
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __toString(collection: MinimalistCollectionHolder, size: number,) {
     let string = ""
-    const sizeMinus1 = collection.size - 1
+    const sizeMinus1 = size - 1
     let index = -1
     while (++index < sizeMinus1)
         string += `${asUpperCaseString(collection.get(index,))}, `
     return `[${string}${asUpperCaseString(collection.get(index,))}]`
 }
 
-//#endregion -------------------- Facade method --------------------
+//#endregion -------------------- Loop method --------------------
