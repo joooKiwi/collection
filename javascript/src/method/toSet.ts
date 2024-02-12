5e8a2255e763b712eec63cd3cefefc9553557fd5
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:12:08 -0500

    Change and addition of handling on the "toSet" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toSet.ts
+++ javascript/src/method/toSet.ts
@@ -5,10 +5,13 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {CollectionConstants} from "../CollectionConstants"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link ReadonlySet set}
@@ -19,13 +22,36 @@ import {CollectionConstants} from "../CollectionConstants"
 export function toSet<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): ReadonlySet<T> {
     if (collection == null)
         return CollectionConstants.EMPTY_SET
-    if (collection.isEmpty)
-        return CollectionConstants.EMPTY_SET
 
     const size = collection.size
+    if (size == 0)
+        return CollectionConstants.EMPTY_SET
+    return __newSet(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to an {@link ReadonlySet set}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection} to convert
+ * @extensionFunction
+ */
+export function toSetByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): ReadonlySet<T> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_SET
+    if (collection.isEmpty)
+        return CollectionConstants.EMPTY_SET
+    return __newSet(collection, collection.size,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __newSet<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const set = new Set<T>()
     let index = -1
     while (++index < size)
         set.add(collection.get(index,),)
     return Object.freeze(set,)
 }
+
+//#endregion -------------------- Loop method --------------------
