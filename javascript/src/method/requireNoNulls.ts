ec305f7e0904e6e5e682593d053f46c2d3bf13fb
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:11:04 -0500

    Change and addition of handling on the "requireNonNulls" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/requireNoNulls.ts
+++ javascript/src/method/requireNoNulls.ts
@@ -10,7 +10,6 @@ import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {CollectionConstants} from "../CollectionConstants"
-import {isCollectionHolder}  from "./isCollectionHolder"
 
 /**
  * Require that no items are <b>null</b> or <b>undefined</b> in the {@link collection}
@@ -35,18 +34,35 @@ export function requireNoNulls<const T, >(collection: Nullable<MinimalistCollect
 export function requireNoNulls<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,) {
     if (collection == null)
         return CollectionConstants.EMPTY_COLLECTION_HOLDER
-    if (collection.isEmpty)
-        return CollectionConstants.EMPTY_COLLECTION_HOLDER
-
-    if (isCollectionHolder(collection,))
-        if (collection.hasNull)
-            throw new TypeError("Forbidden null value. The current collection contains null values.",)
 
     const size = collection.size
+    if (size == 0)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+
     let index = -1
     while (++index < size)
         if (collection.get(index) == null)
             throw new TypeError("Forbidden null value. The current collection contains null values.",)
 
+    return collection
+}
+
+/**
+ * Require that no items are <b>null</b> or <b>undefined</b> in the {@link collection}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @throws {TypeError} There is <b>null</b> or <b>undefined</b> value in the current collection
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/require-no-nulls.html Kotlin requireNoNulls()
+ * @see filterNotNull
+ * @extensionFunction
+ */
+export function requireNoNullsByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): CollectionHolder<NonNullable<T>>
+export function requireNoNullsByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,) {
+    if (collection == null)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+    if (collection.isEmpty)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+    if (collection.hasNull)
+        throw new TypeError("Forbidden null value. The current collection contains null values.",)
     return collection
 }
