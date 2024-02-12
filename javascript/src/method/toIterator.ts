41ecc6a50e1642d1fd1edf52ba2145397e3626b6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:11:40 -0500

    Change and addition of handling on the "toIterator" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toIterator.ts
+++ javascript/src/method/toIterator.ts
@@ -6,11 +6,13 @@
  ******************************************************************************/
 
 import type {Nullable}                   from "../general type"
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 import type {CollectionIterator}         from "../iterator/CollectionIterator"
 
 import {CollectionConstants}       from "../CollectionConstants"
 import {GenericCollectionIterator} from "../iterator/GenericCollectionIterator"
+import {isCollectionHolder}        from "./isCollectionHolder"
 
 /**
  * Convert the {@link collection} to a new {@link CollectionIterator}
@@ -21,8 +23,27 @@ import {GenericCollectionIterator} from "../iterator/GenericCollectionIterator"
 export function toIterator<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): CollectionIterator<T> {
     if (collection == null)
         return CollectionConstants.EMPTY_COLLECTION_ITERATOR
+    if (isCollectionHolder(collection,)) {
+        if (collection.isEmpty)
+            return CollectionConstants.EMPTY_COLLECTION_ITERATOR
+        return new GenericCollectionIterator(collection,)
+    }
+
+    if (collection.size == 0)
+        return CollectionConstants.EMPTY_COLLECTION_ITERATOR
+    return new GenericCollectionIterator(collection,)
+}
+
+/**
+ * Convert the {@link collection} to a new {@link CollectionIterator}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @extensionFunction
+ */
+export function toIteratorByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): CollectionIterator<T> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_COLLECTION_ITERATOR
     if (collection.isEmpty)
         return CollectionConstants.EMPTY_COLLECTION_ITERATOR
-
     return new GenericCollectionIterator(collection,)
 }
