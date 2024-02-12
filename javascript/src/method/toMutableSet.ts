8dcc04ae269e4b316097486c3cbd6cc1171dfd7c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:12:18 -0500

    Change and addition of handling on the "toMutableSet" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toMutableSet.ts
+++ javascript/src/method/toMutableSet.ts
@@ -5,8 +5,11 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link Set mutable set}
@@ -17,13 +20,37 @@ import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 export function toMutableSet<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): Set<T> {
     if (collection == null)
         return new Set()
-    if (collection.isEmpty)
-        return new Set()
 
     const size = collection.size
+    if (size == 0)
+        return new Set()
+    return __newMutableSet(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to an {@link Set mutable set}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection} to convert
+ * @extensionFunction
+ */
+export function toMutableSetByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): Set<T> {
+    if (collection == null)
+        return new Set()
+    if (collection.isEmpty)
+        return new Set()
+    return __newMutableSet(collection, collection.size,)
+
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __newMutableSet<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const set = new Set<T>()
     let index = -1
     while (++index < size)
         set.add(collection.get(index,),)
     return set
 }
+
+//#region -------------------- Loop method --------------------
