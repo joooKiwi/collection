ea3931b311b90d00ddf460e505f63dfe333944e1
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:09:34 -0500

    Change and addition of handling on the "join" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/join.ts
+++ javascript/src/method/join.ts
@@ -5,13 +5,14 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import type {StringCallback}                     from "../CollectionHolder.types"
-import type {Nullable}                           from "../general type"
-import type {MinimalistCollectionHolder}         from "../MinimalistCollectionHolder"
-import type {NonEmptyMinimalistCollectionHolder} from "../NonEmptyMinimalistCollectionHolder"
+import type {CollectionHolder}           from "../CollectionHolder"
+import type {StringCallback}             from "../CollectionHolder.types"
+import type {Nullable}                   from "../general type"
+import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+import type {NonEmptyCollectionHolder}   from "../NonEmptyCollectionHolder"
 
-import {CollectionConstants} from "../CollectionConstants"
-import {maximumIndex}        from "./maximumIndex"
+import {CollectionConstants}                          from "../CollectionConstants"
+import {maximumIndex, maximumIndexByCollectionHolder} from "./maximumIndex"
 
 //#region -------------------- Facade method --------------------
 
@@ -38,41 +39,101 @@ import {maximumIndex}        from "./maximumIndex"
 export function join<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, separator: Nullable<string> = null, prefix: Nullable<string> = null, postfix: Nullable<string> = null, limit: Nullable<number> = null, truncated: Nullable<string> = null, transform: Nullable<StringCallback<T>> = null,): string {
     if (collection == null)
         return prefixAndPostfixOnly(prefix, postfix,)
+
+    const size = collection.size
+    if (size == 0)
+        return prefixAndPostfixOnly(prefix, postfix,)
+
+    if (transform == null) {
+        if (limit == null)
+            return __withNothing(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size,)
+
+        if (size === limit)
+            return __withNothing(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size,)
+        return __withTruncated(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, limit, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED,)
+    }
+
+    if (limit == null) {
+        if (transform.length == 1)
+            return __with1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform as (value: T,) => string,)
+        if (transform.length >= 2)
+            return __with2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform,)
+        return __with0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform as () => string,)
+    }
+
+    if (size === limit) {
+        if (transform.length == 1)
+            return __with1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform as (value: T,) => string,)
+        if (transform.length >= 2)
+            return __with2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform,)
+        return __with0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform as () => string,)
+    }
+
+    if (transform.length == 1)
+        return __withTruncatedAnd1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection, limit, size,) ?? size, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as (value: T,) => string,)
+    if (transform.length >= 2)
+        return __withTruncatedAnd2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection, limit, size,) ?? size, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform,)
+    return __withTruncatedAnd0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection, limit, size,) ?? size, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as () => string,)
+}
+
+/**
+ * Create a new {@link String} from every element in the {@link collection} using a {@link separator}
+ * utilizing the given {@link prefix} and {@link postfix} if supplied.
+ *
+ * Note that if the {@link collection} is huge,
+ * a {@link limit} can be specified followed by a {@link truncated} value.
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param separator  The separator for the result ({@link CollectionConstants.DEFAULT_JOIN_SEPARATOR ", "} by default)
+ * @param prefix     The character before the join ({@link CollectionConstants.DEFAULT_JOIN_PREFIX "["} by default)
+ * @param postfix    The character after the join ({@link CollectionConstants.DEFAULT_JOIN_POSTFIX "]"} by default)
+ * @param limit      The maximum amount of values in the join (null by default)
+ * @param truncated  The truncated string if there is a limit ({@link CollectionConstants.DEFAULT_JOIN_TRUNCATED "…"} by default)
+ * @param transform  A callback to transform into a {@link String}
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link limit} is not within a valid range
+ * @see ReadonlyArray.join
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/join-to-string.html Kotlin joinToString()
+ * @see https://learn.microsoft.com/dotnet/api/system.string.join C# string.Join()
+ * @canReceiveNegativeValue
+ */
+export function joinByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, separator: Nullable<string> = null, prefix: Nullable<string> = null, postfix: Nullable<string> = null, limit: Nullable<number> = null, truncated: Nullable<string> = null, transform: Nullable<StringCallback<T>> = null,): string {
+    if (collection == null)
+        return prefixAndPostfixOnly(prefix, postfix,)
     if (collection.isEmpty)
         return prefixAndPostfixOnly(prefix, postfix,)
 
     if (transform == null) {
         if (limit == null)
-            return __withNothing(collection as NonEmptyMinimalistCollectionHolder, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size,)
+            return __withNothing(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size,)
 
         const size = collection.size
         if (size === limit)
-            return __withNothing(collection as NonEmptyMinimalistCollectionHolder, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size,)
-        return __withTruncated(collection as NonEmptyMinimalistCollectionHolder, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, limit, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED,)
+            return __withNothing(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size,)
+        return __withTruncated(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, limit, truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED,)
     }
 
     if (limit == null) {
         if (transform.length == 1)
-            return __with1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform as (value: T,) => string,)
+            return __with1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform as (value: T,) => string,)
         if (transform.length >= 2)
-            return __with2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform,)
+            return __with2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform,)
         return __with0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, collection.size, transform as () => string,)
     }
 
     const size = collection.size
     if (size === limit) {
         if (transform.length == 1)
-            return __with1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform as (value: T,) => string,)
+            return __with1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform as (value: T,) => string,)
         if (transform.length >= 2)
-            return __with2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform,)
+            return __with2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform,)
         return __with0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, size, transform as () => string,)
     }
 
     if (transform.length == 1)
-        return __withTruncatedAnd1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection as NonEmptyMinimalistCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as (value: T,) => string,)
+        return __withTruncatedAnd1Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as (value: T,) => string,)
     if (transform.length >= 2)
-        return __withTruncatedAnd2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection as NonEmptyMinimalistCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform,)
-    return __withTruncatedAnd0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndex(collection as NonEmptyMinimalistCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as () => string,)
+        return __withTruncatedAnd2Argument(collection, separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform,)
+    return __withTruncatedAnd0Argument(separator ?? CollectionConstants.DEFAULT_JOIN_SEPARATOR, prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX, postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX, maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,), truncated ?? CollectionConstants.DEFAULT_JOIN_TRUNCATED, transform as () => string,)
 }
 
 //#endregion -------------------- Facade method --------------------
@@ -83,6 +144,8 @@ export function join<const T, >(collection: Nullable<MinimalistCollectionHolder<
  *
  * @param prefix The prefix to apply (or {@link CollectionConstants.DEFAULT_JOIN_PREFIX "["} by default)
  * @param postfix The prefix to apply (or {@link CollectionConstants.DEFAULT_JOIN_POSTFIX "]"} by default)
+ * @see join
+ * @see joinByCollectionHolder
  */
 export function prefixAndPostfixOnly(prefix: Nullable<string> = null, postfix: Nullable<string> = null,): string {
     return `${prefix ?? CollectionConstants.DEFAULT_JOIN_PREFIX}${postfix ?? CollectionConstants.DEFAULT_JOIN_POSTFIX}`
@@ -91,7 +154,7 @@ export function prefixAndPostfixOnly(prefix: Nullable<string> = null, postfix: N
 //#endregion -------------------- Prefix & postfix method --------------------
 //#region -------------------- Loop methods --------------------
 
-function __withNothing(collection: NonEmptyMinimalistCollectionHolder, separator: string, prefix: string, postfix: string, lastIndex: number,) {
+function __withNothing(collection: MinimalistCollectionHolder, separator: string, prefix: string, postfix: string, lastIndex: number,) {
     let string = ""
     const sizeMinus1 = lastIndex - 1
     let index = -1
@@ -110,7 +173,7 @@ function __with0Argument(separator: string, prefix: string, postfix: string, las
     return `${prefix}${string}${transform()}${postfix}`
 }
 
-function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T,) => string,) {
+function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T,) => string,) {
     let string = ""
     const sizeMinus1 = lastIndex - 1
     let index = -1
@@ -119,7 +182,7 @@ function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHold
     return `${prefix}${string}${transform(collection.get(index,),)}${postfix}`
 }
 
-function __with2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T, index: number,) => string,) {
+function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T, index: number,) => string,) {
     let string = ""
     const sizeMinus1 = lastIndex - 1
     let index = -1
@@ -129,7 +192,7 @@ function __with2Argument<const T, >(collection: NonEmptyMinimalistCollectionHold
 }
 
 
-function __withTruncated(collection: NonEmptyMinimalistCollectionHolder, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string,) {
+function __withTruncated(collection: MinimalistCollectionHolder, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string,) {
     let string = ""
     const lastIndexMinus1 = lastIndex - 1
     let index = -1
@@ -148,7 +211,7 @@ function __withTruncatedAnd0Argument(separator: string, prefix: string, postfix:
     return `${prefix}${string}${transform()}${separator}${truncated}${postfix}`
 }
 
-function __withTruncatedAnd1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T,) => string,) {
+function __withTruncatedAnd1Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T,) => string,) {
     let string = ""
     const lastIndexMinus1 = lastIndex - 1
     let index = -1
@@ -157,7 +220,7 @@ function __withTruncatedAnd1Argument<const T, >(collection: NonEmptyMinimalistCo
     return `${prefix}${string}${transform(collection.get(index,),)}${separator}${truncated}${postfix}`
 }
 
-function __withTruncatedAnd2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T, index: number,) => string,) {
+function __withTruncatedAnd2Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T, index: number,) => string,) {
     let string = ""
     const lastIndexMinus1 = lastIndex - 1
     let index = -1
