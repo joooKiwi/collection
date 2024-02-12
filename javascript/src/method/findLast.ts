fb9555842e22e9e68113d0b0fa6d0599daf7dfb7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:07:58 -0500

    Change and addition of handling on the "findLast" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/findLast.ts
+++ javascript/src/method/findLast.ts
@@ -5,10 +5,10 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}                           from "../CollectionHolder"
 import type {BooleanCallback, RestrainedBooleanCallback} from "../CollectionHolder.types"
 import type {Nullable, NullOr}                           from "../general type"
 import type {MinimalistCollectionHolder}                 from "../MinimalistCollectionHolder"
-import type {NonEmptyMinimalistCollectionHolder}         from "../NonEmptyMinimalistCollectionHolder"
 
 //#region -------------------- Facade method --------------------
 
@@ -38,29 +38,67 @@ export function findLast<const T, >(collection: Nullable<MinimalistCollectionHol
 export function findLast<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: BooleanCallback<T>,): NullOr<T> {
     if (collection == null)
         return null
+
+    const size = collection.size
+    if (size == 0)
+        return null
+
+    if (predicate.length == 1)
+        return __with1Argument(collection, predicate as (value: T,) => boolean, size,)
+    if (predicate.length >= 2)
+        return __with2Argument(collection, predicate, size,)
+    return __with0Argument(collection, predicate as () => boolean, size,)
+}
+
+/**
+ * Get the last element found or <b>null</b> if nothing was found
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.findLast
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/find-last.html Kotlin findLast(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.lastordefault C# LastOrDefault(predicate)
+ * @typescriptDefinition
+ * @extensionFunction
+ */
+export function findLastByCollectionHolder<const T, const S extends T, >(collection: Nullable<CollectionHolder<T>>, predicate: RestrainedBooleanCallback<T, S>,): NullOr<S>
+/**
+ * Get the last element found or <b>null</b> if nothing was found
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.findLast
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/find-last.html Kotlin findLast(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.lastordefault C# LastOrDefault(predicate)
+ * @extensionFunction
+ */
+export function findLastByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, predicate: BooleanCallback<T>,): NullOr<T>
+export function findLastByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, predicate: BooleanCallback<T>,): NullOr<T> {
+    if (collection == null)
+        return null
     if (collection.isEmpty)
         return null
 
     if (predicate.length == 1)
-        return __with1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (value: T,) => boolean,)
+        return __with1Argument(collection, predicate as (value: T,) => boolean, collection.size,)
     if (predicate.length >= 2)
-        return __with2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate,)
-    return __with0Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as () => boolean,)
+        return __with2Argument(collection, predicate, collection.size,)
+    return __with0Argument(collection, predicate as () => boolean, collection.size,)
 }
 
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
-function __with0Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: () => boolean,) {
-    let index = collection.size
+function __with0Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: () => boolean, size: number,) {
+    let index = size
     while (--index > 0)
         if (predicate())
             return collection.get(index,)
     return null
 }
 
-function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T,) => boolean,) {
-    let index = collection.size
+function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, size: number,) {
+    let index = size
     while (index-- > 0) {
         const value = collection.get(index,)
         if (predicate(value,))
@@ -69,8 +107,8 @@ function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHold
     return null
 }
 
-function __with2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean,) {
-    let index = collection.size
+function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, size: number,) {
+    let index = size
     while (index-- > 0) {
         const value = collection.get(index,)
         if (predicate(value, index,))
