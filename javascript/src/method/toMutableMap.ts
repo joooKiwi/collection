047e31376aa95b1c99faa9094834285c02a87994
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:11:59 -0500

    Change and addition of handling on the "toMutableMap" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toMutableMap.ts
+++ javascript/src/method/toMutableMap.ts
@@ -5,8 +5,11 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link Map mutable map}
@@ -17,12 +20,36 @@ import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 export function toMutableMap<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): Map<number, T> {
     if (collection == null)
         return new Map()
+
+    const size = collection.size
+    if (size == 0)
+        return new Map()
+    return __newMutableMap(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to an {@link Map mutable map}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection} to convert
+ * @extensionFunction
+ */
+export function toMutableMapByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): Map<number, T> {
+    if (collection == null)
+        return new Map()
     if (collection.isEmpty)
         return new Map()
+    return __newMutableMap(collection, collection.size,)
+}
 
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __newMutableMap<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const map = new Map<number, T>()
-    let index = collection.size
+    let index = size
     while (index-- > 0)
         map.set(index, collection.get(index,),)
     return map
 }
+
+//#endregion -------------------- Loop method --------------------
