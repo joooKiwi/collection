ad9dcef330959f06510a26fed6320f87d83058b8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:11:30 -0500

    Change and addition of handling on the "toMutableArray" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toMutableArray.ts
+++ javascript/src/method/toMutableArray.ts
@@ -5,8 +5,11 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link Array mutable array}
@@ -17,13 +20,36 @@ import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 export function toMutableArray<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): T[] {
     if (collection == null)
         return []
-    if (collection.isEmpty)
-        return []
 
     const size = collection.size
+    if (size == 0)
+        return []
+    return __newMutableArray(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to an {@link Array mutable array}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection} to convert
+ * @extensionFunction
+ */
+export function toMutableArrayByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): T[] {
+    if (collection == null)
+        return []
+    if (collection.isEmpty)
+        return []
+    return __newMutableArray(collection, collection.size,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __newMutableArray<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const array = new Array<T>(size,)
     let index = size
     while (index-- > 0)
         array[index] = collection.get(index,)
     return array
 }
+
+//#endregion -------------------- Loop method --------------------
