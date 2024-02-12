2fbfa6edcaabf92a06f60d115e802c03aaadb47f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:09:19 -0500

    Change and addition of handling on the "indexOfLast" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/indexOfLast.ts
+++ javascript/src/method/indexOfLast.ts
@@ -5,14 +5,15 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import type {BooleanCallback}                    from "../CollectionHolder.types"
-import type {Nullable, NullOr}                   from "../general type"
-import type {MinimalistCollectionHolder}         from "../MinimalistCollectionHolder"
-import type {NonEmptyMinimalistCollectionHolder} from "../NonEmptyMinimalistCollectionHolder"
+import type {CollectionHolder}           from "../CollectionHolder"
+import type {BooleanCallback}            from "../CollectionHolder.types"
+import type {Nullable, NullOr}           from "../general type"
+import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+import type {NonEmptyCollectionHolder}   from "../NonEmptyCollectionHolder"
 
-import {endingIndex as endingIndexFunction}     from "./endingIndex"
-import {maximumIndex as maximumIndexFunction}   from "./maximumIndex"
-import {startingIndex as startingIndexFunction} from "./startingIndex"
+import {endingIndex as endingIndexFunction, endingIndexByCollectionHolder}       from "./endingIndex"
+import {maximumIndex as maximumIndexFunction, maximumIndexByCollectionHolder}    from "./maximumIndex"
+import {startingIndex as startingIndexFunction, startingIndexByCollectionHolder} from "./startingIndex"
 
 //#region -------------------- Facade method --------------------
 
@@ -39,7 +40,9 @@ export function indexOfLast<const T, >(collection: Nullable<MinimalistCollection
 
     if (collection == null)
         return null
-    if (collection.isEmpty)
+
+    const size = collection.size
+    if (size == 0)
         return null
     if (fromIndex === 0 && toIndex === 0)
         return null
@@ -48,8 +51,6 @@ export function indexOfLast<const T, >(collection: Nullable<MinimalistCollection
 
     //#endregion -------------------- Early returns --------------------
     //#region -------------------- Initialization (starting/ending index) --------------------
-
-    const size = collection.size
 
     const startingIndex = startingIndexFunction(collection, fromIndex, size,)
     if (startingIndex == null)
@@ -66,9 +67,9 @@ export function indexOfLast<const T, >(collection: Nullable<MinimalistCollection
 
     if (limit == null) {
         if (predicate.length == 1)
-            return __withoutALimitAnd1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
+            return __withoutALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
         if (predicate.length >= 2)
-            return __withoutALimitAnd2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate, startingIndex, endingIndex,)
+            return __withoutALimitAnd2Argument(collection, predicate, startingIndex, endingIndex,)
         return __withoutALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex,)
     }
 
@@ -79,9 +80,9 @@ export function indexOfLast<const T, >(collection: Nullable<MinimalistCollection
         return null
     if (maximumIndex == size) {
         if (predicate.length == 1)
-            return __withoutALimitAnd1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
+            return __withoutALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
         if (predicate.length >= 2)
-            return __withoutALimitAnd2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate, startingIndex, endingIndex,)
+            return __withoutALimitAnd2Argument(collection, predicate, startingIndex, endingIndex,)
         return __withoutALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex,)
     }
     if (endingIndex - startingIndex < maximumIndex - 1)
@@ -90,9 +91,81 @@ export function indexOfLast<const T, >(collection: Nullable<MinimalistCollection
     //#endregion -------------------- Initialization (maximum index) --------------------
 
     if (predicate.length == 1)
-        return __withALimitAnd1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (value: T,) => boolean, startingIndex, endingIndex, maximumIndex,)
+        return __withALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex, maximumIndex,)
     if (predicate.length >= 2)
-        return __withALimitAnd2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate, startingIndex, endingIndex, maximumIndex,)
+        return __withALimitAnd2Argument(collection, predicate, startingIndex, endingIndex, maximumIndex,)
+    return __withALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex, maximumIndex,)
+}
+
+/**
+ * Get the last index matching the {@link predicate}
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param predicate  The given predicate
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @param limit      The maximum index
+ * @return {NullOr<number>} The index matching the {@link predicate} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex}, {@link toIndex} and {@link limit} are not within a valid range
+ * @see ReadonlyArray.findLastIndex
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/index-of-last.html Kotlin indexOfLast(predicate)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function indexOfLastByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, predicate: BooleanCallback<T>, fromIndex: Nullable<number> = null, toIndex: Nullable<number> = null, limit: Nullable<number> = null,): NullOr<number> {
+    //#region -------------------- Early returns --------------------
+
+    if (collection == null)
+        return null
+    if (collection.isEmpty)
+        return null
+    if (fromIndex === 0 && toIndex === 0)
+        return null
+    if (limit === 0)
+        return null
+
+    //#endregion -------------------- Early returns --------------------
+    //#region -------------------- Initialization (starting/ending index) --------------------
+
+    const size = collection.size
+
+    const startingIndex = startingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, fromIndex, size,)
+    const endingIndex = endingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, toIndex, size,)
+    if (endingIndex < startingIndex)
+        return null
+
+    //#endregion -------------------- Initialization (starting/ending index) --------------------
+
+    if (limit == null) {
+        if (predicate.length == 1)
+            return __withoutALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
+        if (predicate.length >= 2)
+            return __withoutALimitAnd2Argument(collection, predicate, startingIndex, endingIndex,)
+        return __withoutALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex,)
+    }
+
+    //#region -------------------- Initialization (maximum index) --------------------
+
+    const maximumIndex = maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,)
+    if (maximumIndex == size) {
+        if (predicate.length == 1)
+            return __withoutALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex,)
+        if (predicate.length >= 2)
+            return __withoutALimitAnd2Argument(collection, predicate, startingIndex, endingIndex,)
+        return __withoutALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex,)
+    }
+    if (endingIndex - startingIndex < maximumIndex - 1)
+        return null
+
+    //#endregion -------------------- Initialization (maximum index) --------------------
+
+    if (predicate.length == 1)
+        return __withALimitAnd1Argument(collection, predicate as (value: T,) => boolean, startingIndex, endingIndex, maximumIndex,)
+    if (predicate.length >= 2)
+        return __withALimitAnd2Argument(collection, predicate, startingIndex, endingIndex, maximumIndex,)
     return __withALimitAnd0Argument(predicate as () => boolean, startingIndex, endingIndex, maximumIndex,)
 }
 
@@ -107,7 +180,7 @@ function __withoutALimitAnd0Argument(predicate: () => boolean, startingIndex: nu
     return null
 }
 
-function __withoutALimitAnd1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, startingIndex: number, endingIndex: number,) {
+function __withoutALimitAnd1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, startingIndex: number, endingIndex: number,) {
     let index = endingIndex + 1
     while (--index >= startingIndex)
         if (predicate(collection.get(index,),))
@@ -115,7 +188,7 @@ function __withoutALimitAnd1Argument<const T, >(collection: NonEmptyMinimalistCo
     return null
 }
 
-function __withoutALimitAnd2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, startingIndex: number, endingIndex: number,) {
+function __withoutALimitAnd2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, startingIndex: number, endingIndex: number,) {
     let index = endingIndex + 1
     while (--index >= startingIndex)
         if (predicate(collection.get(index,), index,))
@@ -134,7 +207,7 @@ function __withALimitAnd0Argument(predicate: () => boolean, startingIndex: numbe
     return null
 }
 
-function __withALimitAnd1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, startingIndex: number, endingIndex: number, maximumIndex: number,) {
+function __withALimitAnd1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, startingIndex: number, endingIndex: number, maximumIndex: number,) {
     let index = endingIndex + 1
     if (index >= maximumIndex)
         index = maximumIndex
@@ -144,7 +217,7 @@ function __withALimitAnd1Argument<const T, >(collection: NonEmptyMinimalistColle
     return null
 }
 
-function __withALimitAnd2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, startingIndex: number, endingIndex: number, maximumIndex: number,) {
+function __withALimitAnd2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, startingIndex: number, endingIndex: number, maximumIndex: number,) {
     let index = endingIndex + 1
     if (index >= maximumIndex)
         index = maximumIndex
