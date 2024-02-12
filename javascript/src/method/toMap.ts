81d99a20cd26343d66be830d6adf424e9c5b24e6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:11:49 -0500

    Change and addition of handling on the "toMap" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toMap.ts
+++ javascript/src/method/toMap.ts
@@ -5,10 +5,13 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {CollectionConstants} from "../CollectionConstants"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link ReadonlyMap map}
@@ -19,12 +22,36 @@ import {CollectionConstants} from "../CollectionConstants"
 export function toMap<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): ReadonlyMap<number, T> {
     if (collection == null)
         return CollectionConstants.EMPTY_MAP
+
+    const size = collection.size
+    if (size == 0)
+        return CollectionConstants.EMPTY_MAP
+    return __newMap(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to an {@link ReadonlyMap map}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection} to convert
+ * @extensionFunction
+ */
+export function toMapByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): ReadonlyMap<number, T> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_MAP
     if (collection.isEmpty)
         return CollectionConstants.EMPTY_MAP
+    return __newMap(collection, collection.size,)
+}
 
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __newMap<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const map = new Map<number, T>()
-    let index = collection.size
+    let index = size
     while (index-- > 0)
         map.set(index, collection.get(index,),)
     return Object.freeze(map,)
 }
+
+//#endregion -------------------- Loop method --------------------
