36ad6ddfe7e42562542f0f5757b18461d3f34c2f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:12:38 -0500

    Change and addition of handling on the "toString" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/toString.ts
+++ javascript/src/method/toString.ts
@@ -5,6 +5,7 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
@@ -24,15 +25,40 @@ import {asString} from "./asString"
 export function toString<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): string {
     if (collection == null)
         return "[]"
+
+    const size = collection.size
+    if (size == 0)
+        return "[]"
+    return __toString(collection, size,)
+}
+
+/**
+ * Convert the {@link collection} to a {@link String} on every value
+ * by calling its "<i>{@link Object.toString toString()}</i>" method
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @see Array.toString
+ * @see Object.toString
+ * @extensionFunction
+ */
+export function toStringByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): string {
+    if (collection == null)
+        return "[]"
     if (collection.isEmpty)
         return "[]"
+    return __toString(collection, collection.size,)
+}
 
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop method --------------------
+
+function __toString(collection: MinimalistCollectionHolder, size: number,) {
     let string = ""
-    const sizeMinus1 = collection.size - 1
+    const sizeMinus1 = size - 1
     let index = -1
     while (++index < sizeMinus1)
         string += `${asString(collection.get(index,),)}, `
     return `[${string}${asString(collection.get(index),)}]`
 }
 
-//#endregion -------------------- Facade method --------------------
+//#endregion -------------------- Loop method --------------------
