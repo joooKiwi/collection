fbd2d27c8c537bb0b5bcef5b96016ad7f9e092f2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:08:57 -0500

    Change and addition of handling on the "indexOf" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/indexOf.ts
+++ javascript/src/method/indexOf.ts
@@ -5,13 +5,14 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import type {Nullable, NullOr}                   from "../general type"
-import type {MinimalistCollectionHolder}         from "../MinimalistCollectionHolder"
-import type {NonEmptyMinimalistCollectionHolder} from "../NonEmptyMinimalistCollectionHolder"
+import type {CollectionHolder}           from "../CollectionHolder"
+import type {Nullable, NullOr}           from "../general type"
+import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+import type {NonEmptyCollectionHolder}   from "../NonEmptyCollectionHolder"
 
-import {endingIndex as endingIndexFunction}     from "./endingIndex"
-import {maximumIndex as maximumIndexFunction}   from "./maximumIndex"
-import {startingIndex as startingIndexFunction} from "./startingIndex"
+import {endingIndex as endingIndexFunction, endingIndexByCollectionHolder}       from "./endingIndex"
+import {maximumIndex as maximumIndexFunction, maximumIndexByCollectionHolder}    from "./maximumIndex"
+import {startingIndex as startingIndexFunction, startingIndexByCollectionHolder} from "./startingIndex"
 
 //#region -------------------- Facade method --------------------
 
@@ -60,7 +61,9 @@ export function indexOf(collection: Nullable<MinimalistCollectionHolder>, elemen
 
     if (collection == null)
         return null
-    if (collection.isEmpty)
+
+    const size = collection.size
+    if (size == 0)
         return null
     if (fromIndex === 0 && toIndex === 0)
         return null
@@ -69,8 +72,6 @@ export function indexOf(collection: Nullable<MinimalistCollectionHolder>, elemen
 
     //#endregion -------------------- Early returns --------------------
     //#region -------------------- Initialization (starting/ending index) --------------------
-
-    const size = collection.size
 
     const startingIndex = startingIndexFunction(collection, fromIndex, size,)
     if (startingIndex == null)
@@ -86,7 +87,7 @@ export function indexOf(collection: Nullable<MinimalistCollectionHolder>, elemen
     //#endregion -------------------- Initialization (starting/ending index) --------------------
 
     if (limit == null)
-        return __withoutALimit(collection as NonEmptyMinimalistCollectionHolder, element, startingIndex, endingIndex,)
+        return __withoutALimit(collection, element, startingIndex, endingIndex,)
 
     //#region -------------------- Initialization (maximum index) --------------------
 
@@ -94,19 +95,100 @@ export function indexOf(collection: Nullable<MinimalistCollectionHolder>, elemen
     if (maximumIndex == null)
         return null
     if (maximumIndex == size)
-        return __withoutALimit(collection as NonEmptyMinimalistCollectionHolder, element, startingIndex, endingIndex,)
+        return __withoutALimit(collection, element, startingIndex, endingIndex,)
     if (endingIndex - startingIndex < maximumIndex - 1)
         return null
 
     //#endregion -------------------- Initialization (maximum index) --------------------
 
-    return __withALimit(collection as NonEmptyMinimalistCollectionHolder, element, startingIndex, endingIndex, maximumIndex,)
+    return __withALimit(collection, element, startingIndex, endingIndex, maximumIndex,)
+}
+
+/**
+ * Get the <b>first</b> occurrence equivalent to the value received
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param element    The element to find
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @param limit      The maximum index
+ * @return {NullOr<number>} The index associated to the {@link element} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex}, {@link toIndex} and {@link limit} are not within a valid range
+ * @see ReadonlyArray.indexOf
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-list/index-of.html Kotlin indexOf(element)
+ * @see https://learn.microsoft.com/dotnet/api/system.collections.generic.list-1.indexof C# IndexOf(item, fromIndex?, limit?)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function indexOfByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, element: T, fromIndex?: Nullable<number>, toIndex?: Nullable<number>, limit?: Nullable<number>,): NullOr<number>
+/**
+ * Get the <b>first</b> occurrence equivalent to the value received
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param element    The element to find
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @param limit      The maximum index
+ * @return {NullOr<number>} The index associated to the {@link element} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex}, {@link toIndex} and {@link limit} are not within a valid range
+ * @see ReadonlyArray.indexOf
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-list/index-of.html Kotlin indexOf(element)
+ * @see https://learn.microsoft.com/dotnet/api/system.collections.generic.list-1.indexof C# IndexOf(item, fromIndex?, limit?)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function indexOfByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, element: unknown, fromIndex?: Nullable<number>, toIndex?: Nullable<number>, limit?: Nullable<number>,): NullOr<number>
+export function indexOfByCollectionHolder(collection: Nullable<CollectionHolder>, element: unknown, fromIndex: Nullable<number> = null, toIndex: Nullable<number> = null, limit: Nullable<number> = null,): NullOr<number> {
+    //#region -------------------- Early returns --------------------
+
+    if (collection == null)
+        return null
+    if (collection.isEmpty)
+        return null
+    if (fromIndex === 0 && toIndex === 0)
+        return null
+    if (limit === 0)
+        return null
+
+    //#endregion -------------------- Early returns --------------------
+    //#region -------------------- Initialization (starting/ending index) --------------------
+
+    const size = collection.size
+
+    const startingIndex = startingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, fromIndex, size,)
+    const endingIndex = endingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, toIndex, size,)
+
+    if (endingIndex < startingIndex)
+        return null
+
+    //#endregion -------------------- Initialization (starting/ending index) --------------------
+
+    if (limit == null)
+        return __withoutALimit(collection, element, startingIndex, endingIndex,)
+
+    //#region -------------------- Initialization (maximum index) --------------------
+
+    const maximumIndex = maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,)
+    if (maximumIndex == size)
+        return __withoutALimit(collection, element, startingIndex, endingIndex,)
+    if (endingIndex - startingIndex < maximumIndex - 1)
+        return null
+
+    //#endregion -------------------- Initialization (maximum index) --------------------
+
+    return __withALimit(collection, element, startingIndex, endingIndex, maximumIndex,)
 }
 
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
-function __withoutALimit(collection: NonEmptyMinimalistCollectionHolder, element: unknown, startingIndex: number, endingIndex: number,): NullOr<number> {
+function __withoutALimit(collection: MinimalistCollectionHolder, element: unknown, startingIndex: number, endingIndex: number,) {
     let index = startingIndex - 1
     while (++index <= endingIndex)
         if (collection.get(index,) === element)
@@ -114,7 +196,7 @@ function __withoutALimit(collection: NonEmptyMinimalistCollectionHolder, element
     return null
 }
 
-function __withALimit(collection: NonEmptyMinimalistCollectionHolder, element: unknown, startingIndex: number, endingIndex: number, maximumIndex: number,): NullOr<number> {
+function __withALimit(collection: MinimalistCollectionHolder, element: unknown, startingIndex: number, endingIndex: number, maximumIndex: number,) {
     let index = startingIndex - 1
     while (++index <= endingIndex)
         if (index >= maximumIndex)
