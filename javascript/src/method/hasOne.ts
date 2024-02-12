d7f2c38ba6ec45895c3b504395add7b2d59d454e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:08:39 -0500

    Change and addition of handling on the "hasOne" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/hasOne.ts
+++ javascript/src/method/hasOne.ts
@@ -5,8 +5,11 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Tell whenever at least one value exist in the {@link collection}
@@ -35,8 +38,47 @@ export function hasOne<const T, >(collection: Nullable<MinimalistCollectionHolde
  */
 export function hasOne(collection: Nullable<MinimalistCollectionHolder>, ...values: readonly unknown[]): boolean
 export function hasOne(collection: Nullable<MinimalistCollectionHolder>, ...values: readonly unknown[]): boolean {
-    //#region -------------------- Early returns --------------------
+    if (collection == null)
+        return false
 
+    const size = collection.size
+    if (size == 0)
+        return false
+
+    const valuesSize = values.length
+    if (valuesSize == 0)
+        return true
+
+    return __hasOne(collection, values, size, valuesSize,)
+}
+
+/**
+ * Tell whenever at least one value exist in the {@link collection}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param values     The values to compare
+ * @return {boolean} <b>true</b> if at least one value is equals to one value in the {@link collection}
+ * @see ReadonlyArray.includes
+ * @see ReadonlySet.has
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-list/contains.html Kotlin contains(element)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.contains C# contains(value)
+ * @extensionFunction
+ */
+export function hasOneByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, ...values: readonly T[]): boolean
+/**
+ * Tell whenever at least one value exist in the {@link collection}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param values     The values to compare
+ * @return {boolean} <b>true</b> if at least one value is equals to one value in the {@link collection}
+ * @see ReadonlyArray.includes
+ * @see ReadonlySet.has
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-list/contains.html Kotlin contains(element)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.contains C# contains(value)
+ * @extensionFunction
+ */
+export function hasOneByCollectionHolder(collection: Nullable<CollectionHolder>, ...values: readonly unknown[]): boolean
+export function hasOneByCollectionHolder(collection: Nullable<CollectionHolder>, ...values: readonly unknown[]): boolean {
     if (collection == null)
         return false
     if (collection.isEmpty)
@@ -46,9 +88,13 @@ export function hasOne(collection: Nullable<MinimalistCollectionHolder>, ...valu
     if (valuesSize == 0)
         return true
 
-    //#endregion -------------------- Early returns --------------------
+    return __hasOne(collection, values, collection.size, valuesSize,)
+}
 
-    const size = collection.size
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __hasOne(collection: MinimalistCollectionHolder, values: readonly unknown[], size: number, valuesSize: number,) {
     let valueIndex = -1
     while (++valueIndex < valuesSize) {
         let index = -1
@@ -59,3 +105,5 @@ export function hasOne(collection: Nullable<MinimalistCollectionHolder>, ...valu
     }
     return false
 }
+
+//#endregion -------------------- Loop methods --------------------
