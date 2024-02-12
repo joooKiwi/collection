f3a0b9b296cb0dc490089568ef2e3cea1665627b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-12 11:10:43 -0500

    Change to the "indexOfFirstIndexed" extension function to internally utilize the index retriever in its file to be independent of the other methods

--- javascript/src/method/indexOfFirstIndexed.ts
+++ javascript/src/method/indexOfFirstIndexed.ts
@@ -9,11 +9,8 @@ import type {CollectionHolder}           from "../CollectionHolder"
 import type {ReverseBooleanCallback}     from "../CollectionHolder.types"
 import type {Nullable, NullOr}           from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
-import type {NonEmptyCollectionHolder}   from "../NonEmptyCollectionHolder"
 
-import {endingIndex as endingIndexFunction, endingIndexByCollectionHolder}       from "./endingIndex"
-import {maximumIndex as maximumIndexFunction, maximumIndexByCollectionHolder}    from "./maximumIndex"
-import {startingIndex as startingIndexFunction, startingIndexByCollectionHolder} from "./startingIndex"
+import {CollectionHolderIndexOutOfBoundsException} from "../exception/CollectionHolderIndexOutOfBoundsException"
 
 //#region -------------------- Facade method --------------------
 
@@ -52,14 +49,8 @@ export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCo
     //#endregion -------------------- Early returns --------------------
     //#region -------------------- Initialization (starting/ending index) --------------------
 
-    const startingIndex = startingIndexFunction(collection, fromIndex, size,)
-    if (startingIndex == null)
-        return null
-
-    const endingIndex = endingIndexFunction(collection, toIndex, size,)
-    if (endingIndex == null)
-        return null
-
+    const startingIndex = __startingIndex(fromIndex, size,)
+    const endingIndex = __endingIndex(toIndex, size,)
     if (endingIndex < startingIndex)
         return null
 
@@ -75,9 +66,7 @@ export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCo
 
     //#region -------------------- Initialization (maximum index) --------------------
 
-    const maximumIndex = maximumIndexFunction(collection, limit, size,)
-    if (maximumIndex == null)
-        return null
+    const maximumIndex = __maximumIndex(limit, size,)
     if (maximumIndex == size) {
         if (predicate.length == 1)
             return __withoutALimitAnd1Argument(predicate as (index: number,) => boolean, startingIndex, endingIndex,)
@@ -131,9 +120,8 @@ export function indexOfFirstIndexedByCollectionHolder<const T, >(collection: Nul
     //#region -------------------- Initialization (starting/ending index) --------------------
 
     const size = collection.size
-
-    const startingIndex = startingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, fromIndex, size,)
-    const endingIndex = endingIndexByCollectionHolder(collection as NonEmptyCollectionHolder, toIndex, size,)
+    const startingIndex = __startingIndex(fromIndex, size,)
+    const endingIndex = __endingIndex(toIndex, size,)
     if (endingIndex < startingIndex)
         return null
 
@@ -149,7 +137,7 @@ export function indexOfFirstIndexedByCollectionHolder<const T, >(collection: Nul
 
     //#region -------------------- Initialization (maximum index) --------------------
 
-    const maximumIndex = maximumIndexByCollectionHolder(collection as NonEmptyCollectionHolder, limit, size,)
+    const maximumIndex = __maximumIndex(limit, size,)
     if (maximumIndex == size) {
         if (predicate.length == 1)
             return __withoutALimitAnd1Argument(predicate as (index: number,) => boolean, startingIndex, endingIndex,)
@@ -170,6 +158,60 @@ export function indexOfFirstIndexedByCollectionHolder<const T, >(collection: Nul
 }
 
 //#endregion -------------------- Facade method --------------------
+//#region -------------------- Utility methods --------------------
+
+function __startingIndex(fromIndex: Nullable<number>, size: number,) {
+    if (fromIndex == null)
+        return 0
+
+    if (fromIndex == size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The starting index "${fromIndex}" is the collection size "${size}".`, fromIndex,)
+    if (fromIndex > size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The starting index "${fromIndex}" is over the collection size "${size}".`, fromIndex,)
+
+    let startingIndex = fromIndex
+    if (startingIndex < 0)
+        startingIndex += size
+    if (startingIndex == size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The starting index "${fromIndex}" ("${startingIndex}" after calculation) is the collection size "${size}".`, fromIndex,)
+    if (startingIndex < 0)
+        throw new CollectionHolderIndexOutOfBoundsException(`The starting index "${fromIndex}" ("${startingIndex}" after calculation) is under 0.`, fromIndex,)
+    return startingIndex
+}
+
+function __endingIndex(toIndex: Nullable<number>, size: number,) {
+    if (toIndex == null)
+        return size - 1
+
+    if (toIndex == size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The ending index "${toIndex}" is the collection size "${size}".`, toIndex,)
+
+    let endingIndex = toIndex
+    if (endingIndex < 0)
+        endingIndex += size
+    if (endingIndex < 0)
+        throw new CollectionHolderIndexOutOfBoundsException(`The ending index "${toIndex}" ("${endingIndex}" after calculation) is under 0.`, toIndex,)
+    if (endingIndex == size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The ending index "${toIndex}" ("${endingIndex}" after calculation) is the collection size "${size}".`, toIndex,)
+    if (endingIndex > size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The ending index "${toIndex}" ("${endingIndex}" after calculation) is over the collection size "${size}".`, toIndex,)
+    return endingIndex
+}
+
+function __maximumIndex(limit: number, size: number,) {
+    if (limit > size)
+        throw new CollectionHolderIndexOutOfBoundsException(`The limit "${limit}" cannot over the collection size "${size}".`, limit,)
+
+    let maximumIndex = limit
+    if (maximumIndex < 0)
+        maximumIndex += size
+    if (maximumIndex < 0)
+        throw new CollectionHolderIndexOutOfBoundsException(`The limit "${limit}" ("${maximumIndex}" after calculation) cannot under 0.`, limit,)
+
+    return maximumIndex
+}
+
+//#endregion -------------------- Utility methods --------------------
 //#region -------------------- Loop methods --------------------
 
 function __withoutALimitAnd0Argument(predicate: () => boolean, startingIndex: number, endingIndex: number,) {
