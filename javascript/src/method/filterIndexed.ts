0f77e01a36f9353aa69dc3657728ad085c078b5c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:07:14 -0500

    Change and addition of handling on the "filterIndexed" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/filterIndexed.ts
+++ javascript/src/method/filterIndexed.ts
@@ -9,7 +9,6 @@ import type {CollectionHolder}                                         from "../
 import type {ReverseBooleanCallback, ReverseRestrainedBooleanCallback} from "../CollectionHolder.types"
 import type {Nullable}                                                 from "../general type"
 import type {MinimalistCollectionHolder}                               from "../MinimalistCollectionHolder"
-import type {NonEmptyMinimalistCollectionHolder}                       from "../NonEmptyMinimalistCollectionHolder"
 
 import {CollectionConstants} from "../CollectionConstants"
 
@@ -45,22 +44,63 @@ export function filterIndexed<const T, >(collection: Nullable<MinimalistCollecti
 export function filterIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>,) {
     if (collection == null)
         return CollectionConstants.EMPTY_COLLECTION_HOLDER
+
+    const size = collection.size
+    if (size == 0)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+
+    if (predicate.length == 1)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with1Argument(collection, predicate as (index: number,) => boolean, size,),)
+    if (predicate.length >= 2)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with2Argument(collection, predicate, size,),)
+    return new CollectionConstants.LazyGenericCollectionHolder(() => __with0Argument(collection, predicate as () => boolean, size,),)
+}
+
+/**
+ * Get a new {@link CollectionHolder}
+ * matching only the given {@link predicate}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.filter
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/filter-indexed.html Kotlin filterIndexed(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.where C# Where(predicate)
+ * @see filterIndexedNot
+ * @typescriptDefinition
+ * @extensionFunction
+ */
+export function filterIndexedByCollectionHolder<const T, const S extends T, >(collection: Nullable<CollectionHolder<T>>, predicate: ReverseRestrainedBooleanCallback<T, S>,): CollectionHolder<S>
+/**
+ * Get a new {@link CollectionHolder}
+ * matching only the given {@link predicate}
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.filter
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/filter-indexed.html Kotlin filterIndexed(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.where C# Where(predicate)
+ * @see filterIndexedNot
+ * @extensionFunction
+ */
+export function filterIndexedByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, predicate: ReverseBooleanCallback<T>,): CollectionHolder<T>
+export function filterIndexedByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>, predicate: ReverseBooleanCallback<T>,) {
+    if (collection == null)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
     if (collection.isEmpty)
         return CollectionConstants.EMPTY_COLLECTION_HOLDER
 
     if (predicate.length == 1)
-        return new CollectionConstants.LazyGenericCollectionHolder(() => __with1Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as (index: number,) => boolean,),)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with1Argument(collection, predicate as (index: number,) => boolean, collection.size,),)
     if (predicate.length >= 2)
-        return new CollectionConstants.LazyGenericCollectionHolder(() => __with2Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate,),)
-    return new CollectionConstants.LazyGenericCollectionHolder(() => __with0Argument(collection as NonEmptyMinimalistCollectionHolder<T>, predicate as () => boolean,),)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with2Argument(collection, predicate, collection.size,),)
+    return new CollectionConstants.LazyGenericCollectionHolder(() => __with0Argument(collection, predicate as () => boolean, collection.size,),)
 }
 
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
-function __with0Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: () => boolean,) {
+function __with0Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: () => boolean, size: number,) {
     const newArray = [] as T[]
-    const size = collection.size
     let index = -1
     while (++index < size)
         if (predicate())
@@ -68,9 +108,8 @@ function __with0Argument<const T, >(collection: NonEmptyMinimalistCollectionHold
     return newArray
 }
 
-function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (index: number,) => boolean,) {
+function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (index: number,) => boolean, size: number,) {
     const newArray = [] as T[]
-    const size = collection.size
     let index = -1
     while (++index < size)
         if (predicate(index,))
@@ -78,9 +117,8 @@ function __with1Argument<const T, >(collection: NonEmptyMinimalistCollectionHold
     return newArray
 }
 
-function __with2Argument<const T, >(collection: NonEmptyMinimalistCollectionHolder<T>, predicate: (index: number, value: T,) => boolean,) {
+function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (index: number, value: T,) => boolean, size: number,) {
     const newArray = [] as T[]
-    const size = collection.size
     let index = -1
     while (++index < size) {
         const value = collection.get(index,)
