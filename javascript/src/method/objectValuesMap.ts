fb0ef3d785c03ee73fee576c507b8a9e4a39fe71
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-11 19:10:51 -0500

    Change and addition of handling on the "objectValuesMap" extension method for a minimalist and non-minimalist collection

--- javascript/src/method/objectValuesMap.ts
+++ javascript/src/method/objectValuesMap.ts
@@ -5,11 +5,14 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
+import type {CollectionHolder}           from "../CollectionHolder"
 import type {ObjectOf}                   from "../CollectionHolder.types"
 import type {Nullable}                   from "../general type"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {CollectionConstants} from "../CollectionConstants"
+
+//#region -------------------- Facade method --------------------
 
 /**
  * Get an every object associated to the typed value
@@ -18,15 +21,38 @@ import {CollectionConstants} from "../CollectionConstants"
  */
 export function objectValuesMap<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): ReadonlyMap<T, ObjectOf<T>> {
     if (collection == null)
-        return CollectionConstants.EMPTY_MAP as ReadonlyMap<T, ObjectOf<T>>
-    if (collection.isEmpty)
-        return CollectionConstants.EMPTY_MAP as ReadonlyMap<T, ObjectOf<T>>
+        return CollectionConstants.EMPTY_MAP
 
+    const size = collection.size
+    if (size == 0)
+        return CollectionConstants.EMPTY_MAP
+    return __newMap(collection, size,)
+}
+
+/**
+ * Get an every object associated to the typed value
+ *
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
+ */
+export function objectValuesMapByCollectionHolder<const T, >(collection: Nullable<CollectionHolder<T>>,): ReadonlyMap<T, ObjectOf<T>> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_MAP
+    if (collection.isEmpty)
+        return CollectionConstants.EMPTY_MAP
+    return __newMap(collection, collection.size,)
+}
+
+//#endregion -------------------- Facade method --------------------
+//#region -------------------- Loop methods --------------------
+
+function __newMap<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const map = new Map<T, ObjectOf<T>>
-    let index = collection.size
+    let index = size
     while (index-- > 0) {
         const value = collection.get(index,)
         map.set(value, Object(value,),)
     }
     return Object.freeze(map,)
 }
+
+//#endregion -------------------- Loop methods --------------------
