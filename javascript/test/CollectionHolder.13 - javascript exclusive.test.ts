841a582c545a1dad4b7b5c12548c081d18470f14
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-15 12:23:42 -0500

    Addition of tests about empty collections

--- javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
+++ javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
@@ -5,12 +5,16 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import {AB}            from "./constantCollections"
+import {AB, EMPTY}     from "./constantCollections"
 import {everyInstance} from "./constantValues"
+
+import {CollectionConstants} from "../src/CollectionConstants"
 
 describe("CollectionHolderTest (javascript exclusive)", () => {
 describe.each(everyInstance,)("%s", ({value: {newInstance,},},) => {
     describe("Symbol.iterator", () => {
+        test("empty",     () => expect(newInstance(EMPTY,)[Symbol.iterator](),).toBe(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
+
         const iterator = newInstance(AB,)[Symbol.iterator]()
         test("1st: a",    () => expect(iterator.next().value,).toBe('a',),)
         test("2nd: b",    () => expect(iterator.next().value,).toBe('b',),)
--- javascript/test/CollectionHolder.14 - conversion.test.ts
+++ javascript/test/CollectionHolder.14 - conversion.test.ts
@@ -5,12 +5,16 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import {AB, AB_OBJECT, SINGULAR_A_OBJECT, SINGULAR_B_OBJECT} from "./constantCollections"
-import {everyInstance}                                       from "./constantValues"
+import {AB, AB_AB, AB_OBJECT, EMPTY, SINGULAR_A_OBJECT, SINGULAR_B_OBJECT} from "./constantCollections"
+import {everyInstance}                                                     from "./constantValues"
+
+import {CollectionConstants} from "../src/CollectionConstants"
 
 describe("CollectionHolderTest (conversion)", () => {
 describe.each(everyInstance,)("%s", ({value: {newInstance, isMinimalist,},},) => {
     describe("to iterator", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toIterator(),).toBe(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
+
         const iterator = newInstance(AB,).toIterator()
         test("1st: a",    () => expect(iterator.next().value,).toBe('a',),)
         test("2nd: b",    () => expect(iterator.next().value,).toBe('b',),)
@@ -18,22 +22,26 @@ describe.each(everyInstance,)("%s", ({value: {newInstance, isMinimalist,},},) =>
     })
 
     describe("to array", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toArray(),).toBe(CollectionConstants.EMPTY_ARRAY,),)
         test("basic",  () => expect(newInstance(AB,).toArray(),).toEqual(['a', 'b',],),)
         test("frozen", () => expect(newInstance(AB,).toArray(),).toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toArray(),).toHaveLength(2,),)
     },)
     describe("to mutable array", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toMutableArray(),).toEqual([],),)
         test("basic",  () => expect(newInstance(AB,).toMutableArray(),).toEqual(['a', 'b',],),)
         test("frozen", () => expect(newInstance(AB,).toMutableArray(),).not.toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toMutableArray(),).toHaveLength(2,),)
     },)
 
     describe("to set", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toSet(),).toBe(CollectionConstants.EMPTY_SET,),)
         test("basic",  () => expect(newInstance(AB,).toSet(),).toEqual(new Set(['a', 'b',],),),)
         test("frozen", () => expect(newInstance(AB,).toSet(),).toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toSet().size,).toBe(2,),)
     },)
     describe("to mutable set", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toMutableSet(),).toEqual(new Set(),),)
         test("basic",  () => expect(newInstance(AB,).toMutableSet(),).toEqual(new Set(['a', 'b',],),),)
         test("frozen", () => expect(newInstance(AB,).toMutableSet(),).not.toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toMutableSet().size,).toBe(2,),)
@@ -41,21 +49,25 @@ describe.each(everyInstance,)("%s", ({value: {newInstance, isMinimalist,},},) =>
 
     if (!isMinimalist) {
         describe("to weak set", () => {
-            test("basic", () => expect(newInstance(AB_OBJECT,).toWeakSet(),).toEqual(new WeakSet([SINGULAR_A_OBJECT, SINGULAR_B_OBJECT,],),),)
+            test("empty",  () => expect(newInstance(EMPTY,).toWeakSet(),).toBe(CollectionConstants.EMPTY_WEAK_SET,),)
+            test("basic",  () => expect(newInstance(AB_OBJECT,).toWeakSet(),).toEqual(new WeakSet([SINGULAR_A_OBJECT, SINGULAR_B_OBJECT,],),),)
             test("frozen", () => expect(newInstance(AB,).toWeakSet(),).toBeFrozen(),)
         },)
         describe("to mutable weak set", () => {
-            test("basic", () => expect(newInstance(AB_OBJECT,).toMutableWeakSet(),).toEqual(new WeakSet([SINGULAR_A_OBJECT, SINGULAR_B_OBJECT,],),),)
+            test("empty",  () => expect(newInstance(EMPTY,).toMutableWeakSet(),).toEqual(new WeakSet(),),)
+            test("basic",  () => expect(newInstance(AB_OBJECT,).toMutableWeakSet(),).toEqual(new WeakSet([SINGULAR_A_OBJECT, SINGULAR_B_OBJECT,],),),)
             test("frozen", () => expect(newInstance(AB,).toMutableWeakSet(),).not.toBeFrozen(),)
         },)
     }
 
     describe("to map", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toMap(),).toBe(CollectionConstants.EMPTY_MAP,),)
         test("basic",  () => expect(newInstance(AB,).toMap(),).toEqual(new Map([[0, 'a',], [1, 'b',],],),),)
         test("frozen", () => expect(newInstance(AB,).toMap(),).toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toMap().size,).toBe(2,),)
     },)
     describe("to mutable map", () => {
+        test("empty",  () => expect(newInstance(EMPTY,).toMutableMap(),).toEqual(new Map(),),)
         test("basic",  () => expect(newInstance(AB,).toMutableMap(),).toEqual(new Map([[0, 'a',], [1, 'b',],],),),)
         test("frozen", () => expect(newInstance(AB,).toMutableMap(),).not.toBeFrozen(),)
         test("size",   () => expect(newInstance(AB,).toMutableMap().size,).toBe(2,),)
