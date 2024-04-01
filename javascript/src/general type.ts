6fa54e19d30760ac1849d70369e1ebf558208092
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-03-31 23:33:59 -0400

    [Javascript] Addition of deprecation to the types that are no longer useful

--- javascript/src/general type.ts
+++ javascript/src/general type.ts
@@ -5,29 +5,37 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-//TODO move in a separate project
-
 /**
  * An encapsulation type toward a specified type to be the value, <b>null</b>
  *
  * @see Nullable
+ * @deprecated Use NullOr from "@joookiwi/type" instead. It will be removed in version 1.9.
  */
 export type NullOr<T, > = | T | null
 /**
  * An encapsulation type toward a specified type to be the value, <b>undefined</b>
  *
  * @see Nullable
+ * @deprecated Use UndefinedOr from "@joookiwi/type" instead. It will be removed in version 1.9.
  */
 export type UndefinedOr<T, > = | T | undefined
 /**
  * An encapsulation type toward a specified type to be the value, <b>null</b> or <b>undefined</b>
  *
  * @see NullOr
+ * @see UndefinedOr
+ * @deprecated Use Nullable from "@joookiwi/type" instead. It will be removed in version 1.9.
  */
 export type Nullable<T, > = | T | null | undefined
 
-/** A type for the {@link Number} as itself or included in a {@link String} directly */
-export type NumberOrNumberInString = | number | `${number}`
+/**
+ * A type for the {@link Number} as itself or included in a {@link String} directly
+ * @deprecated Use The TemplateOrNumber from "@joookiwi/type" instead. It will be removed in version 1.9.
+ */
+export type NumberOrNumberInString<T extends number = number, > = | T | `${T}`
 
-/** A type for the possible numeric data ({@link Number} or {@link BigInt} */
-export type PossibleNumeric = | number | Number | bigint | BigInt
+/**
+ * A type for the possible numeric data ({@link Number} or {@link BigInt}
+ * @deprecated Use The NumericOrObject from "@joookiwi/type" instead. It will be removed in version 1.9.
+ */
+export type PossibleNumeric<T extends | number | bigint = | number | bigint, > = | T | Number | BigInt
