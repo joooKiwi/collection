ae00bb4c265d705c2bb2ab1ccbaf18273dc03c49
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-06-24 15:46:01 -0400

    [Javascript] Addition of isolated tests and test on the "Symbol.toStringTag"

--- javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
+++ javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
@@ -12,12 +12,39 @@ import {CollectionConstants} from "../src/CollectionConstants"
 
 describe("CollectionHolderTest (javascript exclusive)", () => {
 describe.each(everyInstance,)("%s", ({value: {newInstance,},},) => {
+    describe("Symbol.toStringTag", () => expect(newInstance(EMPTY,)[Symbol.toStringTag],).toBe(CollectionConstants.COLLECTION_HOLDER_TO_STRING_TAG,),)
     describe("Symbol.iterator", () => {
         test("empty",     () => expect(newInstance(EMPTY,)[Symbol.iterator](),).toBe(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
 
-        const iterator = newInstance(AB,)[Symbol.iterator]()
-        test("1st: a",    () => expect(iterator.next().value,).toBe('a',),)
-        test("2nd: b",    () => expect(iterator.next().value,).toBe('b',),)
-        test("3rd: done", () => expect(iterator.next().done,).toBe(true,),)
+        const newIterator = () => newInstance(AB,)[Symbol.iterator]()
+        test("1st", () => {
+            const value = newIterator().next()
+            expect(value.value,).toBe("a",)
+            expect(value.done,).toBeFalse()
+        },)
+        test("2nd", () => {
+            const iterator = newIterator()
+            iterator.next()
+            const value = iterator.next()
+            expect(value.value,).toBe("b",)
+            expect(value.done,).toBeFalse()
+        },)
+        test("3rd", () => {
+            const iterator = newIterator()
+            iterator.next()
+            iterator.next()
+            const value = iterator.next()
+            expect(value.value,).toBe(CollectionConstants.AFTER_LAST_VALUE_IN_ITERATOR_SYMBOL,)
+            expect(value.done,).toBeTrue()
+        },)
+        test("4th", () => {
+            const iterator = newIterator()
+            iterator.next()
+            iterator.next()
+            iterator.next()
+            const value = iterator.next()
+            expect(value.value,).toBe(CollectionConstants.AFTER_LAST_VALUE_IN_ITERATOR_SYMBOL,)
+            expect(value.done,).toBeTrue()
+        },)
     },)
 },)},)
