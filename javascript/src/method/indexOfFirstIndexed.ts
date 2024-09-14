c8345330d15a4e512b28527b299365a13daa56b6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-09-14 16:52:06 -0400

    [Javascript] Change to the extension methods "indexOfFirstIndexed" to add a differentiation for the "MinimalistCollectionHolder"

--- javascript/src/method/indexOfFirstIndexed.ts
+++ javascript/src/method/indexOfFirstIndexed.ts
@@ -12,8 +12,40 @@ import type {ReverseBooleanCallback}     from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {__endingIndex, __maximumIndex, __startingIndex} from "./_indexes utility"
+import {isCollectionHolder}                             from "./isCollectionHolder"
+import {isCollectionHolderByStructure}                  from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
+
+/**
+ * Get the first index matching the {@link predicate}
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param predicate  The given predicate
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @param limit      The maximum index
+ * @return {NullOrNumber} The index matching the {@link predicate} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex}, {@link toIndex} and {@link limit} are not within a valid range
+ * @throws ForbiddenIndexException                   The {@link fromIndex}, {@link toIndex} or {@link limit} are a forbidden {@link Number} (±∞ / {@link Number.NaN NaN})
+ * @see ReadonlyArray.findIndex
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/index-of-first.html Kotlin indexOfFirst(predicate)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null, limit: NullableNumber = null,): NullOrNumber {
+    if (collection == null)
+        return null
+    if (isCollectionHolder<T>(collection,))
+        return indexOfFirstIndexedByCollectionHolder(collection, predicate, fromIndex, toIndex, limit,)
+    if (isCollectionHolderByStructure<T>(collection,))
+        return indexOfFirstIndexedByCollectionHolder(collection, predicate, fromIndex, toIndex, limit,)
+    return indexOfFirstIndexedByMinimalistCollectionHolder(collection, predicate, fromIndex, toIndex, limit,)
+}
+
 
 /**
  * Get the first index matching the {@link predicate}
@@ -34,7 +66,7 @@ import {__endingIndex, __maximumIndex, __startingIndex} from "./_indexes utility
  * @onlyGivePositiveValue
  * @extensionFunction
  */
-export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null, limit: NullableNumber = null,): NullOrNumber {
+export function indexOfFirstIndexedByMinimalistCollectionHolder<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null, limit: NullableNumber = null,): NullOrNumber {
     //#region -------------------- Early returns --------------------
 
     if (collection == null)
