845b529796d7d0a5ffd4e61f452eda23a337a4c5
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-09-27 13:14:28 -0400

    [Javascript] Restructuration of the "javascript exclusive" tests

--- javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
+++ javascript/test/CollectionHolder.13 - javascript exclusive.test.ts
@@ -5,17 +5,26 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import {AB, EMPTY}     from "./constantCollections"
-import {everyInstance} from "./constantValues"
+import {EmptyCollectionHolderForTest}               from "./instance/EmptyCollectionHolderForTest"
+import {AB, EMPTY}                                  from "./value/arrays"
+import {everyCollectionInstancesAsCollectionHolder} from "./value/instances"
 
 import {CollectionConstants}       from "../src/CollectionConstants"
 import {GenericCollectionIterator} from "../src/iterator/GenericCollectionIterator"
 
 describe("CollectionHolderTest (javascript exclusive)", () => {
-describe.each(everyInstance,)("%s", ({value: {newInstance,},},) => {
-    describe("Symbol.toStringTag", () => expect(newInstance(EMPTY,)[Symbol.toStringTag],).toBe(CollectionConstants.COLLECTION_HOLDER_TO_STRING_TAG,),)
-    describe("Symbol.iterator", () => {
-        test("empty",     () => expect(newInstance(EMPTY,)[Symbol.iterator](),).toBe(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
-        test("non-empty", () => expect(newInstance(AB,)[Symbol.iterator](),).toBeInstanceOf(GenericCollectionIterator,),)
+
+    describe("EmptyCollectionHolder", () => {
+        test("Symbol.toStringTag", () => expect(new EmptyCollectionHolderForTest()[Symbol.toStringTag],).toEqual(CollectionConstants.COLLECTION_HOLDER_TO_STRING_TAG,),)
+        test("Symbol.iterator",    () => expect(new EmptyCollectionHolderForTest()[Symbol.iterator](),).toEqual(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
     },)
-},)},)
+
+    describe.each(everyCollectionInstancesAsCollectionHolder,)("%s", ({value: {instance,},},) => {
+        describe("Symbol.toStringTag", () => expect(new instance(EMPTY,)[Symbol.toStringTag],).toBe(CollectionConstants.COLLECTION_HOLDER_TO_STRING_TAG,),)
+        describe("Symbol.iterator", () => {
+            test("empty",     () => expect(new instance(EMPTY,)[Symbol.iterator](),).toBe(CollectionConstants.EMPTY_COLLECTION_ITERATOR,),)
+            test("non-empty", () => expect(new instance(AB,)[Symbol.iterator](),).toBeInstanceOf(GenericCollectionIterator,),)
+        },)
+    },)
+
+},)
