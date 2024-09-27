ceda8985a3667fe62b80fa3d65049b178d07b8f4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-09-27 13:18:28 -0400

    [Javascript] Restructuration on the "forEach" tests of the CollectionIterator

--- javascript/test/CollectionIterator.03 - forEach.test.ts
+++ javascript/test/CollectionIterator.03 - forEach.test.ts
@@ -5,138 +5,126 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-import {GenericCollectionIterator} from "../src/iterator/GenericCollectionIterator"
+import {CollectionHolderFromArray}                     from "./instance/CollectionHolderFromArray"
+import {EmptyCollectionIterator_ForEachWatcher}        from "./instance/EmptyCollectionIterator_ForEachWatcher"
+import {EmptyCollectionIterator_ForEachIndexedWatcher} from "./instance/EmptyCollectionIterator_ForEachIndexedWatcher"
+import {A, AB, EMPTY}                                  from "./value/arrays"
 
-import {A, AB, EMPTY}                                        from "./constantCollections"
-import {everyCollectionInstanceByIterable, iterableCreation} from "./constantValues"
-import {newEmptyIterator}                                    from "./newEmptyIterator"
+import {GenericCollectionIterator} from "../src/iterator/GenericCollectionIterator"
 
 describe("CollectionIteratorTest (forEach)", () => {
 
     describe("EmptyCollectionIterator", () => {
-        test("forEach", () => {
-            const callback = jest.fn()
-
-            newEmptyIterator().forEach(callback,)
-            expect(callback,).not.toHaveBeenCalled()
-        },)
-        test("forEachIndexed", () => {
-            const callback = jest.fn()
-
-            newEmptyIterator().forEachIndexed(callback,)
-            expect(callback,).not.toHaveBeenCalled()
-        },)
+        test("forEach",        () => expect(new EmptyCollectionIterator_ForEachWatcher().execute(it => it.forEach(),).amountOfCall,).toBe(1,),)
+        test("forEachIndexed", () => expect(new EmptyCollectionIterator_ForEachIndexedWatcher().execute(it => it.forEachIndexed(),).amountOfCall,).toBe(1,),)
     },)
+
     describe("GenericCollectionIterator", () => {
         const EMPTY_CALLBACK = () => {}
+        const newInstanceOf0 = () => new GenericCollectionIterator(new CollectionHolderFromArray(EMPTY,),)
+        const newInstanceOf1 = () => new GenericCollectionIterator(new CollectionHolderFromArray(A,),)
+        const newInstanceOf2 = () => new GenericCollectionIterator(new CollectionHolderFromArray(AB,),)
 
-        describe.each(everyCollectionInstanceByIterable,)("%s", ({value: {newInstance: newCollectionInstance, },},) => {
-        describe.each(iterableCreation,)("%s", ({value: iterableCreation,},) => {
-            const newInstanceOf0 = () => new GenericCollectionIterator(newCollectionInstance(iterableCreation, EMPTY,),)
-            const newInstanceOf1 = () => new GenericCollectionIterator(newCollectionInstance(iterableCreation, A,),)
-            const newInstanceOf2 = () => new GenericCollectionIterator(newCollectionInstance(iterableCreation, AB,),)
+        describe("forEach", () => {
+            describe("empty", () => {
+                let instance: GenericCollectionIterator
+                const size = 0
 
-            describe("forEach", () => {
-                describe("with size of 0", () => {
-                    let instance: GenericCollectionIterator
-                    const size = 0
+                beforeEach(() => instance = newInstanceOf0(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
 
-                    beforeEach(() => instance = newInstanceOf0(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
-
-                        instance.forEach(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
-                    test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBeNull(),)
-                    test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+                    instance.forEach(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
                 },)
-                describe("with size of 1", () => {
-                    let instance: GenericCollectionIterator
-                    const lastIndex = 0
-                    const size = 1
-
-                    beforeEach(() => instance = newInstanceOf1(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
-
-                        instance.forEach(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
-                    test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
-                    test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
-                },)
-                describe("with size of 2", () => {
-                    let instance: GenericCollectionIterator
-                    const secondLastIndex = 0
-                    const lastIndex = 1
-                    const size = 2
-
-                    beforeEach(() => instance = newInstanceOf2(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
-
-                        instance.forEach(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBe(secondLastIndex,),)
-                    test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
-                    test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
-                },)
+                test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
+                test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBeNull(),)
+                test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
             },)
-            describe("forEachIndexed", () => {
-                describe("with size of 0", () => {
-                    let instance: GenericCollectionIterator
-                    const size = 0
+            describe("1 field", () => {
+                let instance: GenericCollectionIterator
+                const lastIndex = 0
+                const size = 1
 
-                    beforeEach(() => instance = newInstanceOf0(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
+                beforeEach(() => instance = newInstanceOf1(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
 
-                        instance.forEachIndexed(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
-                    test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBeNull(),)
-                    test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+                    instance.forEach(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
                 },)
-                describe("with size of 1", () => {
-                    let instance: GenericCollectionIterator
-                    const lastIndex = 0
-                    const size = 1
-
-                    beforeEach(() => instance = newInstanceOf1(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
-
-                        instance.forEachIndexed(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
-                    test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
-                    test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
-                },)
-                describe("with size of 2", () => {
-                    let instance: GenericCollectionIterator
-                    const secondLastIndex = 0
-                    const lastIndex = 1
-                    const size = 2
-
-                    beforeEach(() => instance = newInstanceOf2(),)
-                    test("amount of time", () => {
-                        const callback = jest.fn()
-
-                        instance.forEachIndexed(callback,)
-                        expect(callback,).toHaveBeenCalledTimes(size,)
-                    },)
-                    test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBe(secondLastIndex,),)
-                    test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
-                    test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
-                },)
+                test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
+                test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
+                test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
             },)
-        },)},)
+            describe("2 fields", () => {
+                let instance: GenericCollectionIterator
+                const secondLastIndex = 0
+                const lastIndex = 1
+                const size = 2
+
+                beforeEach(() => instance = newInstanceOf2(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
+
+                    instance.forEach(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
+                },)
+                test("previousIndex", () => expect(instance.forEach(EMPTY_CALLBACK,).previousIndex,).toBe(secondLastIndex,),)
+                test("currentIndex",  () => expect(instance.forEach(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
+                test("nextIndex",     () => expect(instance.forEach(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+            },)
+        },)
+        describe("forEachIndexed", () => {
+            describe("empty", () => {
+                let instance: GenericCollectionIterator
+                const size = 0
+
+                beforeEach(() => instance = newInstanceOf0(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
+
+                    instance.forEachIndexed(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
+                },)
+                test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
+                test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBeNull(),)
+                test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+            },)
+            describe("1 field", () => {
+                let instance: GenericCollectionIterator
+                const lastIndex = 0
+                const size = 1
+
+                beforeEach(() => instance = newInstanceOf1(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
+
+                    instance.forEachIndexed(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
+                },)
+                test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBeNull(),)
+                test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
+                test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+            },)
+            describe("2 fields", () => {
+                let instance: GenericCollectionIterator
+                const secondLastIndex = 0
+                const lastIndex = 1
+                const size = 2
+
+                beforeEach(() => instance = newInstanceOf2(),)
+                test("amount of time", () => {
+                    const callback = jest.fn()
+
+                    instance.forEachIndexed(callback,)
+                    expect(callback,).toHaveBeenCalledTimes(size,)
+                },)
+                test("previousIndex", () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).previousIndex,).toBe(secondLastIndex,),)
+                test("currentIndex",  () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).currentIndex,).toBe(lastIndex,),)
+                test("nextIndex",     () => expect(instance.forEachIndexed(EMPTY_CALLBACK,).nextIndex,).toBeNull(),)
+            },)
+        },)
     },)
 
 },)
