616e0fcf4414416065088b6f6a926dd06b8b7bd1
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:36:07 -0400

    [Javascript] Addition of an array to the extension method "first"

--- javascript/src/method/first.ts
+++ javascript/src/method/first.ts
@@ -13,6 +13,8 @@ import type {MinimalistCollectionHolder}                 from "../MinimalistColl
 
 import {CollectionHolderIndexOutOfBoundsException} from "../exception/CollectionHolderIndexOutOfBoundsException"
 import {EmptyCollectionHolderException}            from "../exception/EmptyCollectionHolderException"
+import {isArray}                                   from "./isArray"
+import {isArrayByStructure}                        from "./isArrayByStructure"
 import {isCollectionHolder}                        from "./isCollectionHolder"
 import {isCollectionHolderByStructure}             from "./isCollectionHolderByStructure"
 
@@ -21,7 +23,7 @@ import {isCollectionHolderByStructure}             from "./isCollectionHolderByS
 /**
  * Get the first element in the {@link collection}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @throws TypeError                      The {@link collection} was <b>null</b> or <b>undefined</b>
  * @throws EmptyCollectionHolderException The {@link collection} <b>is empty</b>
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/first.html Kotlin first()
@@ -29,12 +31,12 @@ import {isCollectionHolderByStructure}             from "./isCollectionHolderByS
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first C# First()
  * @extensionFunction
  */
-export function first<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): T
+export function first<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): T
 /**
  * Get the first element in the {@link collection}
  * matching the given {@link predicate}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate  The matching predicate
  * @throws TypeError                                 The {@link collection} was <b>null</b> or <b>undefined</b>
  * @throws EmptyCollectionHolderException            The {@link collection} <b>is empty</b>
@@ -44,12 +46,12 @@ export function first<const T, >(collection: Nullable<MinimalistCollectionHolder
  * @typescriptDefinition
  * @extensionFunction
  */
-export function first<const T, const S extends T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: Nullable<RestrainedBooleanCallback<T, S>>,): S
+export function first<const T, const S extends T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: Nullable<RestrainedBooleanCallback<T, S>>,): S
 /**
  * Get the first element in the {@link collection}
  * matching the given {@link predicate}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate  The matching predicate
  * @throws TypeError                                 The {@link collection} was <b>null</b> or <b>undefined</b>
  * @throws EmptyCollectionHolderException            The {@link collection} <b>is empty</b>
@@ -58,14 +60,18 @@ export function first<const T, const S extends T, >(collection: Nullable<Minimal
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first C# First(predicate)
  * @extensionFunction
  */
-export function first<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: Nullable<BooleanCallback<T>>,): T
-export function first<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate?: Nullable<BooleanCallback<T>>,) {
+export function first<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: Nullable<BooleanCallback<T>>,): T
+export function first<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate?: Nullable<BooleanCallback<T>>,) {
     if (collection == null)
         throw new TypeError("No element could be retrieved from a null collection.",) // TODO change to custom exception
     if (isCollectionHolder<T>(collection,))
         return firstByCollectionHolder(collection, predicate,)
+    if (isArray(collection,))
+        return firstByArray(collection, predicate,)
     if (isCollectionHolderByStructure<T>(collection,))
         return firstByCollectionHolder(collection, predicate,)
+    if (isArrayByStructure<T>(collection,))
+        return firstByArray(collection, predicate,)
     return firstByMinimalistCollectionHolder(collection, predicate,)
 }
 
@@ -184,6 +190,63 @@ export function firstByCollectionHolder<const T, >(collection: Nullable<Collecti
     return __with0Argument(collection, predicate as () => boolean, collection.size,)
 }
 
+/**
+ * Get the first element in the {@link collection}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @throws TypeError                      The {@link collection} was <b>null</b> or <b>undefined</b>
+ * @throws EmptyCollectionHolderException The {@link collection} <b>is empty</b>
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/first.html Kotlin first()
+ * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/SequencedCollection.html#getFirst() Java getFirst()
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first C# First()
+ * @extensionFunction
+ */
+export function firstByArray<const T, >(collection: Nullable<readonly T[]>,): T
+/**
+ * Get the first element in the {@link collection}
+ * matching the given {@link predicate}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate  The matching predicate
+ * @throws TypeError                                 The {@link collection} was <b>null</b> or <b>undefined</b>
+ * @throws EmptyCollectionHolderException            The {@link collection} <b>is empty</b>
+ * @throws CollectionHolderIndexOutOfBoundsException No element could be found from the {@link predicate}
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/first.html Kotlin first(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first C# First(predicate)
+ * @typescriptDefinition
+ * @extensionFunction
+ */
+export function firstByArray<const T, const S extends T, >(collection: Nullable<readonly T[]>, predicate: Nullable<RestrainedBooleanCallback<T, S>>,): S
+/**
+ * Get the first element in the {@link collection}
+ * matching the given {@link predicate}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate  The matching predicate
+ * @throws TypeError                                 The {@link collection} was <b>null</b> or <b>undefined</b>
+ * @throws EmptyCollectionHolderException            The {@link collection} <b>is empty</b>
+ * @throws CollectionHolderIndexOutOfBoundsException No element could be found from the {@link predicate}
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/first.html Kotlin first(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first C# First(predicate)
+ * @extensionFunction
+ */
+export function firstByArray<const T, >(collection: Nullable<readonly T[]>, predicate: Nullable<BooleanCallback<T>>,): T
+export function firstByArray<const T, >(collection: Nullable<readonly T[]>, predicate?: Nullable<BooleanCallback<T>>,) {
+    if (collection == null)
+        throw new TypeError("No element could be retrieved from a null collection.",) // TODO change to custom exception
+
+    const size = collection.length
+    if (size == 0)
+        throw new EmptyCollectionHolderException()
+    if (predicate == null)
+        return collection[0]
+    if (predicate.length == 1)
+        return __with1ArgumentByArray(collection, predicate as (value: T,) => boolean, size,)
+    if (predicate.length >= 2)
+        return __with2ArgumentByArray(collection, predicate, size,)
+    return __with0ArgumentByArray(collection, predicate as () => boolean, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -193,6 +256,7 @@ function __withNoPredicate<const T, >(collection: CollectionHolder<T>,) {
     return collection.get(0,)
 }
 
+
 function __with0Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: () => boolean, size: number,) {
     let index = -1
     while (++index < size)
@@ -200,6 +264,15 @@ function __with0Argument<const T, >(collection: MinimalistCollectionHolder<T>, p
             return collection.get(index,)
     throw new CollectionHolderIndexOutOfBoundsException("Index out of bound. No element could be found from the filter predicate received in the collection.", 0,)
 }
+
+function __with0ArgumentByArray<const T, >(collection: readonly T[], predicate: () => boolean, size: number,) {
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return collection[index] as T
+    throw new CollectionHolderIndexOutOfBoundsException("Index out of bound. No element could be found from the filter predicate received in the collection.", 0,)
+}
+
 
 function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, size: number,) {
     let index = -1
@@ -211,10 +284,31 @@ function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, p
     throw new CollectionHolderIndexOutOfBoundsException("Index out of bound. No element could be found from the filter predicate received in the collection.", 0,)
 }
 
+function __with1ArgumentByArray<const T, >(collection: readonly T[], predicate: (value: T,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size) {
+        const value = collection[index] as T
+        if (predicate(value,))
+            return value
+    }
+    throw new CollectionHolderIndexOutOfBoundsException("Index out of bound. No element could be found from the filter predicate received in the collection.", 0,)
+}
+
+
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, size: number,) {
     let index = -1
     while (++index < size) {
         const value = collection.get(index,)
+        if (predicate(value, index,))
+            return value
+    }
+    throw new CollectionHolderIndexOutOfBoundsException("Index out of bound. No element could be found from the filter predicate received in the collection.", 0,)
+}
+
+function __with2ArgumentByArray<const T, >(collection: readonly T[], predicate: (value: T, index: number,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size) {
+        const value = collection[index] as T
         if (predicate(value, index,))
             return value
     }
