f2f198c976e4bdd38fd53c536a29989cd93972b8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:49:00 -0400

    [Javascript] Addition of an array to the extension method "hasDuplicate"

--- javascript/src/method/hasDuplicate.ts
+++ javascript/src/method/hasDuplicate.ts
@@ -10,6 +10,8 @@ import type {Nullable} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -18,17 +20,21 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 /**
  * The {@link collection} has at least one duplicate value
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @return {boolean} <b>true</b> only if one element is equal (===) to another one
  * @extensionFunction
  */
-export function hasDuplicate<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): boolean {
+export function hasDuplicate<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): boolean {
     if (collection == null)
         return false
-    if (isCollectionHolder(collection,))
+    if (isCollectionHolder<T>(collection,))
         return hasDuplicateByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return hasDuplicateByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return hasDuplicateByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return hasDuplicateByArray(collection,)
     return hasDuplicateByMinimalistCollectionHolder(collection,)
 }
 
@@ -65,6 +71,23 @@ export function hasDuplicateByCollectionHolder<const T, >(collection: Nullable<C
     return __hasDuplicate(collection, collection.size,)
 }
 
+/**
+ * The {@link collection} has at least one duplicate value
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @return {boolean} <b>true</b> only if one element is equal (===) to another one
+ * @extensionFunction
+ */
+export function hasDuplicateByArray<const T, >(collection: Nullable<readonly T[]>,): boolean {
+    if (collection == null)
+        return false
+
+    const size = collection.length
+    if (size == 0)
+        return false
+    return __hasDuplicateByArray(collection, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -75,6 +98,23 @@ function __hasDuplicate<T, >(collection: MinimalistCollectionHolder<T>, size: nu
     let index = 0
     while (++index < size) {
         const value = collection.get(index,)
+        let index2 = -1
+        while (++index2 < amountOfItemAdded)
+            if (temporaryArray[index2] === value)
+                return true
+        temporaryArray[amountOfItemAdded++] = value
+    }
+
+    return false
+}
+
+function __hasDuplicateByArray<T, >(collection: readonly T[], size: number,) {
+    const temporaryArray = new Array<T>(size,)
+    temporaryArray[0] = collection[0] as T
+    let amountOfItemAdded = 1
+    let index = 0
+    while (++index < size) {
+        const value = collection[index] as T
         let index2 = -1
         while (++index2 < amountOfItemAdded)
             if (temporaryArray[index2] === value)
