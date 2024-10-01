cffc8baa7a0321507f1a9f525d39f90c06acfb4b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:54:32 -0400

    [Javascript] Addition of an array to the extension method "indexOfFirstIndexed"

--- javascript/src/method/indexOfFirstIndexed.ts
+++ javascript/src/method/indexOfFirstIndexed.ts
@@ -11,9 +11,11 @@ import type {CollectionHolder}           from "../CollectionHolder"
 import type {ReverseBooleanCallback}     from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {__endingIndex, __maximumIndex, __startingIndex} from "./_indexes utility"
-import {isCollectionHolder}                             from "./isCollectionHolder"
-import {isCollectionHolderByStructure}                      from "./isCollectionHolderByStructure"
+import {__endingIndex, __startingIndex} from "./_indexes utility"
+import {isArray}                        from "./isArray"
+import {isArrayByStructure}             from "./isArrayByStructure"
+import {isCollectionHolder}             from "./isCollectionHolder"
+import {isCollectionHolderByStructure}  from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
 
@@ -22,7 +24,7 @@ import {isCollectionHolderByStructure}                      from "./isCollection
  * or <b>null</b> if it was not in the {@link collection}
  * from a range (if provided)
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate  The given predicate
  * @param fromIndex  The inclusive starting index
  * @param toIndex    The inclusive ending index
@@ -35,7 +37,7 @@ import {isCollectionHolderByStructure}                      from "./isCollection
  * @onlyGivePositiveValue
  * @extensionFunction
  */
-export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex?: NullableNumber, toIndex?: NullableNumber,): NullOrNumber
+export function indexOfFirstIndexed<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: ReverseBooleanCallback<T>, fromIndex?: NullableNumber, toIndex?: NullableNumber,): NullOrNumber
 /**
  * Get the first index matching the {@link predicate}
  * or <b>null</b> if it was not in the {@link collection}
@@ -58,13 +60,17 @@ export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCo
  * @extensionFunction
  */
 export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex?: NullableNumber, toIndex?: NullableNumber, limit?: NullableNumber,): NullOrNumber
-export function indexOfFirstIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
+export function indexOfFirstIndexed<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
     if (collection == null)
         return null
     if (isCollectionHolder<T>(collection,))
         return indexOfFirstIndexedByCollectionHolder(collection, predicate, fromIndex, toIndex,)
+    if (isArray(collection,))
+        return indexOfFirstIndexedByArray(collection, predicate, fromIndex, toIndex,)
     if (isCollectionHolderByStructure<T>(collection,))
         return indexOfFirstIndexedByCollectionHolder(collection, predicate, fromIndex, toIndex,)
+    if (isArrayByStructure<T>(collection,))
+        return indexOfFirstIndexedByArray(collection, predicate, fromIndex, toIndex,)
     return indexOfFirstIndexedByMinimalistCollectionHolder(collection, predicate, fromIndex, toIndex,)
 }
 
@@ -188,6 +194,43 @@ export function indexOfFirstIndexedByCollectionHolder<const T, >(collection: Nul
     return __with0Argument(predicate as () => boolean, startingIndex, endingIndex,)
 }
 
+/**
+ * Get the first index matching the {@link predicate}
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate  The given predicate
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @return {NullOrNumber} The index matching the {@link predicate} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex} or {@link toIndex} are not within a valid range
+ * @throws ForbiddenIndexException                   The {@link fromIndex} or {@link toIndex} are an undetermined {@link Number} (±∞ / {@link Number.NaN NaN})
+ * @see ReadonlyArray.findIndex
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/index-of-first.html Kotlin indexOfFirst(predicate)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function indexOfFirstIndexedByArray<const T, >(collection: Nullable<readonly T[]>, predicate: ReverseBooleanCallback<T>, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
+    if (collection == null)
+        return null
+
+    const size = collection.length
+    if (size === 0)
+        return null
+
+    const startingIndex = __startingIndex(fromIndex, size,)
+    const endingIndex = __endingIndex(toIndex, size,)
+    if (endingIndex < startingIndex)
+        return null
+    if (predicate.length == 1)
+        return __with1Argument(predicate as (index: number,) => boolean, startingIndex, endingIndex,)
+    if (predicate.length >= 2)
+        return __with2ArgumentByArray(collection, predicate, startingIndex, endingIndex,)
+    return __with0Argument(predicate as () => boolean, startingIndex, endingIndex,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -199,6 +242,7 @@ function __with0Argument(predicate: () => boolean, startingIndex: number, ending
     return null
 }
 
+
 function __with1Argument(predicate: (index: number,) => boolean, startingIndex: number, endingIndex: number,) {
     let index = startingIndex - 1
     while (++index <= endingIndex)
@@ -207,10 +251,19 @@ function __with1Argument(predicate: (index: number,) => boolean, startingIndex: 
     return null
 }
 
+
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (index: number, value: T,) => boolean, startingIndex: number, endingIndex: number,) {
     let index = startingIndex - 1
     while (++index <= endingIndex)
         if (predicate(index, collection.get(index,),))
+            return index
+    return null
+}
+
+function __with2ArgumentByArray<const T, >(collection: readonly T[], predicate: (index: number, value: T,) => boolean, startingIndex: number, endingIndex: number,) {
+    let index = startingIndex - 1
+    while (++index <= endingIndex)
+        if (predicate(index, collection[index] as T,))
             return index
     return null
 }
