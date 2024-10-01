eec066bd2c307e75574d1a49ce478ad8be2f72d4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:51:56 -0400

    [Javascript] Addition of an array to the extension method "lastIndexOf"

--- javascript/src/method/lastIndexOf.ts
+++ javascript/src/method/lastIndexOf.ts
@@ -10,9 +10,11 @@ import type {Nullable, NullableNumber, NullOrNumber} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {__endingIndex, __maximumIndex, __startingIndex} from "./_indexes utility"
-import {isCollectionHolder}                             from "./isCollectionHolder"
-import {isCollectionHolderByStructure}                  from "./isCollectionHolderByStructure"
+import {__endingIndex, __startingIndex} from "./_indexes utility"
+import {isArray}                        from "./isArray"
+import {isArrayByStructure}             from "./isArrayByStructure"
+import {isCollectionHolder}             from "./isCollectionHolder"
+import {isCollectionHolderByStructure}  from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
 
@@ -21,7 +23,7 @@ import {isCollectionHolderByStructure}                  from "./isCollectionHold
  * or <b>null</b> if it was not in the {@link collection}
  * from a range (if provided)
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param element    The element to find
  * @param fromIndex  The inclusive starting index
  * @param toIndex    The inclusive ending index
@@ -36,7 +38,7 @@ import {isCollectionHolderByStructure}                  from "./isCollectionHold
  * @onlyGivePositiveValue
  * @extensionFunction
  */
-export function lastIndexOf<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, element: T, fromIndex?: NullableNumber, toIndex?: NullableNumber,): NullOrNumber
+export function lastIndexOf<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, element: T, fromIndex?: NullableNumber, toIndex?: NullableNumber,): NullOrNumber
 /**
  * Get the <b>last</b> occurrence equivalent to the value received
  * or <b>null</b> if it was not in the {@link collection}
@@ -85,13 +87,17 @@ export function lastIndexOf<const T, >(collection: Nullable<MinimalistCollection
  * @deprecated Use a value that is present in the {@link collection} instead
  */
 export function lastIndexOf<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, element: unknown, fromIndex?: NullableNumber, toIndex?: NullableNumber, limit?: NullableNumber,): NullOrNumber
-export function lastIndexOf<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, element: T, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
+export function lastIndexOf<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, element: T, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
     if (collection == null)
         return null
     if (isCollectionHolder<T>(collection,))
         return lastIndexOfByCollectionHolder(collection, element, fromIndex, toIndex,)
+    if (isArray(collection,))
+        return lastIndexOfByArray(collection, element, fromIndex, toIndex,)
     if (isCollectionHolderByStructure<T>(collection,))
         return lastIndexOfByCollectionHolder(collection, element, fromIndex, toIndex,)
+    if (isArrayByStructure<T>(collection,))
+        return lastIndexOfByArray(collection, element, fromIndex, toIndex,)
     return lastIndexOfByMinimalistCollectionHolder(collection, element, fromIndex, toIndex,)
 }
 
@@ -259,6 +265,41 @@ export function lastIndexOfByCollectionHolder<const T, >(collection: Nullable<Co
     return __findLastInRange(collection, element, startingIndex, endingIndex,)
 }
 
+/**
+ * Get the <b>last</b> occurrence equivalent to the value received
+ * or <b>null</b> if it was not in the {@link collection}
+ * from a range (if provided)
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param element    The element to find
+ * @param fromIndex  The inclusive starting index
+ * @param toIndex    The inclusive ending index
+ * @return {NullOrNumber} The index associated to the {@link element} within the range or <b>null</b>
+ * @throws CollectionHolderIndexOutOfBoundsException The {@link fromIndex} or {@link toIndex} are not within a valid range
+ * @throws ForbiddenIndexException                   The {@link fromIndex} or {@link toIndex} are an undetermined {@link Number} (±∞ / {@link Number.NaN NaN})
+ * @see ReadonlyArray.lastIndexOf
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/last-index-of.html Kotlin lastIndexOf(element)
+ * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/List.html#lastIndexOf(java.lang.Object) Java lastIndexOf(element)
+ * @see https://learn.microsoft.com/dotnet/api/system.collections.generic.list-1.lastindexof C# LastIndexOf(item)
+ * @canReceiveNegativeValue
+ * @onlyGivePositiveValue
+ * @extensionFunction
+ */
+export function lastIndexOfByArray<const T, >(collection: Nullable<readonly T[]>, element: T, fromIndex: NullableNumber = null, toIndex: NullableNumber = null,): NullOrNumber {
+    if (collection == null)
+        return null
+
+    const size = collection.length
+    if (size === 0)
+        return null
+
+    const startingIndex = __startingIndex(fromIndex, size,)
+    const endingIndex = __endingIndex(toIndex, size,)
+    if (endingIndex < startingIndex)
+        return null
+    return __findLastInRangeByArray(collection, element, startingIndex, endingIndex,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -266,6 +307,14 @@ function __findLastInRange<const T, >(collection: MinimalistCollectionHolder<T>,
     let index = endingIndex + 1
     while (--index >= startingIndex)
         if (collection.get(index,) === element)
+            return index
+    return null
+}
+
+function __findLastInRangeByArray<const T, >(collection: readonly T[], element: T, startingIndex: number, endingIndex: number,) {
+    let index = endingIndex + 1
+    while (--index >= startingIndex)
+        if (collection[index] === element)
             return index
     return null
 }
