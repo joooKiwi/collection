35e1cdfe25951896455c02552d24d22d49453f70
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:34:12 -0400

    [Javascript] Addition of an array to the extension method "findIndexed"

--- javascript/src/method/findIndexed.ts
+++ javascript/src/method/findIndexed.ts
@@ -11,6 +11,8 @@ import type {CollectionHolder}                                         from "../
 import type {ReverseBooleanCallback, ReverseRestrainedBooleanCallback} from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder}                               from "../MinimalistCollectionHolder"
 
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -19,7 +21,7 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 /**
  * Get the first element found or <b>null</b> if nothing was found
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate  The given predicate
  * @see ReadonlyArray.find
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find.html Kotlin find(predicate)
@@ -27,25 +29,29 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * @typescriptDefinition
  * @extensionFunction
  */
-export function findIndexed<const T, const S extends T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseRestrainedBooleanCallback<T, S>,): NullOr<S>
+export function findIndexed<const T, const S extends T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: ReverseRestrainedBooleanCallback<T, S>,): NullOr<S>
 /**
  * Get the first element found or <b>null</b> if nothing was found
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate  The given predicate
  * @see ReadonlyArray.find
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find.html Kotlin find(predicate)
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
  * @extensionFunction
  */
-export function findIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>,): NullOr<T>
-export function findIndexed<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: ReverseBooleanCallback<T>,): NullOr<T> {
+export function findIndexed<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: ReverseBooleanCallback<T>,): NullOr<T>
+export function findIndexed<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: ReverseBooleanCallback<T>,) {
     if (collection == null)
         return null
     if (isCollectionHolder<T>(collection,))
         return findIndexedByCollectionHolder(collection, predicate,)
+    if (isArray(collection,))
+        return findIndexedByArray(collection, predicate,)
     if (isCollectionHolderByStructure<T>(collection,))
         return findIndexedByCollectionHolder(collection, predicate,)
+    if (isArrayByStructure<T>(collection,))
+        return findIndexedByArray(collection, predicate,)
     return findIndexedByMinimalistCollectionHolder(collection, predicate,)
 }
 
@@ -124,6 +130,43 @@ export function findIndexedByCollectionHolder<const T, >(collection: Nullable<Co
     return __with0Argument(collection, predicate as () => boolean, collection.size,)
 }
 
+/**
+ * Get the first element found or <b>null</b> if nothing was found
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.find
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find.html Kotlin find(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
+ * @typescriptDefinition
+ * @extensionFunction
+ */
+export function findIndexedByArray<const T, const S extends T, >(collection: Nullable<readonly T[]>, predicate: ReverseRestrainedBooleanCallback<T, S>,): NullOr<S>
+/**
+ * Get the first element found or <b>null</b> if nothing was found
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate  The given predicate
+ * @see ReadonlyArray.find
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find.html Kotlin find(predicate)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.firstordefault C# FirstOrDefault(predicate)
+ * @extensionFunction
+ */
+export function findIndexedByArray<const T, >(collection: Nullable<readonly T[]>, predicate: ReverseBooleanCallback<T>,): NullOr<T>
+export function findIndexedByArray<const T, >(collection: Nullable<readonly T[]>, predicate: ReverseBooleanCallback<T>,) {
+    if (collection == null)
+        return null
+
+    const size = collection.length
+    if (size == 0)
+        return null
+    if (predicate.length == 1)
+        return __with1ArgumentByArray(collection, predicate as (index: number,) => boolean, size,)
+    if (predicate.length >= 2)
+        return __with2ArgumentByArray(collection, predicate, size,)
+    return __with0ArgumentByArray(collection, predicate as () => boolean, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -135,6 +178,15 @@ function __with0Argument<const T, >(collection: MinimalistCollectionHolder<T>, p
     return null
 }
 
+function __with0ArgumentByArray<const T, >(collection: readonly T[], predicate: () => boolean, size: number,) {
+    let index = -1
+    while (++index < size)
+        if (predicate())
+            return collection[index] as T
+    return null
+}
+
+
 function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (index: number,) => boolean, size: number,) {
     let index = -1
     while (++index < size)
@@ -143,10 +195,29 @@ function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, p
     return null
 }
 
+function __with1ArgumentByArray<const T, >(collection: readonly T[], predicate: (index: number,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size)
+        if (predicate(index,))
+            return collection[index] as T
+    return null
+}
+
+
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (index: number, value: T,) => boolean, size: number,) {
     let index = -1
     while (++index < size) {
         const value = collection.get(index,)
+        if (predicate(index, value,))
+            return value
+    }
+    return null
+}
+
+function __with2ArgumentByArray<const T, >(collection: readonly T[], predicate: (index: number, value: T,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size) {
+        const value = collection[index] as T
         if (predicate(index, value,))
             return value
     }
