a422a3ab3b6501a786699a0bf5851efebe3c5114
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:37:42 -0400

    [Javascript] Addition of an array to the extension method "forEach"

--- javascript/src/method/forEach.ts
+++ javascript/src/method/forEach.ts
@@ -11,6 +11,8 @@ import type {CollectionHolder}           from "../CollectionHolder"
 import type {ValueIndexCallback}         from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -19,7 +21,7 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 /**
  * Perform a given {@link action} on each element
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param action     The given action
  * @see ReadonlyArray.forEach
  * @see ReadonlySet.forEach
@@ -27,13 +29,17 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/lang/Iterable.html#forEach(java.util.function.Consumer) Java forEach(action)
  * @extensionFunction
  */
-export function forEach<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, action: ValueIndexCallback<T>,): void {
+export function forEach<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, action: ValueIndexCallback<T>,): void {
     if (collection == null)
         return
     if (isCollectionHolder<T>(collection,))
         forEachByCollectionHolder(collection, action,)
+    else if (isArray(collection,))
+        forEachByArray(collection, action,)
     else if (isCollectionHolderByStructure<T>(collection,))
         forEachByCollectionHolder(collection, action,)
+    else if (isArrayByStructure<T>(collection,))
+        forEachByArray(collection, action,)
     else
         forEachByMinimalistCollectionHolder(collection, action,)
 }
@@ -89,6 +95,32 @@ export function forEachByCollectionHolder<const T, >(collection: Nullable<Collec
         __with0Argument(action as () => void, collection.size,)
 }
 
+/**
+ * Perform a given {@link action} on each element
+ *
+ * @param collection The {@link ReadonlyArray collection}
+ * @param action     The given action
+ * @see ReadonlyArray.forEach
+ * @see ReadonlySet.forEach
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/for-each.html Kotlin forEach(action)
+ * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/lang/Iterable.html#forEach(java.util.function.Consumer) Java forEach(action)
+ * @extensionFunction
+ */
+export function forEachByArray<const T, >(collection: Nullable<readonly T[]>, action: ValueIndexCallback<T>,): void {
+    if (collection == null)
+        return
+
+    const size = collection.length
+    if (size == 0)
+        return
+    if (action.length == 1)
+        __with1ArgumentByArray(collection, action as (value: T,) => void, size,)
+    else if (action.length >= 2)
+        __with2ArgumentByArray(collection, action, size,)
+    else
+        __with0Argument(action as () => void, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -98,16 +130,30 @@ function __with0Argument(action: () => void, size: number,) {
         action()
 }
 
+
 function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, action: (value: T,) => void, size: number,) {
     let index = -1
     while (++index < size)
         action(collection.get(index,),)
 }
 
+function __with1ArgumentByArray<const T, >(collection: readonly T[], action: (value: T,) => void, size: number,) {
+    let index = -1
+    while (++index < size)
+        action(collection[index] as T,)
+}
+
+
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, action: (value: T, index: number,) => void, size: number,) {
     let index = -1
     while (++index < size)
         action(collection.get(index,), index,)
+}
+
+function __with2ArgumentByArray<const T, >(collection: readonly T[], action: (value: T, index: number,) => void, size: number,) {
+    let index = -1
+    while (++index < size)
+        action(collection[index] as T, index,)
 }
 
 //#endregion -------------------- Loop methods --------------------
