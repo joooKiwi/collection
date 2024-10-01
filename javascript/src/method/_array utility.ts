a45ee86edae09be16da62d2d647a3bf67e854ee6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 16:10:48 -0400

    [Javascript] Addition of array utility methods

---
+++ javascript/src/method/_array utility.ts
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+import {CollectionHolderIndexOutOfBoundsException} from "../exception/CollectionHolderIndexOutOfBoundsException"
+import {EmptyCollectionHolderException}            from "../exception/EmptyCollectionHolderException"
+import {ForbiddenIndexException}                   from "../exception/ForbiddenIndexException"
+
+export function __get<const T, >(array: readonly T[], index: number,): T {
+    const size = array.length
+    if (size == 0)
+        throw new EmptyCollectionHolderException(null, index,)
+
+    if (Number.isNaN(index,))
+        throw new ForbiddenIndexException("Forbidden index. The index cannot be NaN.", index,)
+    if (index == Number.NEGATIVE_INFINITY)
+        throw new ForbiddenIndexException("Forbidden index. The index cannot be -∞.", index,)
+    if (index == Number.POSITIVE_INFINITY)
+        throw new ForbiddenIndexException("Forbidden index. The index cannot be +∞.", index,)
+
+    if (index in array)
+        return array[index] as T
+
+    if (index > size)
+        throw new CollectionHolderIndexOutOfBoundsException(`Index out of bound. The index "${index}" is over the size of the collection (${size}).`, index,)
+    if (index == size)
+        throw new CollectionHolderIndexOutOfBoundsException(`Index out of bound. The index "${index}" is the size of the collection (${size}).`, index,)
+    if (index >= 0)
+        return array[index] as T
+
+    const indexToRetrieve = size + index
+    if (indexToRetrieve < 0)
+        throw new CollectionHolderIndexOutOfBoundsException(`Index out of bound. The index "${index}" (${indexToRetrieve} after calculation) is under 0.`, index,)
+    if (indexToRetrieve > size)
+        throw new CollectionHolderIndexOutOfBoundsException(`Index out of bound. The index "${index}" (${indexToRetrieve} after calculation) is over the size of the collection (${size}).`, index,)
+    return array[indexToRetrieve] as T
+}
--- javascript/src/method/_tables utility.ts
+++ javascript/src/method/_tables utility.ts
@@ -17,6 +17,16 @@ export function __values<const T, >(collection: MinimalistCollectionHolder<T>, s
 }
 
 /** @internal */
+export function __valuesByArray<const T, >(collection: readonly T[], size: number,) {
+    const array = new Array<T>(size,)
+    let index = size
+    while (index-- > 0)
+        array[index] = collection[index] as T
+    return array
+}
+
+
+/** @internal */
 export function __uniqueValues<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     //#region -------------------- Possibly remove duplicates --------------------
 
@@ -50,10 +60,53 @@ export function __uniqueValues<const T, >(collection: MinimalistCollectionHolder
 }
 
 /** @internal */
+export function __uniqueValuesByArray<const T, >(collection: readonly T[], size: number,) {
+    //#region -------------------- Possibly remove duplicates --------------------
+
+    const array1 = new Array<T>(size,)
+    array1[0] = collection[0] as T
+    let amountOfItemAdded = 1
+    let index1 = -1
+    loopToRemoveDuplicate: while (++index1 < size) {
+        const value = collection[index1] as T
+        let index2 = -1
+        while (++index2 < amountOfItemAdded)
+            if (array1[index2] === value)
+                continue loopToRemoveDuplicate // It is equal, so we don't add it to the duplicates
+            else
+                array1[amountOfItemAdded++] = value
+    }
+
+    if (amountOfItemAdded == size)
+        return array1
+
+    //#endregion -------------------- Possibly remove duplicates --------------------
+    //#region -------------------- Return the non-duplicated values --------------------
+
+    const array2 = new Array<T>(amountOfItemAdded,)
+    let index3 = amountOfItemAdded
+    while (index3-- > 0)
+        array2[index3] = array1[index3] as T
+    return array2
+
+    //#endregion -------------------- Return the non-duplicated values --------------------
+}
+
+
+/** @internal */
 export function __associativeValues<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     const array = new Array<readonly [number, T,]>(size,)
     let index = size
     while (index-- > 0)
         array[index] = [index, collection.get(index,),]
+    return array
+}
+
+/** @internal */
+export function __associativeValuesByArray<const T, >(collection: readonly T[], size: number,) {
+    const array = new Array<readonly [number, T,]>(size,)
+    let index = size
+    while (index-- > 0)
+        array[index] = [index, collection[index] as T,]
     return array
 }
