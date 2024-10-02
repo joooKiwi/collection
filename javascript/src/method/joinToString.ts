b1cf422c5585c0dcd333a70202fd2a244503d95c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:09:46 -0400

    [Javascript] Addition of an array to the extension method "joinToString"

--- javascript/src/method/joinToString.ts
+++ javascript/src/method/joinToString.ts
@@ -12,6 +12,8 @@ import type {StringCallback}             from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
 import {__lastIndex}                   from "./_indexes utility"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -24,7 +26,7 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * Note that if the {@link collection} is huge,
  * a {@link limit} can be specified followed by a {@link truncated} value.
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param separator  The {@link String} to separate the values ({@link CollectionConstants.DEFAULT_JOIN_SEPARATOR ", "} by default)
  * @param prefix     The {@link String} before the join ({@link CollectionConstants.DEFAULT_JOIN_PREFIX '['} by default)
  * @param postfix    The {@link String} after the join ({@link CollectionConstants.DEFAULT_JOIN_POSTFIX ']'} by default)
@@ -37,13 +39,17 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * @see https://learn.microsoft.com/dotnet/api/system.string.join C# string.Join()
  * @canReceiveNegativeValue
  */
-export function joinToString<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, separator: NullableString = null, prefix: NullableString = null, postfix: NullableString = null, limit: NullableNumber = null, truncated: NullableString = null, transform: Nullable<StringCallback<T>> = null,): string {
+export function joinToString<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, separator: NullableString = null, prefix: NullableString = null, postfix: NullableString = null, limit: NullableNumber = null, truncated: NullableString = null, transform: Nullable<StringCallback<T>> = null,): string {
     if (collection == null)
         return prefixAndPostfixOnly(prefix, postfix,)
     if (isCollectionHolder<T>(collection,))
         return joinToStringByCollectionHolder(collection, separator, prefix, postfix, limit, truncated, transform,)
+    if (isArray(collection,))
+        return joinToStringByArray(collection, separator, prefix, postfix, limit, truncated, transform,)
     if (isCollectionHolderByStructure<T>(collection,))
         return joinToStringByCollectionHolder(collection, separator, prefix, postfix, limit, truncated, transform,)
+    if (isArrayByStructure<T>(collection,))
+        return joinToStringByArray(collection, separator, prefix, postfix, limit, truncated, transform,)
     return joinToStringByMinimalistCollectionHolder(collection, separator, prefix, postfix, limit, truncated, transform,)
 }
 
@@ -184,6 +190,72 @@ export function joinToStringByCollectionHolder<const T, >(collection: Nullable<C
     return __withTruncatedAnd0Argument(separator ?? ", ", prefix ?? '[', postfix ?? ']', lastIndex, truncated ?? '…', transform as () => string,)
 }
 
+/**
+ * Create a new {@link String} from every element in the {@link collection} using a {@link separator}
+ * utilizing the given {@link prefix} and {@link postfix} if supplied.
+ *
+ * Note that if the {@link collection} is huge,
+ * a {@link limit} can be specified followed by a {@link truncated} value.
+ *
+ * @param collection The {@link Nullable nullable} {@link MinimalistCollectionHolder collection}
+ * @param separator  The {@link String} to separate the values ({@link CollectionConstants.DEFAULT_JOIN_SEPARATOR ", "} by default)
+ * @param prefix     The {@link String} before the join ({@link CollectionConstants.DEFAULT_JOIN_PREFIX '['} by default)
+ * @param postfix    The {@link String} after the join ({@link CollectionConstants.DEFAULT_JOIN_POSTFIX ']'} by default)
+ * @param limit     The maximum number of values to loop (<b>null</b> by default)
+ * @param truncated  The truncated string if there is a limit ({@link CollectionConstants.DEFAULT_JOIN_TRUNCATED '…'} by default)
+ * @param transform  A callback to transform into a {@link String}
+ * @throws ForbiddenIndexException  The {@link limit} is an undetermined {@link Number} ({@link Number.NaN NaN})
+ * @see ReadonlyArray.join
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/join-to-string.html Kotlin joinToString()
+ * @see https://learn.microsoft.com/dotnet/api/system.string.join C# string.Join()
+ * @canReceiveNegativeValue
+ */
+export function joinToStringByArray<const T, >(collection: Nullable<readonly T[]>, separator: NullableString = null, prefix: NullableString = null, postfix: NullableString = null, limit: NullableNumber = null, truncated: NullableString = null, transform: Nullable<StringCallback<T>> = null,): string {
+    if (collection == null)
+        return prefixAndPostfixOnly(prefix, postfix,)
+
+    const size = collection.length
+    if (size == 0)
+        return prefixAndPostfixOnly(prefix, postfix,)
+
+    if (limit == 0)
+        return "[]"
+    if (transform == null) {
+        if (limit == null)
+            return __withNothingByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', collection.length,)
+
+        const lastIndex = __lastIndex(limit, size,)
+        if (lastIndex == 0)
+            return "[]"
+        if (lastIndex == size)
+            return __withNothingByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', size,)
+        return __withTruncatedByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', lastIndex, truncated ?? '…',)
+    }
+    if (limit == null)
+        if (transform.length == 1)
+            return __with1ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform as (value: T,) => string,)
+        else if (transform.length >= 2)
+            return __with2ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform,)
+        else
+            return __with0Argument(separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform as () => string,)
+
+    const lastIndex = __lastIndex(limit, size,)
+    if (lastIndex == 0)
+        return "[]"
+    if (lastIndex == size)
+        if (transform.length == 1)
+            return __with1ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform as (value: T,) => string,)
+        else if (transform.length >= 2)
+            return __with2ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform,)
+        else
+            return __with0Argument(separator ?? ", ", prefix ?? '[', postfix ?? ']', size, transform as () => string,)
+    if (transform.length == 1)
+        return __withTruncatedAnd1ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', lastIndex, truncated ?? '…', transform as (value: T,) => string,)
+    if (transform.length >= 2)
+        return __withTruncatedAnd2ArgumentByArray(collection, separator ?? ", ", prefix ?? '[', postfix ?? ']', lastIndex, truncated ?? '…', transform,)
+    return __withTruncatedAnd0Argument(separator ?? ", ", prefix ?? '[', postfix ?? ']', lastIndex, truncated ?? '…', transform as () => string,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Prefix & postfix method --------------------
 
@@ -212,6 +284,15 @@ function __withNothing(collection: MinimalistCollectionHolder, separator: string
     return `${prefix}${string}${collection.get(index,)}${postfix}`
 }
 
+function __withNothingByArray(collection: readonly unknown[], separator: string, prefix: string, postfix: string, lastIndex: number,) {
+    let string = ""
+    const sizeMinus1 = lastIndex - 1
+    let index = -1
+    while (++index < sizeMinus1)
+        string += `${collection[index]}${separator}`
+    return `${prefix}${string}${collection[index]}${postfix}`
+}
+
 
 function __with0Argument(separator: string, prefix: string, postfix: string, lastIndex: number, transform: () => string,) {
     let string = ""
@@ -222,6 +303,7 @@ function __with0Argument(separator: string, prefix: string, postfix: string, las
     return `${prefix}${string}${transform()}${postfix}`
 }
 
+
 function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T,) => string,) {
     let string = ""
     const sizeMinus1 = lastIndex - 1
@@ -230,6 +312,16 @@ function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, s
         string += `${transform(collection.get(index,),)}${separator}`
     return `${prefix}${string}${transform(collection.get(index,),)}${postfix}`
 }
+
+function __with1ArgumentByArray<const T, >(collection: readonly T[], separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T,) => string,) {
+    let string = ""
+    const sizeMinus1 = lastIndex - 1
+    let index = -1
+    while (++index < sizeMinus1)
+        string += `${transform(collection[index] as T,)}${separator}`
+    return `${prefix}${string}${transform(collection[index] as T,)}${postfix}`
+}
+
 
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T, index: number,) => string,) {
     let string = ""
@@ -240,12 +332,29 @@ function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, s
     return `${prefix}${string}${transform(collection.get(index,), index,)}${postfix}`
 }
 
+function __with2ArgumentByArray<const T, >(collection: readonly T[], separator: string, prefix: string, postfix: string, lastIndex: number, transform: (value: T, index: number,) => string,) {
+    let string = ""
+    const sizeMinus1 = lastIndex - 1
+    let index = -1
+    while (++index < sizeMinus1)
+        string += `${transform(collection[index] as T, index,)}${separator}`
+    return `${prefix}${string}${transform(collection[index] as T, index,)}${postfix}`
+}
+
 
 function __withTruncated(collection: MinimalistCollectionHolder, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string,) {
     let string = ""
     let index = -1
     while (++index < lastIndex)
         string += `${collection.get(index,)}${separator}`
+    return `${prefix}${string}${truncated}${postfix}`
+}
+
+function __withTruncatedByArray(collection: readonly unknown[], separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string,) {
+    let string = ""
+    let index = -1
+    while (++index < lastIndex)
+        string += `${collection[index]}${separator}`
     return `${prefix}${string}${truncated}${postfix}`
 }
 
@@ -258,6 +367,7 @@ function __withTruncatedAnd0Argument(separator: string, prefix: string, postfix:
     return `${prefix}${string}${truncated}${postfix}`
 }
 
+
 function __withTruncatedAnd1Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T,) => string,) {
     let string = ""
     let index = -1
@@ -266,11 +376,28 @@ function __withTruncatedAnd1Argument<const T, >(collection: MinimalistCollection
     return `${prefix}${string}${truncated}${postfix}`
 }
 
+function __withTruncatedAnd1ArgumentByArray<const T, >(collection: readonly T[], separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T,) => string,) {
+    let string = ""
+    let index = -1
+    while (++index < lastIndex)
+        string += `${transform(collection[index] as T,)}${separator}`
+    return `${prefix}${string}${truncated}${postfix}`
+}
+
+
 function __withTruncatedAnd2Argument<const T, >(collection: MinimalistCollectionHolder<T>, separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T, index: number,) => string,) {
     let string = ""
     let index = -1
     while (++index < lastIndex)
         string += `${transform(collection.get(index,), index,)}${separator}`
+    return `${prefix}${string}${truncated}${postfix}`
+}
+
+function __withTruncatedAnd2ArgumentByArray<const T, >(collection: readonly T[], separator: string, prefix: string, postfix: string, lastIndex: number, truncated: string, transform: (value: T, index: number,) => string,) {
+    let string = ""
+    let index = -1
+    while (++index < lastIndex)
+        string += `${transform(collection[index] as T, index,)}${separator}`
     return `${prefix}${string}${truncated}${postfix}`
 }
 
