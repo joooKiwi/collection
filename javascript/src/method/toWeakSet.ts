3b2c61d048d64911570086bee7ea7531f96370f7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:42:28 -0400

    [Javascript] Addition of an array to the extension method "toWeakSet"

--- javascript/src/method/toWeakSet.ts
+++ javascript/src/method/toWeakSet.ts
@@ -10,24 +10,32 @@ import type {Nullable} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {CollectionConstants}           from "../CollectionConstants"
-import {__uniqueValues, __values}      from "./_tables utility"
-import {isCollectionHolder}            from "./isCollectionHolder"
-import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
+import {CollectionConstants}                             from "../CollectionConstants"
+import {__uniqueValues, __uniqueValuesByArray, __values} from "./_tables utility"
+import {isArray}                                         from "./isArray"
+import {isArrayByStructure}                              from "./isArrayByStructure"
+import {isCollectionHolder}                              from "./isCollectionHolder"
+import {isCollectionHolderByStructure}                   from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link WeakSet weak set}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder}) to convert
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array}) to convert
  * @extensionFunction
  */
-export function toWeakSet<const T extends WeakKey, >(collection: Nullable<MinimalistCollectionHolder<T>>,): Readonly<WeakSet<T>> {
+export function toWeakSet<const T extends WeakKey, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): Readonly<WeakSet<T>> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_WEAK_SET
     if (isCollectionHolder<T>(collection,))
         return toWeakSetByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return toWeakSetByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return toWeakSetByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return toWeakSetByArray(collection,)
     return toWeakSetByMinimalistCollectionHolder(collection,)
 }
 
@@ -45,7 +53,7 @@ export function toWeakSetByMinimalistCollectionHolder<const T extends WeakKey, >
     const size = collection.size
     if (size == 0)
         return CollectionConstants.EMPTY_WEAK_SET
-    return __withoutDuplicate(collection, size,)
+    return __withDuplicate(collection, size,)
 }
 
 /**
@@ -64,12 +72,33 @@ export function toWeakSetByCollectionHolder<const T extends WeakKey, >(collectio
     return __withoutDuplicate(collection, collection.size,)
 }
 
+/**
+ * Convert the {@link collection} to an {@link WeakSet weak set}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection} to convert
+ * @extensionFunction
+ */
+export function toWeakSetByArray<const T extends WeakKey, >(collection: Nullable<readonly T[]>,): Readonly<WeakSet<T>> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_WEAK_SET
+
+    const size = collection.length
+    if (size == 0)
+        return CollectionConstants.EMPTY_WEAK_SET
+    return __withDuplicateByArray(collection, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Core method --------------------
 
 function __withDuplicate<const T extends WeakKey, >(collection: MinimalistCollectionHolder<T>, size: number,): Readonly<WeakSet<T>> {
     return Object.freeze(new WeakSet(__uniqueValues(collection, size,),),)
 }
+
+function __withDuplicateByArray<const T extends WeakKey, >(collection: readonly T[], size: number,): Readonly<WeakSet<T>> {
+    return Object.freeze(new WeakSet(__uniqueValuesByArray(collection, size,),),)
+}
+
 
 function __withoutDuplicate<const T extends WeakKey, >(collection: MinimalistCollectionHolder<T>, size: number,): Readonly<WeakSet<T>> {
     return Object.freeze(new WeakSet(__values(collection, size,),),)
