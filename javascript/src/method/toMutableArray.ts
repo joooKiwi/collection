fd10cb5770728b03670d1df4cc86cae9f33c0726
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:40:37 -0400

    [Javascript] Addition of an array to the extension method "toMutableArray"

--- javascript/src/method/toMutableArray.ts
+++ javascript/src/method/toMutableArray.ts
@@ -10,7 +10,9 @@ import type {Nullable} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {__values}                      from "./_tables utility"
+import {__values, __valuesByArray}     from "./_tables utility"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -19,16 +21,20 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 /**
  * Convert the {@link collection} to an {@link Array mutable array}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder}) to convert
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array}) to convert
  * @extensionFunction
  */
-export function toMutableArray<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): T[] {
+export function toMutableArray<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): T[] {
     if (collection == null)
         return []
     if (isCollectionHolder<T>(collection,))
         return toMutableArrayByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return toMutableArrayByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return toMutableArrayByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return toMutableArrayByArray(collection,)
     return toMutableArrayByMinimalistCollectionHolder(collection,)
 }
 
@@ -63,11 +69,31 @@ export function toMutableArrayByCollectionHolder<const T, >(collection: Nullable
     return __newMutableArray(collection, collection.size,)
 }
 
+/**
+ * Convert the {@link collection} to an {@link Array mutable array}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection} to convert
+ * @extensionFunction
+ */
+export function toMutableArrayByArray<const T, >(collection: Nullable<readonly T[]>,): T[] {
+    if (collection == null)
+        return []
+
+    const size = collection.length
+    if (size == 0)
+        return []
+    return __newMutableArrayByArray(collection, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop method --------------------
 
 function __newMutableArray<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     return __values(collection, size,)
+}
+
+function __newMutableArrayByArray<const T, >(collection: readonly T[], size: number,) {
+    return __valuesByArray(collection, size,)
 }
 
 //#endregion -------------------- Loop method --------------------
