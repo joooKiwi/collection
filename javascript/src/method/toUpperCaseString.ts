ff9307803c9ac4a5cc4a70cb8966960e9ad9652f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:46:14 -0400

    [Javascript] Addition of an array to the extension method "toUpperCaseString"

--- javascript/src/method/toUpperCaseString.ts
+++ javascript/src/method/toUpperCaseString.ts
@@ -10,9 +10,11 @@ import type {Nullable} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
+import {asUpperCaseString}             from "./asString"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
-import {asUpperCaseString}             from "./asString"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 
 //#region -------------------- Facade method --------------------
 
@@ -20,17 +22,21 @@ import {asUpperCaseString}             from "./asString"
  * Convert the {@link collection} to a {@link String} on every value
  * by calling its "<i>{@link String.toUpperCase toUpperCase()}</i>" method
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @see String.toUpperCase
  * @extensionFunction
  */
-export function toUpperCaseString<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): string {
+export function toUpperCaseString<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): string {
     if (collection == null)
         return "[]"
-    if (isCollectionHolder(collection,))
+    if (isCollectionHolder<T>(collection,))
         return toUpperCaseStringByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return toUpperCaseStringByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return toUpperCaseStringByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return toUpperCaseStringByArray(collection,)
     return toUpperCaseStringByMinimalistCollectionHolder(collection,)
 }
 
@@ -69,6 +75,24 @@ export function toUpperCaseStringByCollectionHolder<const T, >(collection: Nulla
     return __toString(collection, collection.size,)
 }
 
+/**
+ * Convert the {@link collection} to a {@link String} on every value
+ * by calling its "<i>{@link String.toUpperCase toUpperCase()}</i>" method
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @see String.toUpperCase
+ * @extensionFunction
+ */
+export function toUpperCaseStringByArray<const T, >(collection: Nullable<readonly T[]>,): string {
+    if (collection == null)
+        return "[]"
+
+    const size = collection.length
+    if (size == 0)
+        return "[]"
+    return __toStringByArray(collection, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop method --------------------
 
@@ -79,6 +103,15 @@ function __toString(collection: MinimalistCollectionHolder, size: number,) {
     while (++index < sizeMinus1)
         string += `${asUpperCaseString(collection.get(index,),)}, `
     return `[${string}${asUpperCaseString(collection.get(index,),)}]`
+}
+
+function __toStringByArray(collection: readonly unknown[], size: number,) {
+    let string = ""
+    const sizeMinus1 = size - 1
+    let index = -1
+    while (++index < sizeMinus1)
+        string += `${asUpperCaseString(collection[index],)}, `
+    return `[${string}${asUpperCaseString(collection[index],)}]`
 }
 
 //#endregion -------------------- Loop method --------------------
