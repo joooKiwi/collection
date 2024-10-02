5800a7ed646d9fa51ef953cd90279b353066a5a9
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:24:37 -0400

    [Javascript] Addition of an array to the extension method "map"

--- javascript/src/method/map.ts
+++ javascript/src/method/map.ts
@@ -12,6 +12,8 @@ import type {ValueIndexWithReturnCallback} from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder}   from "../MinimalistCollectionHolder"
 
 import {CollectionConstants}           from "../CollectionConstants"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
@@ -21,7 +23,7 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * Create a new {@link CollectionHolder} applying a {@link transform} function
  * on each element of the {@link collection}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param transform  The given transform
  * @see ReadonlyArray.map
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/map.html Kotlin map(transform)
@@ -29,13 +31,17 @@ import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
  * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.select C# Select(transform)
  * @extensionFunction
  */
-export function map<const T, const U, >(collection: Nullable<MinimalistCollectionHolder<T>>, transform: ValueIndexWithReturnCallback<T, U>,): CollectionHolder<U> {
+export function map<const T, const U, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, transform: ValueIndexWithReturnCallback<T, U>,): CollectionHolder<U> {
     if (collection == null)
         return CollectionConstants.EMPTY_COLLECTION_HOLDER
     if (isCollectionHolder<T>(collection,))
         return mapByCollectionHolder(collection, transform,)
+    if (isArray(collection,))
+        return mapByArray(collection, transform,)
     if (isCollectionHolderByStructure<T>(collection,))
         return mapByCollectionHolder(collection, transform,)
+    if (isArrayByStructure<T>(collection,))
+        return mapByArray(collection, transform,)
     return mapByMinimalistCollectionHolder(collection, transform,)
 }
 
@@ -90,6 +96,32 @@ export function mapByCollectionHolder<const T, const U, >(collection: Nullable<C
     return new CollectionConstants.LazyGenericCollectionHolder(() => __with0Argument(transform as () => U, collection.size,),)
 }
 
+/**
+ * Create a new {@link CollectionHolder} applying a {@link transform} function
+ * on each element of the {@link collection}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param transform  The given transform
+ * @see ReadonlyArray.map
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/map.html Kotlin map(transform)
+ * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/stream/Stream.html#map(java.util.function.Function) Java map(transform)
+ * @see https://learn.microsoft.com/dotnet/api/system.linq.enumerable.select C# Select(transform)
+ * @extensionFunction
+ */
+export function mapByArray<const T, const U, >(collection: Nullable<readonly T[]>, transform: ValueIndexWithReturnCallback<T, U>,): CollectionHolder<U> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+
+    const size = collection.length
+    if (size == 0)
+        return CollectionConstants.EMPTY_COLLECTION_HOLDER
+    if (transform.length == 1)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with1ArgumentByArray(collection, transform as (value: T,) => U, size,),)
+    if (transform.length >= 2)
+        return new CollectionConstants.LazyGenericCollectionHolder(() => __with2ArgumentByArray(collection, transform, size,),)
+    return new CollectionConstants.LazyGenericCollectionHolder(() => __with0Argument(transform as () => U, size,),)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -101,6 +133,7 @@ function __with0Argument<const U, >(transform: () => U, size: number,) {
     return newArray
 }
 
+
 function __with1Argument<const T, const U, >(collection: MinimalistCollectionHolder<T>, transform: (value: T,) => U, size: number,) {
     const newArray = new Array<U>(size,)
     let index = -1
@@ -109,11 +142,28 @@ function __with1Argument<const T, const U, >(collection: MinimalistCollectionHol
     return newArray
 }
 
+function __with1ArgumentByArray<const T, const U, >(collection: readonly T[], transform: (value: T,) => U, size: number,) {
+    const newArray = new Array<U>(size,)
+    let index = -1
+    while (++index < size)
+        newArray[index] = transform(collection[index] as T,)
+    return newArray
+}
+
+
 function __with2Argument<const T, const U, >(collection: MinimalistCollectionHolder<T>, transform: (value: T, index: number,) => U, size: number,) {
     const newArray = new Array<U>(size,)
     let index = -1
     while (++index < size)
         newArray[index] = transform(collection.get(index,), index,)
+    return newArray
+}
+
+function __with2ArgumentByArray<const T, const U, >(collection: readonly T[], transform: (value: T, index: number,) => U, size: number,) {
+    const newArray = new Array<U>(size,)
+    let index = -1
+    while (++index < size)
+        newArray[index] = transform(collection[index] as T, index,)
     return newArray
 }
 
