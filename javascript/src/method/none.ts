4b225c0e7fdad6b1ad7949110505b7e9dd658da3
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:26:17 -0400

    [Javascript] Addition of an array to the extension method "none"

--- javascript/src/method/none.ts
+++ javascript/src/method/none.ts
@@ -11,15 +11,17 @@ import type {CollectionHolder}           from "../CollectionHolder"
 import type {BooleanCallback}            from "../CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {isCollectionHolder}                   from "./isCollectionHolder"
-import {isCollectionHolderByStructure}        from "./isCollectionHolderByStructure"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
+import {isCollectionHolder}            from "./isCollectionHolder"
+import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
 
 /**
  * Tell if the {@link collection} {@link CollectionHolder.isEmpty is empty}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} {@link CollectionHolder collection}
  * @return {boolean} <b>true</b> if null is received or the {@link collection} <b>is empty</b> otherwise
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/none.html Kotlin none()
  * @extensionFunction
@@ -35,24 +37,37 @@ export function none<const T, const COLLECTION extends CollectionHolder<T> = Col
  */
 export function none<const T, const COLLECTION extends MinimalistCollectionHolder<T> = MinimalistCollectionHolder<T>, >(collection: Nullable<COLLECTION>,): COLLECTION["size"] extends 0 ? true : COLLECTION["size"] extends number ? boolean : false
 /**
+ * Tell if the {@link collection} <b>is empty</b>
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @return {boolean} <b>true</b> if null is received or the {@link collection} <b>is empty</b> otherwise
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/none.html Kotlin none()
+ * @extensionFunction
+ */
+export function none<const T, const COLLECTION extends readonly T[] = readonly T[], >(collection: Nullable<COLLECTION>,): COLLECTION["length"] extends 0 ? true : COLLECTION["length"] extends number ? boolean : false
+/**
  * Check if <b>no</b> element in the {@link collection}
  * match the given {@link predicate}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @param predicate The given predicate
  * @return {boolean} <b>false</b> if at least one {@link predicate} is <b>true</b> on a value of the {@link collection}
  * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/none.html Kotlin none(predicate)
  * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/stream/Stream.html#noneMatch(java.util.function.Predicate) Java noneMatch(predicate)
  * @extensionFunction
  */
-export function none<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate: Nullable<BooleanCallback<T>>,): boolean
-export function none<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>, predicate?: Nullable<BooleanCallback<T>>,) {
+export function none<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate: Nullable<BooleanCallback<T>>,): boolean
+export function none<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>, predicate?: Nullable<BooleanCallback<T>>,) {
     if (collection == null)
         return true
     if (isCollectionHolder<T>(collection,))
         return noneByCollectionHolder(collection, predicate,)
+    if (isArray(collection,))
+        return noneByArray(collection, predicate,)
     if (isCollectionHolderByStructure<T>(collection,))
         return noneByCollectionHolder(collection, predicate,)
+    if (isArrayByStructure<T>(collection,))
+        return noneByArray(collection, predicate,)
     return noneByMinimalistCollectionHolder(collection, predicate,)
 }
 
@@ -129,6 +144,43 @@ export function noneByCollectionHolder<const T, >(collection: Nullable<Collectio
     return __with0Argument(predicate as () => boolean, collection.size,)
 }
 
+/**
+ * Tell if the {@link collection} <b>is empty</b>
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @return {boolean} <b>true</b> if null is received or the {@link collection} <b>is empty</b> otherwise
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/none.html Kotlin none()
+ * @extensionFunction
+ */
+export function noneByArray<const T, const COLLECTION extends readonly T[] = readonly T[], >(collection: Nullable<COLLECTION>,): COLLECTION["length"] extends 0 ? true : COLLECTION["length"] extends number ? boolean : false
+/**
+ * Check if <b>no</b> element in the {@link collection}
+ * match the given {@link predicate}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @param predicate The given predicate
+ * @return {boolean} <b>false</b> if at least one {@link predicate} is <b>true</b> on a value of the {@link collection}
+ * @see https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/none.html Kotlin none(predicate)
+ * @see https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/stream/Stream.html#noneMatch(java.util.function.Predicate) Java noneMatch(predicate)
+ * @extensionFunction
+ */
+export function noneByArray<const T, >(collection: Nullable<readonly T[]>, predicate: Nullable<BooleanCallback<T>>,): boolean
+export function noneByArray<const T, >(collection: Nullable<readonly T[]>, predicate?: Nullable<BooleanCallback<T>>,) {
+    if (collection == null)
+        return true
+    if (predicate == null)
+        return collection.length == 0
+
+    const size = collection.length
+    if (size == 0)
+        return true
+    if (predicate.length == 1)
+        return __with1ArgumentByArray(collection, predicate as (value: T,) => boolean, size,)
+    if (predicate.length >= 2)
+        return __with2ArgumentByArray(collection, predicate, size,)
+    return __with0Argument(predicate as () => boolean, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop methods --------------------
 
@@ -140,6 +192,7 @@ function __with0Argument(predicate: () => boolean, size: number,) {
     return true
 }
 
+
 function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T,) => boolean, size: number,) {
     let index = -1
     while (++index < size)
@@ -148,10 +201,27 @@ function __with1Argument<const T, >(collection: MinimalistCollectionHolder<T>, p
     return true
 }
 
+function __with1ArgumentByArray<const T, >(collection: readonly T[], predicate: (value: T,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size)
+        if (predicate(collection[index] as T,))
+            return false
+    return true
+}
+
+
 function __with2Argument<const T, >(collection: MinimalistCollectionHolder<T>, predicate: (value: T, index: number,) => boolean, size: number,) {
     let index = -1
     while (++index < size)
         if (predicate(collection.get(index,), index,))
+            return false
+    return true
+}
+
+function __with2ArgumentByArray<const T, >(collection: readonly T[], predicate: (value: T, index: number,) => boolean, size: number,) {
+    let index = -1
+    while (++index < size)
+        if (predicate(collection[index] as T, index,))
             return false
     return true
 }
