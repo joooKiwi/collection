818d5cab8a06a1587b863b4e816dd7d383b17cd8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:41:13 -0400

    [Javascript] Addition of an array to the extension method "toMap"

--- javascript/src/method/toMap.ts
+++ javascript/src/method/toMap.ts
@@ -10,26 +10,32 @@ import type {Nullable} from "@joookiwi/type"
 import type {CollectionHolder}           from "../CollectionHolder"
 import type {MinimalistCollectionHolder} from "../MinimalistCollectionHolder"
 
-import {CollectionConstants}           from "../CollectionConstants"
-import {__associativeValues}           from "./_tables utility"
-import {isCollectionHolder}            from "./isCollectionHolder"
-import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
+import {CollectionConstants}                             from "../CollectionConstants"
+import {__associativeValues, __associativeValuesByArray} from "./_tables utility"
+import {isArray}                                         from "./isArray"
+import {isArrayByStructure}                              from "./isArrayByStructure"
+import {isCollectionHolder}                              from "./isCollectionHolder"
+import {isCollectionHolderByStructure}                   from "./isCollectionHolderByStructure"
 
 //#region -------------------- Facade method --------------------
 
 /**
  * Convert the {@link collection} to an {@link ReadonlyMap map}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder}) to convert
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array}) to convert
  * @extensionFunction
  */
-export function toMap<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): ReadonlyMap<number, T> {
+export function toMap<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): ReadonlyMap<number, T> {
     if (collection == null)
         return CollectionConstants.EMPTY_MAP
     if (isCollectionHolder<T>(collection,))
         return toMapByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return toMapByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return toMapByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return toMapByArray(collection,)
     return toMapByMinimalistCollectionHolder(collection,)
 }
 
@@ -64,11 +70,31 @@ export function toMapByCollectionHolder<const T, >(collection: Nullable<Collecti
     return __newMap(collection, collection.size,)
 }
 
+/**
+ * Convert the {@link collection} to an {@link ReadonlyMap map}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection} to convert
+ * @extensionFunction
+ */
+export function toMapByArray<const T, >(collection: Nullable<readonly T[]>,): ReadonlyMap<number, T> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_MAP
+
+    const size = collection.length
+    if (size == 0)
+        return CollectionConstants.EMPTY_MAP
+    return __newMapByArray(collection, size,)
+}
+
 //#endregion -------------------- Facade method --------------------
 //#region -------------------- Loop method --------------------
 
 function __newMap<const T, >(collection: MinimalistCollectionHolder<T>, size: number,) {
     return Object.freeze(new Map(__associativeValues(collection, size,),),)
+}
+
+function __newMapByArray<const T, >(collection: readonly T[], size: number,) {
+    return Object.freeze(new Map(__associativeValuesByArray(collection, size,),),)
 }
 
 //#endregion -------------------- Loop method --------------------
