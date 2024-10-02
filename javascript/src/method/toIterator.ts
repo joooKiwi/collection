e5a59b32f9ffa293047a0de5d8f338712f8fdb1e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-01 21:43:31 -0400

    [Javascript] Addition of an array to the extension method "toIterator"

--- javascript/src/method/toIterator.ts
+++ javascript/src/method/toIterator.ts
@@ -13,22 +13,28 @@ import type {CollectionIterator}         from "../iterator/CollectionIterator"
 
 import {CollectionConstants}           from "../CollectionConstants"
 import {GenericCollectionIterator}     from "../iterator/GenericCollectionIterator"
+import {isArray}                       from "./isArray"
+import {isArrayByStructure}            from "./isArrayByStructure"
 import {isCollectionHolder}            from "./isCollectionHolder"
 import {isCollectionHolderByStructure} from "./isCollectionHolderByStructure"
 
 /**
  * Convert the {@link collection} to a new {@link CollectionIterator}
  *
- * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder} or {@link CollectionHolder})
+ * @param collection The {@link Nullable nullable} collection ({@link MinimalistCollectionHolder}, {@link CollectionHolder} or {@link ReadonlyArray Array})
  * @extensionFunction
  */
-export function toIterator<const T, >(collection: Nullable<MinimalistCollectionHolder<T>>,): CollectionIterator<T> {
+export function toIterator<const T, >(collection: Nullable<| MinimalistCollectionHolder<T> | readonly T[]>,): CollectionIterator<T> {
     if (collection == null)
         return CollectionConstants.EMPTY_COLLECTION_ITERATOR
     if (isCollectionHolder<T>(collection,))
         return toIteratorByCollectionHolder(collection,)
+    if (isArray(collection,))
+        return toIteratorByArray(collection,)
     if (isCollectionHolderByStructure<T>(collection,))
         return toIteratorByCollectionHolder(collection,)
+    if (isArrayByStructure<T>(collection,))
+        return toIteratorByArray(collection,)
     return toIteratorByMinimalistCollectionHolder(collection,)
 }
 
@@ -59,4 +65,18 @@ export function toIteratorByCollectionHolder<const T, >(collection: Nullable<Col
     if (collection.isEmpty)
         return CollectionConstants.EMPTY_COLLECTION_ITERATOR
     return new GenericCollectionIterator(collection,)
+}
+
+/**
+ * Convert the {@link collection} to a new {@link CollectionIterator}
+ *
+ * @param collection The {@link Nullable nullable} {@link ReadonlyArray collection}
+ * @extensionFunction
+ */
+export function toIteratorByArray<const T, >(collection: Nullable<readonly T[]>,): CollectionIterator<T> {
+    if (collection == null)
+        return CollectionConstants.EMPTY_COLLECTION_ITERATOR
+    if (collection.length == 0)
+        return CollectionConstants.EMPTY_COLLECTION_ITERATOR
+    return new GenericCollectionIterator(new CollectionConstants.GenericMinimalistCollectionHolder(collection,),)
 }
