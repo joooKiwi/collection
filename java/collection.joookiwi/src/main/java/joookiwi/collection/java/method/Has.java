e76034c02a5a99ddd76d93dcce7950599ef923d4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-16 12:37:04 -0400

    [Java] Addition of an array to the extension method "has"

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/Has.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/Has.java
@@ -7,6 +7,7 @@ import joookiwi.collection.java.exception.ImpossibleConstructionException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
 import static joookiwi.collection.java.CommonContracts.IF_1ST_NULL_THEN_FALSE_2;
@@ -15,9 +16,7 @@ public final class Has
         extends Utility {
 
     @Contract(ALWAYS_FAIL_0)
-    private Has() {
-        throw new ImpossibleConstructionException("The utility class “Has” cannot be constructed.", Has.class);
-    }
+    private Has() { throw new ImpossibleConstructionException("The utility class “Has” cannot be constructed.", Has.class); }
 
     //#region -------------------- Facade methods --------------------
 
@@ -25,6 +24,7 @@ public final class Has
     ///
     /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
     /// @param value      The value to compare
+    /// @param <T>        The `collection` type
     /// @return `true` if the `value` is equals to one value in the `collection`
     /// @see java.util.Collection#contains(Object)
     /// @see java.util.Map#containsValue(Object)
@@ -50,6 +50,7 @@ public final class Has
     ///
     /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
     /// @param value      The value to compare
+    /// @param <T>        The `collection` type
     /// @return `true` if the `value` is equals to one value in the `collection`
     /// @see java.util.Collection#contains(Object)
     /// @see java.util.Map#containsValue(Object)
@@ -68,6 +69,32 @@ public final class Has
         return __has(collection, collection.size(), value);
     }
 
+    /// Tell whenever the `value` exist in the `collection`
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param value      The value to compare
+    /// @param <T>        The `collection` type
+    /// @return `true` if the `value` is equals to one value in the `collection`
+    /// @see java.util.Collection#contains(Object)
+    /// @see java.util.Map#containsValue(Object)
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/includes">Javascript Array.includes(value)</a>
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/has">Javascript Set.has(value)</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/contains.html">Kotlin contains(value)</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/-list/containsValue.html">Kotlin Map.containsValue(element)</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.contains">C# contains(value)</a>
+    @ExtensionFunction
+    @Contract(IF_1ST_NULL_THEN_FALSE_2)
+    public static <T> boolean has(final T @Nullable @Unmodifiable [] collection,
+                                  final T value) {
+        if (collection == null)
+            return false;
+
+        var size = collection.length;
+        if (size == 0)
+            return false;
+        return __has(collection, size, value);
+    }
+
     //#endregion -------------------- Facade methods --------------------
     //#region -------------------- Loop methods --------------------
 
@@ -83,6 +110,24 @@ public final class Has
 
         while (++index < size) {
             final var collectionValue = collection.get(index);
+            if (value == collectionValue || value.equals(collectionValue))
+                return true;
+        }
+        return false;
+    }
+
+    private static <T> boolean __has(final T @NotNull @Unmodifiable [] collection, int size,
+                                     final T value) {
+        var index = -1;
+        if (value == null) {
+            while (++index < size)
+                if (collection[index] == null)
+                    return true;
+            return false;
+        }
+
+        while (++index < size) {
+            final var collectionValue = collection[index];
             if (value == collectionValue || value.equals(collectionValue))
                 return true;
         }
