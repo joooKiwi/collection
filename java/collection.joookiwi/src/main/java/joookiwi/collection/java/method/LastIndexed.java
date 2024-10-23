951bc3cc2c3716daa2ee87be0ba0c34a232c9f9d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-23 17:59:49 -0400

    [Java] Separation of the "lastIndexed" into "getLast" and "findLastIndexed"

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/LastIndexed.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/LastIndexed.java
@@ -11,23 +11,65 @@ import joookiwi.collection.java.exception.EmptyCollectionHolderException;
 import joookiwi.collection.java.exception.ImpossibleConstructionException;
 import joookiwi.collection.java.exception.NullCollectionHolderException;
 import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
 import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
-import static joookiwi.collection.java.CommonContracts.IF_1ST_NULL_THEN_NULL_2;
+import static joookiwi.collection.java.CommonContracts.IF_1ST_NULL_THEN_FAIL_1;
+import static joookiwi.collection.java.CommonContracts.IF_1ST_NULL_THEN_FAIL_2;
 
 @NotNullByDefault
-public final class FindLastIndexed
-        extends Utility {
+public final class LastIndexed
+        extends AliasUtility {
 
     @Contract(ALWAYS_FAIL_0)
-    private FindLastIndexed() { throw new ImpossibleConstructionException("The utility class “FindLastIndexed” cannot be constructed.", FindLastIndexed.class); }
+    private LastIndexed() { throw new ImpossibleConstructionException("The utility class “LastIndexed” cannot be constructed.", LastIndexed.class); }
 
     //#region -------------------- Facade methods --------------------
 
+    //#region -------------------- ∅ --------------------
+
+    /// Get the last element in the current `collection`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param <T>        The `collection` type
+    /// @throws NullCollectionHolderException  The `collection` was `null`
+    /// @throws EmptyCollectionHolderException The `collection` **is empty**
+    /// @see java.util.SequencedCollection#getLast()
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/last.html">Kotlin last()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last()</a>
+    @ExtensionFunction
+    @Contract(IF_1ST_NULL_THEN_FAIL_1)
+    public static <T> T lastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection) { return GetLast.getLast(collection); }
+
+    /// Get the last element in the current `collection`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param <T>        The `collection` type
+    /// @throws NullCollectionHolderException  The `collection` was `null`
+    /// @throws EmptyCollectionHolderException The `collection` [is empty][CollectionHolder#isEmpty]
+    /// @see java.util.SequencedCollection#getLast()
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/last.html">Kotlin last()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last()</a>
+    @ExtensionFunction
+    @Contract(IF_1ST_NULL_THEN_FAIL_1)
+    public static <T> T lastIndexed(final @Nullable CollectionHolder<? extends T> collection) { return GetLast.getLast(collection); }
+
+    /// Get the last element in the current `collection`
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param <T>        The `collection` type
+    /// @throws NullCollectionHolderException  The `collection` was `null`
+    /// @throws EmptyCollectionHolderException The `collection` **is empty**
+    /// @see java.util.SequencedCollection#getLast()
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/last.html">Kotlin last()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last()</a>
+    @ExtensionFunction
+    @Contract(IF_1ST_NULL_THEN_FAIL_1)
+    public static <T> T lastIndexed(final T @Nullable @Unmodifiable [] collection) { return GetLast.getLast(collection); }
+
+    //#endregion -------------------- ∅ --------------------
     //#region -------------------- predicate (int, T) → boolean --------------------
 
     /// Get the last element found in the `collection`
@@ -43,16 +85,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
-                                                  final @NotNull IntObjPredicate<? super T> predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with2Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                    final @Nullable IntObjPredicate<? super T> predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -68,16 +106,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable CollectionHolder<? extends T> collection,
-                                                  final @NotNull IntObjPredicate<? super T> predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with2Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                    final @Nullable IntObjPredicate<? super T> predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -93,16 +127,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final T @Nullable @Unmodifiable [] collection,
-                                                  final @NotNull IntObjPredicate<? super T> predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.length;
-        if (size == 0)
-            return null;
-        return __with2Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final T @Nullable @Unmodifiable [] collection,
+                                    final @Nullable IntObjPredicate<? super T> predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     //#endregion -------------------- predicate (int, T) → boolean --------------------
@@ -121,16 +151,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
-                                                  final @NotNull IntPredicate predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with1Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                    final @Nullable IntPredicate predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -146,16 +172,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable CollectionHolder<? extends T> collection,
-                                                  final @NotNull IntPredicate predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with1Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                    final @Nullable IntPredicate predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -171,16 +193,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final T @Nullable @Unmodifiable [] collection,
-                                                  final @NotNull IntPredicate predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.length;
-        if (size == 0)
-            return null;
-        return __with1Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final T @Nullable @Unmodifiable [] collection,
+                                    final @Nullable IntPredicate predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     //#endregion -------------------- predicate (int) → boolean --------------------
@@ -199,16 +217,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
-                                                  final @NotNull BooleanSupplier predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with0Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                    final @Nullable BooleanSupplier predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -224,16 +238,12 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final @Nullable CollectionHolder<? extends T> collection,
-                                                  final @NotNull BooleanSupplier predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.size();
-        if (size == 0)
-            return null;
-        return __with0Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                    final @Nullable BooleanSupplier predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     /// Get the last element found in the `collection`
@@ -249,89 +259,16 @@ public final class FindLastIndexed
     /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/find-last.html">Kotlin findLast(predicate)</a>
     /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last(predicate)</a>
     @ExtensionFunction
-    @Contract(IF_1ST_NULL_THEN_NULL_2)
-    public static <T> @Nullable T findLastIndexed(final T @Nullable @Unmodifiable [] collection,
-                                                  final @NotNull BooleanSupplier predicate) {
-        if (collection == null)
-            return null;
-
-        final var size = collection.length;
-        if (size == 0)
-            return null;
-        return __with0Argument(collection, predicate, size);
+    @Contract(IF_1ST_NULL_THEN_FAIL_2)
+    public static <T> T lastIndexed(final T @Nullable @Unmodifiable [] collection,
+                                    final @Nullable BooleanSupplier predicate) {
+        if (predicate == null)
+            return GetLast.getLast(collection);
+        return FindLastIndexed.findLastIndexed(collection, predicate);
     }
 
     //#endregion -------------------- predicate () → boolean --------------------
 
     //#endregion -------------------- Facade methods --------------------
-    //#region -------------------- Loop methods --------------------
-
-    private static <T> @Nullable T __with0Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
-                                                   final @NotNull BooleanSupplier predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0)
-            if (predicate.getAsBoolean())
-                return collection.get(index);
-        return null;
-    }
-
-    private static <T> @Nullable T __with0Argument(final T @NotNull @Unmodifiable [] collection,
-                                                   final @NotNull BooleanSupplier predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0)
-            if (predicate.getAsBoolean())
-                return collection[index];
-        return null;
-    }
-
-
-    private static <T> @Nullable T __with1Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
-                                                   final @NotNull IntPredicate predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0)
-            if (predicate.test(index))
-                return collection.get(index);
-        return null;
-    }
-
-    private static <T> @Nullable T __with1Argument(final T @NotNull @Unmodifiable [] collection,
-                                                   final @NotNull IntPredicate predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0)
-            if (predicate.test(index))
-                return collection[index];
-        return null;
-    }
-
-
-    private static <T> @Nullable T __with2Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
-                                                   final @NotNull IntObjPredicate<? super T> predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0) {
-            final var value = collection.get(index);
-            if (predicate.test(index, value))
-                return value;
-        }
-        return null;
-    }
-
-    private static <T> @Nullable T __with2Argument(final T @NotNull @Unmodifiable [] collection,
-                                                   final @NotNull IntObjPredicate<? super T> predicate,
-                                                   final int size) {
-        var index = size;
-        while (index-- > 0) {
-            final var value = collection[index];
-            if (predicate.test(index, value))
-                return value;
-        }
-        return null;
-    }
-
-    //#endregion -------------------- Loop methods --------------------
 
 }
