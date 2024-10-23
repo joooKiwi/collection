732ec87e3bb1231cf008bd7fc02850ec15ce5b7d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-22 23:03:51 -0400

    [Java] Creation of CopyOnWriteArraySet subclasses

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/EmptyCopyOnWriteArraySet.java
@@ -0,0 +1,128 @@
+package joookiwi.collection.java.extended;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+import joookiwi.collection.java.annotation.Singleton;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
+
+import static joookiwi.collection.java.CollectionConstants.*;
+import static joookiwi.collection.java.CommonContracts.*;
+
+/// A [Singleton] implementation of the [java.util.concurrent.CopyOnWriteArraySet],
+/// but in an empty form
+///
+/// @param <T> The type of the element
+/// @see Collections#emptySet
+/// @see joookiwi.collection.java.CollectionConstants#emptyHashSet
+@Singleton
+@NotNullByDefault
+public class EmptyCopyOnWriteArraySet<T>
+        extends ImmutableCopyOnWriteArraySet<T> {
+
+    @Serial private static final long serialVersionUID = 1374410244316048758L;
+
+    //#region -------------------- Singleton usage --------------------
+
+    private static volatile @Nullable EmptyCopyOnWriteArraySet<?> __instance;
+
+    protected EmptyCopyOnWriteArraySet() { super(); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> EmptyCopyOnWriteArraySet<T> getInstance() {
+        final var instance = __instance;
+        if (instance != null)
+            return (EmptyCopyOnWriteArraySet<T>) instance;
+        synchronized (EmptyCopyOnWriteArraySet.class) {
+            final var instance2 = __instance;
+            if (instance2 != null)
+                return (EmptyCopyOnWriteArraySet<T>) instance2;
+            return (EmptyCopyOnWriteArraySet<T>) (__instance = new EmptyCopyOnWriteArraySet<>());
+        }
+    }
+
+    //#endregion -------------------- Singleton usage --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Size methods --------------------
+
+    @Contract(pure = true) public @Override @Range(from = 0, to = 0) int size() { return 0; }
+
+    @Contract(value = ALWAYS_TRUE_0, pure = true) public @Override boolean isEmpty() { return true; }
+
+    //#endregion -------------------- Size methods --------------------
+    //#region -------------------- Has methods --------------------
+
+    @Contract(value = ALWAYS_FALSE_1, pure = true) public @Override boolean contains(final @Nullable Object value) { return false; }
+
+    @Contract(value = IF_1ST_NULL_THEN_FALSE_1, pure = true) public @Override boolean containsAll(final @Nullable Collection<?> values) {
+        if (values == null)
+            return false;
+        return values.isEmpty();
+    }
+
+    //#endregion -------------------- Has methods --------------------
+    //#region -------------------- For each methods --------------------
+
+    @Contract(pure = true) public @Override void forEach(final @Nullable Consumer<? super T> action) {}
+
+    //#endregion -------------------- For each methods --------------------
+    //#region -------------------- Iterator methods --------------------
+
+    @Contract(pure = true) public @Override @NotNull Iterator<T>    iterator(   ) { return emptyIterator(); }
+    @Contract(pure = true) public @Override @NotNull Spliterator<T> spliterator() { return emptySpliterator(); }
+
+    //#endregion -------------------- Iterator methods --------------------
+    //#region -------------------- To array methods --------------------
+
+    @Contract(value = ALWAYS_NEW_0, pure = true)           public @Override     Object @NotNull [] toArray(                                                   ) { return new Object[0]; }
+    @Contract(value = ALWAYS_1ST_PARAMETER_1, pure = true) public @Override <U> U @NotNull []      toArray(final U @NotNull []                           array) { return array; }
+    @Contract(pure = true)                                 public @Override <U> U @NotNull []      toArray(final @NotNull IntFunction<U @NotNull []> generator) { return generator.apply(0); }
+
+    //#endregion -------------------- To array methods --------------------
+    //#region -------------------- Stream methods --------------------
+
+    @Contract(pure = true) public @Override @NotNull Stream<T> stream() { return emptyStream(); }
+
+    @Contract(pure = true) public @Override @NotNull Stream<T> parallelStream() { return emptyParellelStream(); }
+
+    //#endregion -------------------- Stream methods --------------------
+    //#region -------------------- Equals methods --------------------
+
+    @Contract(pure = true) public @Override @Range(from = 0, to = 0) int hashCode() { return DEFAULT_EMPTY_HASH_CODE; }
+
+    @Contract(value = IF_1ST_NULL_THEN_FALSE_1, pure = true) public @Override boolean equals(final @Nullable Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (!(other instanceof Set<?>))
+            return false;
+        return ((Collection<?>) other).isEmpty();
+    }
+
+    //#endregion -------------------- Equals methods --------------------
+    //#region -------------------- Clone methods --------------------
+
+    @Contract(value = ALWAYS_THIS_0, pure = true) @Override public EmptyCopyOnWriteArraySet<T> clone() { return this; }
+
+    //#endregion -------------------- Clone methods --------------------
+    //#region -------------------- To string methods --------------------
+
+    @Contract(pure = true) public @Override String toString() { return DEFAULT_EMPTY_COLLECTION; }
+
+    //#endregion -------------------- To string methods --------------------
+
+    //#endregion -------------------- Methods --------------------
+
+}
---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ImmutableCopyOnWriteArraySet.java
@@ -0,0 +1,127 @@
+package joookiwi.collection.java.extended;
+
+import org.intellij.lang.annotations.Flow;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.function.Predicate;
+
+import static joookiwi.collection.java.CollectionConstants.*;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_1;
+
+/// An [immutable-like][Unmodifiable] behaviour of a [CopyOnWriteArraySet]
+///
+/// @param <T> The type of the element
+@NotNullByDefault
+public class ImmutableCopyOnWriteArraySet<T>
+        extends CopyOnWriteArraySet<T> {
+
+    //#region -------------------- Fields --------------------
+
+    @Serial private static final long serialVersionUID = -7724536483598230951L;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructors --------------------
+
+    //#region -------------------- ∅ --------------------
+
+    /// Create an empty [immutable-like][Unmodifiable] instance of [CopyOnWriteArraySet]
+    /// (similar to [java.util.Set#of()])
+    /// with a load factor of [1][joookiwi.collection.java.CollectionConstants#DEFAULT_EMPTY_LOAD_FACTOR]
+    /// and a capacity of [0][joookiwi.collection.java.CollectionConstants#DEFAULT_EMPTY_INITIAL_CAPACITY]
+    public ImmutableCopyOnWriteArraySet() { super(); }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- values --------------------
+
+    /// Create an [immutable-like][Unmodifiable] instance of [CopyOnWriteArraySet]
+    /// (similar to {@link java.util.Set#of(Object[])})
+    /// with a load factor of [0.75][joookiwi.collection.java.CollectionConstants#DEFAULT_LOAD_FACTOR]
+    /// and the capacity is the `values.length`
+    public ImmutableCopyOnWriteArraySet(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @NotNull @Unmodifiable [] values) {
+        super();
+        final var size = values.length;
+        if (size == 0)
+            return;
+        var index = -1;
+        while (++index < size)
+            super.add(values[index]);
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [CopyOnWriteArraySet]
+    /// (similar to [java.util.Set#copyOf(Collection)])
+    /// with a load factor of [0.75][joookiwi.collection.java.CollectionConstants#DEFAULT_LOAD_FACTOR]
+    /// and the capacity is the <code>values.[size][Collection#size()]</code>
+    public ImmutableCopyOnWriteArraySet(final @Flow(sourceIsContainer = true, targetIsContainer = true) @NotNull @Unmodifiable Collection<? extends T> values) {
+        super();
+        if (values.isEmpty())
+            return;
+        super.addAll(values);
+    }
+
+    //#endregion -------------------- values --------------------
+
+    //#endregion -------------------- Constructors --------------------
+    //#region -------------------- Unsupported methods --------------------
+
+    /// Fail to add a `value` to the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean add(final T value) { throw new UnsupportedOperationException("The method “add” is not supported in an immutable CopyOnWriteArraySet."); }
+
+    /// Fail to add the `values` in the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param values The (_never used_) elements to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean addAll(final @Nullable Collection<? extends T> values) { throw new UnsupportedOperationException("The method “addAll” is not supported in an immutable CopyOnWriteArraySet."); }
+
+
+    /// Fail to clear the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public void clear() { throw new UnsupportedOperationException("The method “clear” is not supported in an immutable CopyOnWriteArraySet."); }
+
+
+    /// Fail to remove a `value` in the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param value The (_never used_) element to remove
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean remove(final Object value) { throw new UnsupportedOperationException("The method “remove” is not supported in an immutable CopyOnWriteArraySet."); }
+
+    /// Fail to remove the `values` in the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param values The (_never used_) elements to remove
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean removeAll(final Collection<?> values) { throw new UnsupportedOperationException("The method “removeAll” is not supported in an immutable CopyOnWriteArraySet."); }
+
+    /// Fail to remove anything to the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param filter The (_never used_) predicate
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean removeIf(final Predicate<? super T> filter) { throw new UnsupportedOperationException("The method “removeIf” is not supported in an immutable CopyOnWriteArraySet."); }
+
+    /// Fail to keep the `values` in the current [ImmutableCopyOnWriteArraySet]
+    ///
+    /// @param values The (_never used_) values to keep
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean retainAll(final @NotNull Collection<?> values) { throw new UnsupportedOperationException("The method “retainAll” is not supported in an immutable CopyOnWriteArraySet."); }
+
+    //#endregion -------------------- Unsupported methods --------------------
+
+}
