9bfd76edd21362f0088eb5098cdb2f4bc756240d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-24 11:47:03 -0400

    [Java] Addition of an array to the extension method "toLowerCaseString"

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToLowerCaseString.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToLowerCaseString.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 import static joookiwi.collection.java.CollectionConstants.*;
 import static joookiwi.collection.java.method.AsString.asLowerCaseString;
@@ -23,7 +24,7 @@ public final class ToLowerCaseString
     //#region -------------------- Facade method --------------------
 
     /// Convert the `collection` to a [String] on every value
-    /// by calling its "_[#toLowerCase()] toLowerCase()}_" method
+    /// by calling its "_[toLowerCase\(\)][String#toLowerCase()]_" method
     ///
     /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
     /// @see String#toLowerCase()
@@ -39,7 +40,7 @@ public final class ToLowerCaseString
     }
 
     /// Convert the `collection` to a [String] on every value
-    /// by calling its "_[#toLowerCase()] toLowerCase()}_" method
+    /// by calling its "_[toLowerCase\(\)][String#toLowerCase()]_" method
     ///
     /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
     /// @see String#toLowerCase()
@@ -52,17 +53,43 @@ public final class ToLowerCaseString
         return __toString(collection, collection.size());
     }
 
+    /// Convert the `collection` to a [String] on every value
+    /// by calling its "_[toLowerCase\(\)][String#toLowerCase()]_" method
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @see String#toLowerCase()
+    @ExtensionFunction
+    public static <T> @NotNull String toLowerCaseString(final T @Nullable @Unmodifiable [] collection) {
+        if (collection == null)
+            return DEFAULT_EMPTY_COLLECTION;
+
+        var size = collection.length;
+        if (size == 0)
+            return DEFAULT_EMPTY_COLLECTION;
+        return __toString(collection, size);
+    }
+
     //#endregion -------------------- Facade method --------------------
     //#region -------------------- Loop method --------------------
 
-    private static @NotNull String __toString(final @NotNull MinimalistCollectionHolder<?> collection,
-                                              final int size) {
+    private static <T> @NotNull String __toString(final @NotNull MinimalistCollectionHolder<? extends T> collection,
+                                                  final int size) {
         var string = new StringBuilder();
         var sizeMinus1 = size - 1;
         var index = -1;
         while (++index < sizeMinus1)
             string.append(asLowerCaseString(collection.get(index))).append(DEFAULT_JOIN_SEPARATOR);
         return DEFAULT_JOIN_POSTFIX_STRING + string + asLowerCaseString(collection.get(index)) + DEFAULT_JOIN_POSTFIX;
+    }
+
+    private static <T> @NotNull String __toString(final T @NotNull @Unmodifiable [] collection,
+                                                  final int size) {
+        var string = new StringBuilder();
+        var sizeMinus1 = size - 1;
+        var index = -1;
+        while (++index < sizeMinus1)
+            string.append(asLowerCaseString(collection[index])).append(DEFAULT_JOIN_SEPARATOR);
+        return DEFAULT_JOIN_POSTFIX_STRING + string + asLowerCaseString(collection[index]) + DEFAULT_JOIN_POSTFIX;
     }
 
     //#endregion -------------------- Loop method --------------------
