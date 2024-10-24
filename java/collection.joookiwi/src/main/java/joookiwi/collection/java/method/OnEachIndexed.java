c14c087f41c88b7150e6b1edb8762da726d511d8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-24 02:46:52 -0400

    [Java] Addition of an array to the extension method "onEachIndexed"

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/OnEachIndexed.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/OnEachIndexed.java
@@ -11,6 +11,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
 import static joookiwi.collection.java.CommonContracts.ALWAYS_1ST_PARAMETER_2;
@@ -68,6 +69,28 @@ public final class OnEachIndexed
         return __with2Argument(collection, action, collection.size());
     }
 
+    /// Perform a given `action` on each element
+    /// and return the `collection` afterwards
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param action The given action
+    /// @see Iterable#forEach(Consumer)
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach">Javascript ReadonlyArray.forEach</a>
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach">Javascript ReadonlySet.forEach</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/on-each-indexed.html">Kotlin onEachIndexed(action)</a>
+    @ExtensionFunction
+    @Contract(ALWAYS_1ST_PARAMETER_2)
+    public static <T> T @Nullable @Unmodifiable [] onEachIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                 final @NotNull IntObjConsumer<? super T> action) {
+        if (collection == null)
+            return null;
+
+        final var size = collection.length;
+        if (size == 0)
+            return collection;
+        return __with2Argument(collection, action, size);
+    }
+
     //#endregion -------------------- predicate (int, T) → boolean --------------------
     //#region -------------------- predicate (int) → boolean --------------------
 
@@ -112,6 +135,29 @@ public final class OnEachIndexed
             return collection;
         return __with1Argument(collection, action, collection.size());
     }
+
+    /// Perform a given `action` on each element
+    /// and return the `collection` afterwards
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param action The given action
+    /// @see Iterable#forEach(Consumer)
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach">Javascript ReadonlyArray.forEach</a>
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach">Javascript ReadonlySet.forEach</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/on-each-indexed.html">Kotlin onEachIndexed(action)</a>
+    @ExtensionFunction
+    @Contract(ALWAYS_1ST_PARAMETER_2)
+    public static <T> T @Nullable @Unmodifiable [] onEachIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                 final @NotNull IntConsumer action) {
+        if (collection == null)
+            return null;
+
+        final var size = collection.length;
+        if (size == 0)
+            return collection;
+        return __with1Argument(collection, action, size);
+    }
+
     //#endregion -------------------- predicate (int) → boolean --------------------
     //#region -------------------- predicate () → boolean --------------------
 
@@ -156,6 +202,29 @@ public final class OnEachIndexed
             return collection;
         return __with0Argument(collection, action, collection.size());
     }
+
+    /// Perform a given `action` on each element
+    /// and return the `collection` afterwards
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param action The given action
+    /// @see Iterable#forEach(Consumer)
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach">Javascript ReadonlyArray.forEach</a>
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach">Javascript ReadonlySet.forEach</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/on-each-indexed.html">Kotlin onEachIndexed(action)</a>
+    @ExtensionFunction
+    @Contract(ALWAYS_1ST_PARAMETER_2)
+    public static <T> T @Nullable @Unmodifiable [] onEachIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                 final @NotNull Runnable action) {
+        if (collection == null)
+            return null;
+
+        final var size = collection.length;
+        if (size == 0)
+            return collection;
+        return __with0Argument(collection, action, size);
+    }
+
     //#endregion -------------------- predicate () → boolean --------------------
 
     //#endregion -------------------- Facade methods --------------------
@@ -170,6 +239,16 @@ public final class OnEachIndexed
         return collection;
     }
 
+    private static <T> T @NotNull @Unmodifiable [] __with0Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull Runnable action,
+                                                                   final int size) {
+        var index = size;
+        while (index-- > 0)
+            action.run();
+        return collection;
+    }
+
+
     private static <T, COLLECTION extends @NotNull MinimalistCollectionHolder<? extends T>> COLLECTION __with1Argument(final COLLECTION collection,
                                                                                                                        final @NotNull IntConsumer action,
                                                                                                                        final int size) {
@@ -179,12 +258,31 @@ public final class OnEachIndexed
         return collection;
     }
 
+    private static <T> T @NotNull @Unmodifiable [] __with1Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull IntConsumer action,
+                                                                   final int size) {
+        var index = -1;
+        while (++index < size)
+            action.accept(index);
+        return collection;
+    }
+
+
     private static <T, COLLECTION extends @NotNull MinimalistCollectionHolder<? extends T>> COLLECTION __with2Argument(final COLLECTION collection,
                                                                                                                        final @NotNull IntObjConsumer<? super T> action,
                                                                                                                        final int size) {
         var index = -1;
         while (++index < size)
             action.accept(index, collection.get(index));
+        return collection;
+    }
+
+    private static <T> T @NotNull @Unmodifiable [] __with2Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull IntObjConsumer<? super T> action,
+                                                                   final int size) {
+        var index = -1;
+        while (++index < size)
+            action.accept(index, collection[index]);
         return collection;
     }
 
