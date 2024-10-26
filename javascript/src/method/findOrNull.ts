2666f5cb63c2d9963236c3d4e870df6f4bb97219
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-25 20:37:27 -0400

    [JavaScript] Separation of the "firstOrNull" and "findOrNull" into a new extension method "findFirstOrNull"

---
+++ javascript/src/method/findOrNull.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    findFirstOrNull                             as findOrNull,
+    findFirstOrNullByMinimalistCollectionHolder as findOrNullByMinimalistCollectionHolder,
+    findFirstOrNullByCollectionHolder           as findOrNullByCollectionHolder,
+    findFirstOrNullByArray                      as findOrNullByArray,
+} from "./findFirstOrNull"
