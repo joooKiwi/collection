c3225537eecc1ad524352753cdeeb3a3611a72a4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-25 22:42:10 -0400

    [JavaScript] New utility method "isWeakSet" and "isWeakSetByStructure"

---
+++ javascript/src/method/isWeakSet.ts
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+/**
+ * Tell that the value is a {@link WeakSet}
+ *
+ * @param value The value to identify
+ * @see isWeakSetByStructure
+ */
+export function isWeakSet<T extends WeakKey, >(value: unknown,): value is Readonly<WeakSet<T>>
+export function isWeakSet(value: unknown,) {
+    if (value == null)
+        return false
+    if (value instanceof WeakSet)
+        return true
+    return false
+}
---
+++ javascript/src/method/isWeakSetByStructure.ts
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+import type {KeyOfWeakSet} from "../CollectionConstants.types"
+
+import {CollectionConstants} from "../CollectionConstants"
+
+/**
+ * Tell that the value has the structure of an {@link WeakSet}
+ *
+ * @param value The value to identify
+ * @see isArray
+ * @see CollectionConstants.WEAK_SET_MEMBERS
+ * @doesNotValidateTheTypes
+ */
+export function isWeakSetByStructure(value: unknown,): value is (& object & Record<KeyOfWeakSet, unknown>)
+/**
+ * Tell that the value has the structure of an {@link WeakSet}
+ *
+ * @param value The value to identify
+ * @see isArray
+ * @see CollectionConstants.WEAK_SET_MEMBERS
+ * @doesNotValidateTheTypes
+ * @note Giving a type to the method is only here to help the implementation, but it will not change the behaviour in Javascript
+ */
+export function isWeakSetByStructure<T extends WeakKey, >(value: unknown,): value is Readonly<WeakSet<T>>
+export function isWeakSetByStructure(value: unknown,) {
+    if (value == null)
+        return false
+    if (typeof value != "object")
+        return false
+
+    const members = CollectionConstants.WEAK_SET_MEMBERS
+    const size = members.length
+    let index = -1
+    while (++index < size)
+        if (!(members[index]! in value))
+            return false
+    return true
+}
