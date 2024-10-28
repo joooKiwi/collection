bf276c54092681c29497ab3b612fcfe5a41f7b12
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-28 11:41:54 -0400

    [JavaScript] Addition of alias extension method "findLastIndexIndexed"

---
+++ javascript/src/method/findLastIndexIndexed.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    indexOfLastIndexed                             as findLastIndexIndexed,
+    indexOfLastIndexedByMinimalistCollectionHolder as findLastIndexIndexedByMinimalistCollectionHolder,
+    indexOfLastIndexedByCollectionHolder           as findLastIndexIndexedByCollectionHolder,
+    indexOfLastIndexedByArray                      as findLastIndexIndexedByArray,
+} from "./indexOfLastIndexed"
