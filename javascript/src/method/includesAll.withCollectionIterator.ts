1a432dedf539c2ca59e74de6fa820bc737b0d1a1
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-31 00:00:06 -0400

    [JavaScript] Addition of an alias extension method "includesAll"

---
+++ javascript/src/method/includesAll.withCollectionIterator.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    hasAllWithCollectionIterator                             as includesAllWithCollectionIterator,
+    hasAllWithCollectionIteratorByMinimalistCollectionHolder as includesAllWithCollectionIteratorByMinimalistCollectionHolder,
+    hasAllWithCollectionIteratorByCollectionHolder           as includesAllWithCollectionIteratorByCollectionHolder,
+    hasAllWithCollectionIteratorByArray                      as includesAllWithCollectionIteratorByArray,
+} from "./hasAll.withCollectionIterator"
