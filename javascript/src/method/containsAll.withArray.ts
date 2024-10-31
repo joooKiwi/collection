064018418f1cd1326ebe6eb7383ddf809aad5d8f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-30 23:59:32 -0400

    [JavaScript] Addition of an alias extension method "containsAll"

---
+++ javascript/src/method/containsAll.withArray.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    hasAllWithArray                             as containsAllWithArray,
+    hasAllWithArrayByMinimalistCollectionHolder as containsAllWithArrayByMinimalistCollectionHolder,
+    hasAllWithArrayByCollectionHolder           as containsAllWithArrayByCollectionHolder,
+    hasAllWithArrayByArray                      as containsAllWithArrayByArray,
+} from "./hasAll.withArray"
