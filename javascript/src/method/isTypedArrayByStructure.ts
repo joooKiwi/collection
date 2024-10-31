99e0a025a40aaac3b7ad967a8c3924c39c38d5eb
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-30 23:29:39 -0400

    [JavaScript] New utility method "isTypedArrayByStructure"

---
+++ javascript/src/method/isTypedArrayByStructure.ts
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+import type {KeyOfTypedArray} from "../CollectionConstants.types"
+import type {TypedArray}      from "../CollectionHolder.types"
+
+import {CollectionConstants} from "../CollectionConstants"
+
+/**
+ * Tell that the value has the structure of an {@link TypedArray}
+ *
+ * @param value The value to identify
+ * @see isArray
+ * @see CollectionConstants.TYPED_ARRAY_MEMBERS
+ * @doesNotValidateTheTypes
+ */
+export function isTypedArrayByStructure(value: unknown,): value is (& object & Record<KeyOfTypedArray, unknown>)
+/**
+ * Tell that the value has the structure of an {@link TypedArray}
+ *
+ * @param value The value to identify
+ * @see isArray
+ * @see CollectionConstants.TYPED_ARRAY_MEMBERS
+ * @doesNotValidateTheTypes
+ * @note Giving a type to the method is only here to help the implementation, but it will not change the behaviour in Javascript
+ */
+export function isTypedArrayByStructure<const INSTANCE extends TypedArray, >(value: unknown,): value is INSTANCE
+export function isTypedArrayByStructure(value: unknown,) {
+    if (value == null)
+        return false
+    if (typeof value != "object")
+        return false
+
+    const members = CollectionConstants.TYPED_ARRAY_MEMBERS
+    const size = members.length
+    let index = -1
+    while (++index < size)
+        if (!(members[index]! in value))
+            return false
+    return true
+}
