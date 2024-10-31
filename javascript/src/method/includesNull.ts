4458935f29a6fe8f38bf7f93f9800574d462e079
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-30 23:53:21 -0400

    [JavaScript] Addition of an alias extension method "includesNull"

---
+++ javascript/src/method/includesNull.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    hasNull                             as includesNull,
+    hasNullByMinimalistCollectionHolder as includesNullByMinimalistCollectionHolder,
+    hasNullByCollectionHolder           as includesNullByCollectionHolder,
+    hasNullByArray                      as includesNullByArray,
+} from "./hasNull"
