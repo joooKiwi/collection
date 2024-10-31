4e0f6c17d3858f9a5904ba4255900f86c3112f40
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-10-30 23:54:01 -0400

    [JavaScript] Addition of an alias extension method "includesDuplicate"

---
+++ javascript/src/method/includesDuplicate.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    hasDuplicate                             as includesDuplicate,
+    hasDuplicateByMinimalistCollectionHolder as includesDuplicateByMinimalistCollectionHolder,
+    hasDuplicateByCollectionHolder           as includesDuplicateByCollectionHolder,
+    hasDuplicateByArray                      as includesDuplicateByArray,
+} from "./hasDuplicate"
