3c0755ed8b55fc2860034d8f7ff764bc0c155e2a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-01 18:30:07 -0400

    [JavaScript] Addition of an alias extension method "indexOfOrNull"

---
+++ javascript/src/method/indexOfOrNull.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    firstIndexOfOrNull                             as indexOfOrNull,
+    firstIndexOfOrNullByMinimalistCollectionHolder as indexOfOrNullByMinimalistCollectionHolder,
+    firstIndexOfOrNullByCollectionHolder           as indexOfOrNullByCollectionHolder,
+    firstIndexOfOrNullByArray                      as indexOfOrNullByArray,
+} from "./firstIndexOfOrNull"
