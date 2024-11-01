e3bbe0141c162de17ea53016c1e33372b1a99bcb
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-01 18:25:37 -0400

    [JavaScript] Addition of an alias extension method "findIndexIndexedOrNull"

---
+++ javascript/src/method/findIndexIndexedOrNull.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    indexOfFirstIndexedOrNull                             as findIndexIndexedOrNull,
+    indexOfFirstIndexedOrNullByMinimalistCollectionHolder as findIndexIndexedOrNullByMinimalistCollectionHolder,
+    indexOfFirstIndexedOrNullByCollectionHolder           as findIndexIndexedOrNullByCollectionHolder,
+    indexOfFirstIndexedOrNullByArray                      as findIndexIndexedOrNullByArray,
+} from "./indexOfFirstIndexedOrNull"
