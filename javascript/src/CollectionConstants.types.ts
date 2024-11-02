bb54db46a4175584e7fb50009f812fb7aca9290d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-01 21:34:35 -0400

    [JavaScript] Change to separate the key types

--- javascript/src/CollectionConstants.types.ts
+++ javascript/src/CollectionConstants.types.ts
@@ -10,40 +10,60 @@ import type {TypedArray}                 from "./CollectionHolder.types"
 import type {MinimalistCollectionHolder} from "./MinimalistCollectionHolder"
 import type {CollectionIterator}         from "./iterator/CollectionIterator"
 
-type ArraySymbolNames = | "iterator" | "unscopables"
+//#region -------------------- Array key --------------------
+
 /** The keys applicable to an {@link ReadonlyArray Array} */
-export type KeyOfArray = Exclude<keyof ReadonlyArray<unknown>, `[Symbol.${ArraySymbolNames}]`> | typeof Symbol[ArraySymbolNames]
+export type KeyOfArray = keyof ReadonlyArray<unknown>
 /** The keys applicable to a {@link Array MutableArray} */
-export type KeyOfMutableArray = Exclude<keyof Array<unknown>, `[Symbol.${ArraySymbolNames}]`> | typeof Symbol[ArraySymbolNames]
+export type KeyOfMutableArray = keyof Array<unknown>
 
-type TypedArraySymbolNames = | "iterator" | "species"
+//#endregion -------------------- Array key --------------------
+//#region -------------------- Typed array key --------------------
+
 /** The keys applicable to a {@link TypedArray} */
-export type KeyOfTypedArray = Exclude<keyof TypedArray, `[Symbol.${TypedArraySymbolNames}]`> | typeof Symbol[TypedArraySymbolNames]
+export type KeyOfTypedArray = keyof TypedArray
 /** The keys applicable to a {@link TypedArray Mutable TypedArray} */
-export type KeyOfMutableTypedArray = Exclude<keyof TypedArray, `[Symbol.${TypedArraySymbolNames}]`> | typeof Symbol[TypedArraySymbolNames]
+export type KeyOfMutableTypedArray = keyof TypedArray
 
-type SetSymbolNames = | "iterator" | "toStringTag"
+//#endregion -------------------- Typed array key --------------------
+//#region -------------------- Set key --------------------
+
 /** The keys applicable to a {@link ReadonlySet Set} */
-export type KeyOfSet = Exclude<keyof ReadonlySet<unknown>, `[Symbol.${SetSymbolNames}]`> | typeof Symbol[SetSymbolNames]
+export type KeyOfSet = keyof ReadonlySet<unknown>
 /** The keys applicable to a {@link Set MutableSet} */
-export type KeyOfMutableSet = Exclude<keyof Set<unknown>, `[Symbol.iterator]`> | typeof Symbol["iterator"]
-// export type KeyOfMutableSet = Exclude<keyof Set<unknown>, `[Symbol.${SetSymbolNames}]`> | typeof Symbol[SetSymbolNames]
+export type KeyOfMutableSet = keyof Set<unknown>
+
+//#endregion -------------------- Set key --------------------
+//#region -------------------- Weak set key --------------------
 
 type WeakSetSymbolNames = "toStringTag"
 /** The keys applicable to a {@link WeakSet} */
 export type KeyOfWeakSet = | "has" | typeof Symbol[WeakSetSymbolNames]
 /** The keys applicable to a {@link WeakSet MutableWeakSet} */
-export type KeyOfMutableWeakSet = Exclude<keyof WeakSet<WeakKey>, `[Symbol.${WeakSetSymbolNames}]`> | typeof Symbol[WeakSetSymbolNames]
+export type KeyOfMutableWeakSet = keyof WeakSet<WeakKey>
+
+//#endregion -------------------- Weak set key --------------------
+//#region -------------------- Minimalist collection holder key --------------------
 
 /** The keys applicable to a {@link MinimalistCollectionHolder} */
 export type KeyOfMinimalistCollectionHolder = keyof MinimalistCollectionHolder
 
+//#endregion -------------------- Minimalist collection holder key --------------------
+//#region -------------------- Collection holder key --------------------
+
 /** The keys applicable to a {@link CollectionHolder} */
 export type KeyOfCollectionHolder = keyof CollectionHolder
 
-type IteratorSymbolNames = "iterator" | "toStringTag"
+//#endregion -------------------- Collection holder key --------------------
+//#region -------------------- Iterator key --------------------
+
 /** The keys applicable to an {@link IteratorObject Iterator} */
-export type KeyOfIterator = Exclude<keyof IteratorObject<unknown>, `[Symbol.${IteratorSymbolNames}]`> | typeof Symbol[IteratorSymbolNames]
+export type KeyOfIterator = IteratorObject<unknown>
+
+//#endregion -------------------- Iterator key --------------------
+//#region -------------------- Collection iterator key --------------------
 
 /** The keys applicable to a {@link CollectionIterator} */
 export type KeyOfCollectionIterator = keyof CollectionIterator
+
+//#endregion -------------------- Collection iterator key --------------------
