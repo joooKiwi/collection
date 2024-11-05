5c05c0514d15ef1753b393c6a2590f23f33ab9d2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-05 15:23:50 -0500

    [JavaScript] Addition of new alias extension methods "limit" and "skip"

---
+++ javascript/src/method/skipWhileIndexed.ts
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+export {
+    dropWhileIndexed                             as skipWhileIndexed,
+    dropWhileIndexedByMinimalistCollectionHolder as skipWhileIndexedByMinimalistCollectionHolder,
+    dropWhileIndexedByCollectionHolder           as skipWhileIndexedByCollectionHolder,
+    dropWhileIndexedByArray                      as skipWhileIndexedByArray,
+} from "./dropWhileIndexed"
