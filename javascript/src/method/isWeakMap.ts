af46490e19487cbc0e709e7a8bae5f8b27dc13e9
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-01-08 17:59:10 -0500

    [JavaScript] Addition of a utility method "isWeakMap"

---
+++ javascript/src/method/isWeakMap.ts
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+/**
+ * Tell that the value is a {@link WeakMap}
+ *
+ * @param value The value to identify
+ * @see isWeakMapByStructure
+ * @note Giving a type to the method is only here to help the implementation, but it will not change the behaviour in JavaScript
+ */
+export function isWeakMap<const K extends WeakKey, const V, const INSTANCE extends WeakMap<K, V> = WeakMap<K, V>, >(value: unknown,): value is INSTANCE {
+    if (value == null)
+        return false
+    if (value instanceof WeakMap)
+        return true
+    return false
+}
