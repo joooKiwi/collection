78974e20372e8a7639b2aff69b3a3a8f016b8ced
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-03-27 15:27:21 -0400

    [JavaScript] Addition of an internal utility method to reduce an array size

--- javascript/src/method/_array utility.ts
+++ javascript/src/method/_array utility.ts
@@ -39,3 +39,19 @@ export function __get<const T, >(array: readonly T[], index: number,): T {
         throw new IndexOutOfBoundsException(`Index out of bound. The index “${index}” (${indexToRetrieve} after calculation) is under 0.`, index,)
     return array[indexToRetrieve] as T
 }
+
+/**
+ * Reduce the given {@link array} to a {@link newSize new size}
+ *
+ * @param array   The array to reduce its size
+ * @param newSize The new size of the {@link array} that will be returned
+ * @note It imply that {@link newSize} is under the size of the {@link array}
+ * @internal
+ */
+export function __reduceTo<const T,>(array: readonly T[], newSize: number,): readonly T[] {
+    const newArray = new Array<T>(newSize,)
+    let index = -1
+    while (++index < newSize)
+        newArray[index] = array[index] as T
+    return newArray
+}
