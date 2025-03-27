25ec2291852d42b1c1035449d2586bc77b127fe7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-03-27 14:57:25 -0400

    [Java] Addition of "dropLastWhileIndexed" as an extension method

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/DropLastWhileIndexed.java
@@ -0,0 +1,298 @@
+package joookiwi.collection.java.method;
+
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.GenericCollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.annotation.ExtensionFunction;
+import joookiwi.collection.java.callback.IntObjPredicate;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.IntPredicate;
+
+import static joookiwi.collection.java.CollectionConstants.emptyArray;
+import static joookiwi.collection.java.CollectionConstants.emptyCollectionHolder;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+public final class DropLastWhileIndexed
+        extends Utility {
+
+    @Contract(ALWAYS_FAIL_0)
+    private DropLastWhileIndexed() { throw new ImpossibleConstructionException("The utility class “DropLastWhileIndexed” cannot be constructed.", DropLastWhileIndexed.class); }
+
+    //#region -------------------- Facade methods --------------------
+
+    //#region -------------------- predicate (int, T) → boolean --------------------
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull IntObjPredicate<? super T> predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with2Argument(collection, predicate, size));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull IntObjPredicate<? super T> predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+        if (collection.isEmpty())
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with2Argument(collection, predicate, collection.size()));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                                                 final @NotNull IntObjPredicate<? super T> predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with2Argument(collection, predicate, size));
+    }
+
+    //#endregion -------------------- predicate (int, T) → boolean --------------------
+    //#region -------------------- predicate (int) → boolean --------------------
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull IntPredicate predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with1Argument(collection, predicate, size));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull IntPredicate predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+        if (collection.isEmpty())
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with1Argument(collection, predicate, collection.size()));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                                                 final @NotNull IntPredicate predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with1Argument(collection, predicate, size));
+    }
+
+    //#endregion -------------------- predicate (int) → boolean --------------------
+    //#region -------------------- predicate () → boolean --------------------
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull BooleanSupplier predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with0Argument(collection, predicate, size));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                 final @NotNull BooleanSupplier predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+        if (collection.isEmpty())
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with0Argument(collection, predicate, collection.size()));
+    }
+
+    /// Get a new [CollectionHolder] **not** having the last elements satisfying the given `predicate`
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param predicate  The given predicate
+    /// @param <T>        The `collection` type
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/drop-last-while.html">Kotlin dropLastWhile(predicate)</a>
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @NotNull CollectionHolder<T> dropLastWhileIndexed(final T @Nullable @Unmodifiable [] collection,
+                                                                                                 final @NotNull BooleanSupplier predicate) {
+        if (collection == null)
+            return emptyCollectionHolder();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyCollectionHolder();
+        return new GenericCollectionHolder<>(() -> __with0Argument(collection, predicate, size));
+    }
+
+    //#endregion -------------------- predicate () → boolean --------------------
+
+    //#endregion -------------------- Facade methods --------------------
+    //#region -------------------- Loop methods --------------------
+
+    private static <T> T @NotNull @Unmodifiable [] __with0Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
+                                                                   final @NotNull BooleanSupplier predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0)
+            if (!predicate.getAsBoolean()) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                var index2 = -1;
+                while (++index2 <= index)
+                    newArrayFrom0ToIndex[index2] = collection.get(index2);
+                return newArrayFrom0ToIndex;
+            }
+        return emptyArray();
+    }
+
+    private static <T> T @NotNull @Unmodifiable [] __with0Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull BooleanSupplier predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0)
+            if (!predicate.getAsBoolean()) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                var index2 = -1;
+                while (++index2 <= index)
+                    newArrayFrom0ToIndex[index2] = collection[index2];
+                return newArrayFrom0ToIndex;
+            }
+        return emptyArray();
+    }
+
+
+    private static <T> T @NotNull @Unmodifiable [] __with1Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
+                                                                   final @NotNull IntPredicate predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0)
+            if (!predicate.test(index)) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                var index2 = -1;
+                while (++index2 <= index)
+                    newArrayFrom0ToIndex[index2] = collection.get(index2);
+                return newArrayFrom0ToIndex;
+            }
+        return emptyArray();
+    }
+
+    private static <T> T @NotNull @Unmodifiable [] __with1Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull IntPredicate predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0)
+            if (!predicate.test(index)) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                var index2 = -1;
+                while (++index2 <= index)
+                    newArrayFrom0ToIndex[index2] = collection[index2];
+                return newArrayFrom0ToIndex;
+            }
+        return emptyArray();
+    }
+
+
+    private static <T> T @NotNull @Unmodifiable [] __with2Argument(final @NotNull MinimalistCollectionHolder<? extends T> collection,
+                                                                   final @NotNull IntObjPredicate<? super T> predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0) {
+            final var value = collection.get(index);
+            if (!predicate.test(index, value)) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                newArrayFrom0ToIndex[index] = value;
+                var index2 = -1;
+                while (++index2 < index)
+                    newArrayFrom0ToIndex[index2] = collection.get(index2);
+                return newArrayFrom0ToIndex;
+            }
+        }
+        return emptyArray();
+    }
+
+    private static <T> T @NotNull @Unmodifiable [] __with2Argument(final T @NotNull @Unmodifiable [] collection,
+                                                                   final @NotNull IntObjPredicate<? super T> predicate,
+                                                                   final int size) {
+        var index = size;
+        while (--index >= 0) {
+            final var value = collection[index];
+            if (!predicate.test(index, value)) {
+                @SuppressWarnings("unchecked cast") final var newArrayFrom0ToIndex = (T[]) new Object[index + 1];
+                newArrayFrom0ToIndex[index] = value;
+                var index2 = -1;
+                while (++index2 < index)
+                    newArrayFrom0ToIndex[index2] = collection[index2];
+                return newArrayFrom0ToIndex;
+            }
+        }
+        return emptyArray();
+    }
+
+    //#endregion -------------------- Loop methods --------------------
+
+}
