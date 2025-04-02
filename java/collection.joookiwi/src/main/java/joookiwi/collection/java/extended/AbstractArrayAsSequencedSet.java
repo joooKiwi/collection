b69aefd095df65e4a909f87ffa77a61579384184
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-02 01:02:02 -0400

    [Java] Addition of a common ancestor to the ArrayAsSequencedSet instances

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/AbstractArrayAsSequencedSet.java
@@ -0,0 +1,35 @@
+package joookiwi.collection.java.extended;
+
+import java.util.SequencedSet;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnmodifiableView;
+
+import static joookiwi.collection.java.CollectionConstants.emptySequencedSet;
+
+/// A definition of an immutable [SequencedSet] to have a common ancestor
+///
+/// @param <T> The type
+/// @see ArrayAsSequencedSet
+/// @see ReversedArrayAsSequencedSet
+@NotNullByDefault
+public abstract class AbstractArrayAsSequencedSet<T extends @Nullable Object>
+        extends AbstractArrayAsSet<T>
+        implements SequencedSet<T> {
+
+    //#region -------------------- Constructor --------------------
+
+    protected AbstractArrayAsSequencedSet() { super(); }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Methods --------------------
+
+    @Override public @UnmodifiableView SequencedSet<T> reversed() {
+        if (isEmpty())
+            return emptySequencedSet();
+        return new ReversedArrayAsSequencedSet<>(this, new ReversedArray<>(_reference()));
+    }
+
+    //#endregion -------------------- Methods --------------------
+
+}
--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ArrayAsSequencedSet.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ArrayAsSequencedSet.java
@@ -1,13 +1,13 @@
 package joookiwi.collection.java.extended;
 
-import java.util.SequencedSet;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.UnmodifiableView;
+import org.jetbrains.annotations.Range;
 
-import static joookiwi.collection.java.CollectionConstants.emptySequencedSet;
+import static java.lang.Integer.MAX_VALUE;
 
-/// A bare-bone implementation of a [java SequencedSet][SequencedSet]
+/// A bare-bone implementation of a [java SequencedSet][java.util.SequencedSet]
 /// with the [immutability][org.jetbrains.annotations.Unmodifiable] in place.
 /// During its creation, it <u>implies</u> that the array received has no duplicate.
 ///
@@ -22,18 +22,41 @@ import static joookiwi.collection.java.CollectionConstants.emptySequencedSet;
 /// @see ReversedArrayAsSequencedSet
 @NotNullByDefault
 public class ArrayAsSequencedSet<T extends @Nullable Object>
-        extends ArrayAsSet<T>
-        implements SequencedSet<T> {
+        extends AbstractArrayAsSequencedSet<T> {
 
-    /// Create an instance of a [SequencedSet] but allowing `null` in the `reference`
+    //#region -------------------- Fields --------------------
+
+    private final T[] __reference;
+
+    private final int __size;
+    private final boolean __isEmpty;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
+    /// Create an instance of a [java.util.SequencedSet]
+    /// but allowing `null`, implying that it has no duplicate in the `reference`
     ///
     /// @param reference The array to be the internal structure
-    public ArrayAsSequencedSet(final T[] reference) { super(reference); }
-
-    @Override public @UnmodifiableView SequencedSet<T> reversed() {
-        if (isEmpty())
-            return emptySequencedSet();
-        return new ReversedArrayAsSequencedSet<>(this, new ReversedArray<>(_reference()));
+    public ArrayAsSequencedSet(final T[] reference) {
+        super();
+        if (__isEmpty = (__size = (__reference = reference).length) == 0)
+            return;
+        _hashCode = 0;
     }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Methods --------------------
+
+    /// The internal reference passed through the constructor
+    @Override protected T[] _reference() { return __reference; }
+
+    @Contract(pure = true)
+    @Override public @Range(from = 0, to = MAX_VALUE) int size() { return __size; }
+
+    @Contract(pure = true)
+    @Override public boolean isEmpty() { return __isEmpty; }
+
+    //#endregion -------------------- Methods --------------------
 
 }
