b89469d61754d034ba4a50ecb9ae82ff0436ac07
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-02 12:05:19 -0400

    [Java] Addition of stored values for the immutable ArrayList instance

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ImmutableArrayList.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ImmutableArrayList.java
@@ -27,6 +27,9 @@ public class ImmutableArrayList<T extends @Nullable Object>
 
     @Serial private static final long serialVersionUID = 7943208248572134989L;
 
+    private final int __size;
+    private final boolean __isEmpty;
+
     //#endregion -------------------- Fields --------------------
     //#region -------------------- Constructors --------------------
 
@@ -35,7 +38,11 @@ public class ImmutableArrayList<T extends @Nullable Object>
     /// Create an empty [immutable-like][org.jetbrains.annotations.Unmodifiable] instance of [ArrayList]
     /// (similar to [java.util.List#of()])
     /// with a capacity of [0][joookiwi.collection.java.CollectionConstants#DEFAULT_EMPTY_INITIAL_CAPACITY]
-    public ImmutableArrayList() { super(0); }
+    public ImmutableArrayList() {
+        super(0);
+        __size = 0;
+        __isEmpty = true;
+    }
 
     //#endregion -------------------- ∅ --------------------
     //#region -------------------- values --------------------
@@ -45,8 +52,8 @@ public class ImmutableArrayList<T extends @Nullable Object>
     /// with the capacity is the `values.length`
     public ImmutableArrayList(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values) {
         super(values.length);
-        final var size = values.length;
-        if (size == 0)
+        final var size = __size = values.length;
+        if (__isEmpty = size == 0)
             return;
         var index = -1;
         while (++index < size)
@@ -58,7 +65,8 @@ public class ImmutableArrayList<T extends @Nullable Object>
     /// with the capacity is the <code>values.[size][Collection#size()]</code>
     public ImmutableArrayList(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values) {
         super(values.size());
-        if (values.isEmpty())
+        final var size = __size = values.size();
+        if (__isEmpty = size == 0)
             return;
         super.addAll(values); //TODO change to setAll or set(index, value) if it is possible
     }
@@ -66,6 +74,17 @@ public class ImmutableArrayList<T extends @Nullable Object>
     //#endregion -------------------- values --------------------
 
     //#endregion -------------------- Constructors --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Supported methods --------------------
+
+    @Contract(pure = true)
+    @Override public int size() { return __size; }
+
+    @Contract(pure = true)
+    @Override public boolean isEmpty() { return __isEmpty; }
+
+    //#endregion -------------------- Supported methods --------------------
     //#region -------------------- Unsupported methods --------------------
 
     /// Fail to mutate the [size][#size()] of the current [ImmutableArrayList]
@@ -214,5 +233,7 @@ public class ImmutableArrayList<T extends @Nullable Object>
     @Override public void sort(final @Nullable Comparator<? super T> comparator) { throw new UnsupportedOperationException("The method “sort” is not supported in an immutable ArrayList."); }
 
     //#endregion -------------------- Unsupported methods --------------------
+
+    //#endregion -------------------- Methods --------------------
 
 }
