9fc0b8b9912a50cf19eb2e6c827c0553aaea6ed0
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-08 13:09:30 -0400

    [Java] Addition of "toSortedSet" as an extension method

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToSortedSet.java
@@ -0,0 +1,305 @@
+package joookiwi.collection.java.method;
+
+import java.util.SortedSet;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.annotation.ExtensionFunction;
+import joookiwi.collection.java.callback.ObjIntFunction;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsSortedSet;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CollectionConstants.emptySortedSet;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class ToSortedSet
+        extends UtilityWithTable {
+
+    @Contract(ALWAYS_FAIL_0)
+    private ToSortedSet() { throw new ImpossibleConstructionException("The utility class “ToSortedSet” cannot be constructed.", ToSortedSet.class); }
+
+    //#region -------------------- Facade methods --------------------
+
+    //#region -------------------- ∅ --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable SortedSet<T> toSortedSet(final @Nullable MinimalistCollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptySortedSet();
+        return __withNoTransform(collection, size);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable SortedSet<T> toSortedSet(final @Nullable CollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return emptySortedSet();
+        if (collection.isEmpty())
+            return emptySortedSet();
+        if (collection.hasDuplicate())
+            return __withNoDuplicate(collection, collection.size());
+        return __withNoTransform(collection, collection.size());
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable SortedSet<T> toSortedSet(final T @Nullable @Unmodifiable [] collection) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptySortedSet();
+        return __withNoTransform(collection, size);
+    }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- (T, int) → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                  final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptySortedSet();
+        return __with2Argument(collection, size, transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                  final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+        if (collection.isEmpty())
+            return emptySortedSet();
+        return __with2Argument(collection, collection.size(), transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable T @Nullable @Unmodifiable [] collection,
+                                                                                                                  final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptySortedSet();
+        return __with2Argument(collection, size, transform);
+    }
+
+    //#endregion -------------------- (T, int) → U --------------------
+    //#region -------------------- (T) → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                  final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptySortedSet();
+        return __with1Argument(collection, size, transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                  final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+        if (collection.isEmpty())
+            return emptySortedSet();
+        return __with1Argument(collection, collection.size(), transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final T @Nullable @Unmodifiable [] collection,
+                                                                                                                  final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptySortedSet();
+        return __with1Argument(collection, size, transform);
+    }
+
+    //#endregion -------------------- (T) → U --------------------
+    //#region -------------------- () → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                  final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptySortedSet();
+        return __with0Argument(size, transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                  final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+        if (collection.isEmpty())
+            return emptySortedSet();
+        return __with0Argument(collection.size(), transform);
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [SortedSet]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> toSortedSet(final T @Nullable @Unmodifiable [] collection,
+                                                                                                                  final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptySortedSet();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptySortedSet();
+        return __with0Argument(size, transform);
+    }
+
+    //#endregion -------------------- () → U --------------------
+
+    //#endregion -------------------- Facade methods --------------------
+    //#region -------------------- Loop methods --------------------
+
+    private static <T extends @Nullable Object> @Unmodifiable SortedSet<T> __withNoDuplicate(final CollectionHolder<? extends T> collection,
+                                                                                             final int size) {
+        return new ArrayAsSortedSet<>(_orderedValues(collection, size));
+    }
+
+
+    private static <T extends @Nullable Object> @Unmodifiable SortedSet<T> __withNoTransform(final MinimalistCollectionHolder<? extends T> collection,
+                                                                                             final int size) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size));
+    }
+
+    private static <T extends @Nullable Object> @Unmodifiable SortedSet<T> __withNoTransform(final T @Unmodifiable [] collection,
+                                                                                             final int size) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size));
+    }
+
+
+    private static <U extends @Nullable Object> @Unmodifiable SortedSet<U> __with0Argument(final int size,
+                                                                                           final Supplier<? extends U> transform) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(size, transform));
+    }
+
+
+    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> __with1Argument(final MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                       final int size,
+                                                                                                                       final Function<? super T, ? extends U> transform) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size, transform));
+    }
+
+    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> __with1Argument(final T @Unmodifiable [] collection,
+                                                                                                                       final int size,
+                                                                                                                       final Function<? super T, ? extends U> transform) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size, transform));
+    }
+
+
+    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> __with2Argument(final MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                       final int size,
+                                                                                                                       final ObjIntFunction<? super T, ? extends U> transform) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size, transform));
+    }
+
+    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable SortedSet<U> __with2Argument(final T @Unmodifiable [] collection,
+                                                                                                                       final int size,
+                                                                                                                       final ObjIntFunction<? super T, ? extends U> transform) {
+        return new ArrayAsSortedSet<>(_orderedUniqueValues(collection, size, transform));
+    }
+
+    //#endregion -------------------- Loop methods --------------------
+
+}
