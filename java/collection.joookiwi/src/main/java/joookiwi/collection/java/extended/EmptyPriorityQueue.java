bc426e5b06509feae9d8485321e21e8d1bb5cd37
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-08 23:05:36 -0400

    [Java] Creation of PriorityQueue subclasses

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/EmptyPriorityQueue.java
@@ -0,0 +1,139 @@
+package joookiwi.collection.java.extended;
+
+import joookiwi.collection.java.annotation.Singleton;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+import static joookiwi.collection.java.CollectionConstants.*;
+import static joookiwi.collection.java.CommonContracts.*;
+
+/// A [Singleton] implementation of the [java.util.PriorityQueue],
+/// but in an empty form
+///
+/// @param <T> The type of the element
+/// @see joookiwi.collection.java.CollectionConstants#emptyPriorityQueue
+@NotNullByDefault
+public class EmptyPriorityQueue<T>
+        extends ImmutablePriorityQueue<T>
+        implements Cloneable {
+
+    @Serial private static final long serialVersionUID = -573062126090813871L;
+
+    //#region -------------------- Singleton usage --------------------
+
+    private static volatile @Nullable EmptyPriorityQueue<?> __instance;
+
+    protected EmptyPriorityQueue() { super(); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> EmptyPriorityQueue<T> getInstance() {
+        final var instance = __instance;
+        if (instance != null)
+            return (EmptyPriorityQueue<T>) instance;
+        synchronized (EmptyPriorityQueue.class) {
+            final var instance2 = __instance;
+            if (instance2 != null)
+                return (EmptyPriorityQueue<T>) instance2;
+            return (EmptyPriorityQueue<T>) (__instance = new EmptyPriorityQueue<>());
+        }
+    }
+
+    //#endregion -------------------- Singleton usage --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Size methods --------------------
+
+    @Contract(pure = true) @Override public @Range(from = 0, to = 0) int size() { return 0; }
+
+    @Contract(value = ALWAYS_TRUE_0, pure = true) @Override public boolean isEmpty() { return true; }
+
+    //#endregion -------------------- Size methods --------------------
+    //#region -------------------- Get methods --------------------
+
+    @Contract(value = ALWAYS_FAIL_0, pure = true) @Override public T element() { throw new NoSuchElementException("No element could be found in an empty PriorityBlockingQueue."); }
+
+    @Contract(value = ALWAYS_NULL_0, pure = true) @Override public @Nullable T peek() { return null; }
+
+    //#endregion -------------------- Get methods --------------------
+    //#region -------------------- Has methods --------------------
+
+    @Contract(value = ALWAYS_FALSE_1, pure = true) @Override public boolean contains(final @Nullable Object value) { return false; }
+
+    @Contract(value = IF_1ST_NULL_THEN_FALSE_1, pure = true) @Override public boolean containsAll(final @Nullable @Unmodifiable Collection<?> values) {
+        if (values == null)
+            return false;
+        return values.isEmpty();
+    }
+
+    //#endregion -------------------- Has methods --------------------
+    //#region -------------------- For each methods --------------------
+
+    @Contract(pure = true) @Override public void forEach(final @Nullable Consumer<? super T> action) {}
+
+    //#endregion -------------------- For each methods --------------------
+    //#region -------------------- Iterator methods --------------------
+
+    @Contract(pure = true) @Override public Iterator<T> iterator() { return emptyIterator(); }
+
+    //#endregion -------------------- Iterator methods --------------------
+    //#region -------------------- Stream methods --------------------
+
+    @Contract(pure = true) @Override public Stream<T> stream() { return emptyStream(); }
+
+    @Contract(pure = true) @Override public Stream<T> parallelStream() { return emptyParallelStream(); }
+
+    //#endregion -------------------- Stream methods --------------------
+    //#region -------------------- To array methods --------------------
+
+    @Contract(value = ALWAYS_NEW_0, pure = true) @Override public     Object[] toArray(                                ) { return new Object[0]; }
+    @Contract(value = ALWAYS_1ST_1, pure = true) @Override public <U> U[]      toArray(final U[]                  array) { return array; }
+    @Contract(pure = true)                       @Override public <U> U[]      toArray(final IntFunction<U[]> generator) { return generator.apply(0); }
+
+    //#endregion -------------------- To array methods --------------------
+    //#region -------------------- Equals methods --------------------
+
+    @Contract(pure = true) @Override public @Range(from = 0, to = 0) int hashCode() { return DEFAULT_EMPTY_HASH_CODE; }
+
+    @Contract(value = IF_1ST_NULL_THEN_FALSE_1, pure = true) @Override public boolean equals(final @Nullable Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (!(other instanceof Collection<?>))
+            return false;
+        return ((Collection<?>) other).isEmpty();
+    }
+
+    //#endregion -------------------- Equals methods --------------------
+    //#region -------------------- Comparator methods --------------------
+
+    @Contract(value = ALWAYS_NULL_0, pure = true) @Override public @Nullable Comparator<? super T> comparator() { return null; }
+
+    //#endregion -------------------- Comparator methods --------------------
+    //#region -------------------- Clone methods --------------------
+
+    @Contract(value = ALWAYS_THIS_0, pure = true) @Override public EmptyPriorityQueue<T> clone() { return this; }
+
+    //#endregion -------------------- Clone methods --------------------
+    //#region -------------------- To string methods --------------------
+
+    @Contract(pure = true) @Override public String toString() { return DEFAULT_EMPTY_COLLECTION; }
+
+    //#endregion -------------------- To string methods --------------------
+
+    //#endregion -------------------- Methods --------------------
+
+}
---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ImmutablePriorityQueue.java
@@ -0,0 +1,225 @@
+package joookiwi.collection.java.extended;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.function.Predicate;
+import org.intellij.lang.annotations.Flow;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.*;
+
+/// An [immutable-like][Unmodifiable] behaviour of a [PriorityQueue]
+///
+/// @param <T> The type of the element
+@NotNullByDefault
+public class ImmutablePriorityQueue<T>
+        extends PriorityQueue<T>
+        implements OrderableCollection<T> {
+
+    //#region -------------------- Fields --------------------
+
+    @Serial private static final long serialVersionUID = 4525892764102403648L;
+
+    private final int __size;
+    private final boolean __isEmpty;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructors --------------------
+
+    //#region -------------------- ∅ --------------------
+
+    /// Create an empty [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// with its [natural ordering][Comparator#naturalOrder]
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue() {
+        super(1, null);
+        __size = 0;
+        __isEmpty = true;
+    }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- comparator --------------------
+
+    /// Create an empty [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the `comparator` received
+    public ImmutablePriorityQueue(final @Nullable Comparator<? super T> comparator) {
+        super(1, comparator);
+        __size = 0;
+        __isEmpty = true;
+    }
+
+    //#endregion -------------------- comparator --------------------
+    //#region -------------------- values --------------------
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// with its [natural ordering][Comparator#naturalOrder]
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) T @Unmodifiable [] values) {
+        super(new ArrayAsSortedSet<>(values));
+        __isEmpty = (__size = values.length) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// with its [natural ordering][Comparator#naturalOrder]
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values) {
+        super(new ImmutableTreeSet<>(values));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the <code>values.[comparator][OrderableCollection#comparator]</code>
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable OrderableCollection<T> values) {
+        super(new ImmutableTreeSet<>(values));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the <code>values.[comparator][SortedSet#comparator]</code>
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable SortedSet<T> values) {
+        super(new ImmutableTreeSet<>(values));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the <code>values.[comparator][PriorityQueue#comparator()]</code>
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable PriorityBlockingQueue<T> values) {
+        super(new ImmutableTreeSet<>(values));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the <code>values.[comparator][PriorityBlockingQueue#comparator()]</code>
+    ///
+    /// @implNote Use a [Comparable] type on [T] to be safe
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable PriorityQueue<T> values) {
+        super(new ImmutableTreeSet<>(values));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    //#endregion -------------------- values --------------------
+    //#region -------------------- values, comparator --------------------
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the `comparator` received
+    public ImmutablePriorityQueue(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                                  final @Nullable Comparator<? super T> comparator) {
+        super(new ArrayAsSortedSet<>(values, comparator));
+        __isEmpty = (__size = values.length) == 0;
+    }
+
+    /// Create an [immutable-like][Unmodifiable] instance of [PriorityQueue]
+    /// ordered according to the `comparator` received
+    public ImmutablePriorityQueue(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                                  final @Nullable Comparator<? super T> comparator) {
+        super(new ImmutableTreeSet<>(values, comparator));
+        __isEmpty = (__size = values.size()) == 0;
+    }
+
+    //#endregion -------------------- values, comparator --------------------
+
+    //#endregion -------------------- Constructors --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Supported methods --------------------
+
+    @Contract(pure = true)
+    @Override public int size() { return __size; }
+
+    @Contract(pure = true)
+    @Override public boolean isEmpty() { return __isEmpty; }
+
+    //#endregion -------------------- Supported methods --------------------
+    //#region -------------------- Unsupported methods --------------------
+
+    /// Fail to add a `value` to the current [ImmutablePriorityQueue]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean add(final @Nullable T value) { throw new UnsupportedOperationException("The method “add” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to add the `values` in the current [ImmutablePriorityQueue]
+    ///
+    /// @param values The (_never used_) elements to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean addAll(final @Nullable @Unmodifiable Collection<? extends T> values) { throw new UnsupportedOperationException("The method “addAll” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to add a `value` to the current [ImmutablePriorityQueue]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean offer(final @Nullable T value) { throw new UnsupportedOperationException("The method “offer” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to remove and retrieve the head of the current [ImmutablePriorityQueue]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T poll() { throw new UnsupportedOperationException("The method “poll” is not supported in an immutable PriorityQueue."); }
+
+
+    /// Fail to clear the current [ImmutablePriorityQueue]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public void clear() { throw new UnsupportedOperationException("The method “clear” is not supported in an immutable PriorityQueue."); }
+
+
+    /// Fail to remove the head of the current [ImmutablePriorityQueue]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T remove() { throw new UnsupportedOperationException("The method “remove” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to remove a `value` in the current [ImmutablePriorityQueue]
+    ///
+    /// @param value The (_never used_) element to remove
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean remove(final @Nullable Object value) { throw new UnsupportedOperationException("The method “remove” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to remove the `values` in the current [ImmutablePriorityQueue]
+    ///
+    /// @param values The (_never used_) elements to remove
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean removeAll(final @Nullable @Unmodifiable Collection<?> values) { throw new UnsupportedOperationException("The method “removeAll” is not supported in an immutable PriorityQueue."); }
+
+    /// Fail to remove anything to the current [ImmutablePriorityQueue]
+    ///
+    /// @param filter The (_never used_) predicate
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean removeIf(final @Nullable Predicate<? super T> filter) { throw new UnsupportedOperationException("The method “removeIf” is not supported in an immutable PriorityQueue."); }
+
+
+    /// Fail to keep the `values` in the current [ImmutablePriorityQueue]
+    ///
+    /// @param values The (_never used_) values to keep
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean retainAll(final @Nullable @Unmodifiable Collection<?> values) { throw new UnsupportedOperationException("The method “retainAll” is not supported in an immutable PriorityQueue."); }
+
+    //#endregion -------------------- Unsupported methods --------------------
+
+    //#endregion -------------------- Methods --------------------
+
+}
