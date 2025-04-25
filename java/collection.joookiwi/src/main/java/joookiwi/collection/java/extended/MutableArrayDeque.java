506a74873bf76c5e23e8fa8abcb15f7e957460c3
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-24 22:15:13 -0400

    [Java] Addition of an initial capacity to the MutableArrayDeque argument

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/MutableArrayDeque.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/MutableArrayDeque.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Range;
 import org.jetbrains.annotations.Unmodifiable;
 
-import static java.lang.Integer.MAX_VALUE;
 import static joookiwi.collection.java.CollectionConstants.DEFAULT_INITIAL_CAPACITY;
 
 /// A mutable behaviour of a [ArrayDeque]
@@ -20,17 +19,15 @@ public class MutableArrayDeque<T>
         extends ArrayDeque<T>
         implements BasicStack<T> {
 
-    @Serial private static final long serialVersionUID = 5277029203307882850L;
+    @Serial private static final long serialVersionUID = 1870490230614814883L;
 
     //#region -------------------- Constructors --------------------
 
     //#region -------------------- ∅ --------------------
 
     /// Create an empty mutable instance of [ArrayDeque]
-    /// with a capacity of 1
-    public MutableArrayDeque() {
-        super(1);
-    }
+    /// with an initial capacity of [16][joookiwi.collection.java.CollectionConstants#DEFAULT_INITIAL_CAPACITY]
+    public MutableArrayDeque() { super(DEFAULT_INITIAL_CAPACITY); }
 
     //#endregion -------------------- ∅ --------------------
     //#region -------------------- initialCapacity --------------------
@@ -39,54 +36,220 @@ public class MutableArrayDeque<T>
     /// with the `initialCapacity` received
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) byte initialCapacity) { super(initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Byte.MAX_VALUE) byte initialCapacity) { super(initialCapacity); }
 
     /// Create a mutable instance of [ArrayDeque]
     /// with the `initialCapacity` received (_or [16][joookiwi.collection.java.CollectionConstants#DEFAULT_INITIAL_CAPACITY] if it was `null`_)
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) @Nullable Byte initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Byte.MAX_VALUE) @Nullable Byte initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
 
     /// Create a mutable instance of [ArrayDeque]
     /// with the `initialCapacity` received
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) short initialCapacity) { super(initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Short.MAX_VALUE) short initialCapacity) { super(initialCapacity); }
 
     /// Create a mutable instance of [ArrayDeque]
     /// with the `initialCapacity` received (_or [16][joookiwi.collection.java.CollectionConstants#DEFAULT_INITIAL_CAPACITY] if it was `null`_)
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) @Nullable Short initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Short.MAX_VALUE) @Nullable Short initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
 
     /// Create a mutable instance of [ArrayDeque]
     /// with the `initialCapacity` received
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) int initialCapacity) { super(initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Integer.MAX_VALUE) int initialCapacity) { super(initialCapacity); }
 
     /// Create a mutable instance of [ArrayDeque]
     /// with the `initialCapacity` received (_or [16][joookiwi.collection.java.CollectionConstants#DEFAULT_INITIAL_CAPACITY] if it was `null`_)
     ///
     /// @throws IllegalArgumentException The `initialCapacity` was negative
-    public MutableArrayDeque(final @Range(from = 0, to = MAX_VALUE) @Nullable Integer initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
+    public MutableArrayDeque(final @Range(from = 0, to = Integer.MAX_VALUE) @Nullable Integer initialCapacity) { super(initialCapacity == null ? DEFAULT_INITIAL_CAPACITY : initialCapacity); }
 
     //#endregion -------------------- initialCapacity --------------------
     //#region -------------------- values --------------------
 
     /// Create a mutable instance of [ArrayDeque]
-    /// with a capacity as the `values.length` (or `1` if it is empty)
+    /// with an initial capacity as the `values.length`
     public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values) {
         super(new ArrayAsCollection<>(values));
     }
 
     /// Create a mutable instance of [ArrayDeque]
-    /// with a capacity as the <code>values.[size][Collection#size()]</code> (or `1` if it is empty)
+    /// with an initial capacity as the <code>values.[size][Collection#size()]</code>
     public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values) {
         super(values);
     }
 
     //#endregion -------------------- values --------------------
+    //#region -------------------- values, initialCapacity --------------------
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) byte initialCapacity) {
+        super(initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    /// (if null provided, then it is the `values.length`)
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) @Nullable Byte initialCapacity) {
+        super(initialCapacity == null ? values.length : initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) short initialCapacity) {
+        super(initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    /// (if null provided, then it is the `values.length`)
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) @Nullable Short initialCapacity) {
+        super(initialCapacity == null ? values.length : initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) int initialCapacity) {
+        super(initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the `values.length`
+    /// (if null provided, then it is the `values.length`)
+    public MutableArrayDeque(final T @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable [] values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) @Nullable Integer initialCapacity) {
+        super(initialCapacity == null ? values.length : initialCapacity < values.length ? values.length : initialCapacity);
+
+        final var size = values.length;
+        if (size == 0)
+            return;
+
+        var index = -1;
+        while (++index < size)
+            addLast(values[index]);
+    }
+
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) byte initialCapacity) {
+        super(initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    /// (if null provided, then it is the <code>values.[size][Collection#size()]</code>)
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Byte.MAX_VALUE) @Nullable Byte initialCapacity) {
+        super(initialCapacity == null ? values.size() : initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Short.MAX_VALUE) short initialCapacity) {
+        super(initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    /// (if null provided, then it is the <code>values.[size][Collection#size()]</code>)
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Short.MAX_VALUE) @Nullable Short initialCapacity) {
+        super(initialCapacity == null ? values.size() : initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Integer.MAX_VALUE) int initialCapacity) {
+        super(initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    /// Create a mutable instance of [ArrayDeque]
+    /// with the largest value between the `initialCapacity` and the <code>values.[size][Collection#size()]</code>
+    /// (if null provided, then it is the <code>values.[size][Collection#size()]</code>)
+    public MutableArrayDeque(final @Flow(sourceIsContainer = true, targetIsContainer = true) @Unmodifiable Collection<? extends T> values,
+                             final @Range(from = 0, to = Integer.MAX_VALUE) @Nullable Integer initialCapacity) {
+        super(initialCapacity == null ? values.size() : initialCapacity < values.size() ? values.size() : initialCapacity);
+        if (values.isEmpty())
+            return;
+        for (final var value : values)
+            addLast(value);
+    }
+
+    //#endregion -------------------- values, initialCapacity --------------------
 
     //#endregion -------------------- Constructors --------------------
     //#region -------------------- Methods --------------------
