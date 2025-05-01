c135cb9352e18962b653ad29b2fc74acd1007b6d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-30 23:11:29 -0400

    [Java] Elevation of the loop methods into the facade method on ToMutableCopyOnWriteArrayList

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToMutableCopyOnWriteArrayList.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToMutableCopyOnWriteArrayList.java
@@ -42,7 +42,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.size();
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __withNoTransform(collection, size);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -56,7 +56,7 @@ public final class ToMutableCopyOnWriteArrayList
             return new MutableCopyOnWriteArrayList<>();
         if (collection.isEmpty())
             return new MutableCopyOnWriteArrayList<>();
-        return __withNoTransform(collection, collection.size());
+        return new MutableCopyOnWriteArrayList<>(_values(collection, collection.size()));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -72,7 +72,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.length;
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __withNoTransform(collection, size);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size));
     }
 
     //#endregion -------------------- ∅ --------------------
@@ -95,7 +95,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.size();
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with2Argument(collection, size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -113,7 +113,7 @@ public final class ToMutableCopyOnWriteArrayList
             return new MutableCopyOnWriteArrayList<>();
         if (collection.isEmpty())
             return new MutableCopyOnWriteArrayList<>();
-        return __with2Argument(collection, collection.size(), transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, collection.size(), transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -133,7 +133,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.length;
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with2Argument(collection, size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
     }
 
     //#endregion -------------------- (T, int) → U --------------------
@@ -156,7 +156,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.size();
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with1Argument(collection, size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -174,7 +174,7 @@ public final class ToMutableCopyOnWriteArrayList
             return new MutableCopyOnWriteArrayList<>();
         if (collection.isEmpty())
             return new MutableCopyOnWriteArrayList<>();
-        return __with1Argument(collection, collection.size(), transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, collection.size(), transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -194,7 +194,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.length;
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with1Argument(collection, size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
     }
 
     //#endregion -------------------- (T) → U --------------------
@@ -217,7 +217,7 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.size();
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with0Argument(size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(size, transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -235,7 +235,7 @@ public final class ToMutableCopyOnWriteArrayList
             return new MutableCopyOnWriteArrayList<>();
         if (collection.isEmpty())
             return new MutableCopyOnWriteArrayList<>();
-        return __with0Argument(collection.size(), transform);
+        return new MutableCopyOnWriteArrayList<>(_values(collection.size(), transform));
     }
 
     /// Convert the `collection` to a new mutable [CopyOnWriteArrayList]
@@ -255,56 +255,11 @@ public final class ToMutableCopyOnWriteArrayList
         final var size = collection.length;
         if (size == 0)
             return new MutableCopyOnWriteArrayList<>();
-        return __with0Argument(size, transform);
+        return new MutableCopyOnWriteArrayList<>(_values(size, transform));
     }
 
     //#endregion -------------------- () → U --------------------
 
     //#endregion -------------------- Facade methods --------------------
-    //#region -------------------- Loop methods --------------------
-
-    private static <T extends @Nullable Object> CopyOnWriteArrayList<T> __withNoTransform(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                          final int size) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size));
-    }
-
-    private static <T extends @Nullable Object> CopyOnWriteArrayList<T> __withNoTransform(final T @Unmodifiable [] collection,
-                                                                                          final int size) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size));
-    }
-
-
-    private static <U extends @Nullable Object> CopyOnWriteArrayList<U> __with0Argument(final int size,
-                                                                                        final Supplier<? extends U> transform) {
-        return new MutableCopyOnWriteArrayList<>(_values(size, transform));
-    }
-
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> CopyOnWriteArrayList<U> __with1Argument(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                                                    final int size,
-                                                                                                                    final Function<? super T, ? extends U> transform) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
-    }
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> CopyOnWriteArrayList<U> __with1Argument(final T @Unmodifiable [] collection,
-                                                                                                                    final int size,
-                                                                                                                    final Function<? super T, ? extends U> transform) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
-    }
-
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> CopyOnWriteArrayList<U> __with2Argument(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                                                    final int size,
-                                                                                                                    final ObjIntFunction<? super T, ? extends U> transform) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
-    }
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> CopyOnWriteArrayList<U> __with2Argument(final T @Unmodifiable [] collection,
-                                                                                                                    final int size,
-                                                                                                                    final ObjIntFunction<? super T, ? extends U> transform) {
-        return new MutableCopyOnWriteArrayList<>(_values(collection, size, transform));
-    }
-
-    //#endregion -------------------- Loop methods --------------------
 
 }
