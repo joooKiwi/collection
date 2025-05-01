1be7ed7bf34dc2774360d0f85c60763606a05433
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-04-30 23:19:23 -0400

    [Java] Elevation of the loop methods into the facade method on ToSet

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToSet.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToSet.java
@@ -40,7 +40,7 @@ public final class ToSet
         final var size = collection.size();
         if (size == 0)
             return emptySet();
-        return __withNoTransform(collection, size);
+        return new ArrayAsSet<>(_uniqueValues(collection, size));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -54,8 +54,8 @@ public final class ToSet
         if (collection.isEmpty())
             return emptySet();
         if (collection.hasDuplicate())
-            return __withNoDuplicate(collection, collection.size());
-        return __withNoTransform(collection, collection.size());
+            return new ArrayAsSet<>(_values(collection, collection.size()));
+        return new ArrayAsSet<>(_uniqueValues(collection, collection.size()));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -70,7 +70,7 @@ public final class ToSet
         final var size = collection.length;
         if (size == 0)
             return emptySet();
-        return __withNoTransform(collection, size);
+        return new ArrayAsSet<>(_uniqueValues(collection, size));
     }
 
     //#endregion -------------------- ∅ --------------------
@@ -92,7 +92,7 @@ public final class ToSet
         final var size = collection.size();
         if (size == 0)
             return emptySet();
-        return __with2Argument(collection, size, transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, size, transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -109,7 +109,7 @@ public final class ToSet
             return emptySet();
         if (collection.isEmpty())
             return emptySet();
-        return __with2Argument(collection, collection.size(), transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, collection.size(), transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -128,7 +128,7 @@ public final class ToSet
         final var size = collection.length;
         if (size == 0)
             return emptySet();
-        return __with2Argument(collection, size, transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, size, transform));
     }
 
     //#endregion -------------------- (T, int) → U --------------------
@@ -150,7 +150,7 @@ public final class ToSet
         final var size = collection.size();
         if (size == 0)
             return emptySet();
-        return __with1Argument(collection, size, transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, size, transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -167,7 +167,7 @@ public final class ToSet
             return emptySet();
         if (collection.isEmpty())
             return emptySet();
-        return __with1Argument(collection, collection.size(), transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, collection.size(), transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -186,7 +186,7 @@ public final class ToSet
         final var size = collection.length;
         if (size == 0)
             return emptySet();
-        return __with1Argument(collection, size, transform);
+        return new ArrayAsSet<>(_uniqueValues(collection, size, transform));
     }
 
     //#endregion -------------------- (T) → U --------------------
@@ -208,7 +208,7 @@ public final class ToSet
         final var size = collection.size();
         if (size == 0)
             return emptySet();
-        return __with0Argument(size, transform);
+        return new ArrayAsSet<>(_uniqueValues(size, transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -225,7 +225,7 @@ public final class ToSet
             return emptySet();
         if (collection.isEmpty())
             return emptySet();
-        return __with0Argument(collection.size(), transform);
+        return new ArrayAsSet<>(_uniqueValues(collection.size(), transform));
     }
 
     /// Convert the `collection` to an [immutable][Unmodifiable] [Set]
@@ -244,62 +244,11 @@ public final class ToSet
         final var size = collection.length;
         if (size == 0)
             return emptySet();
-        return __with0Argument(size, transform);
+        return new ArrayAsSet<>(_uniqueValues(size, transform));
     }
 
     //#endregion -------------------- () → U --------------------
 
     //#endregion -------------------- Facade methods --------------------
-    //#region -------------------- Loop methods --------------------
-
-    private static <T extends @Nullable Object> @Unmodifiable Set<T> __withNoDuplicate(final CollectionHolder<? extends T> collection,
-                                                                                       final int size) {
-        return new ArrayAsSet<>(_values(collection, size));
-    }
-
-
-    private static <T extends @Nullable Object> @Unmodifiable Set<T> __withNoTransform(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                       final int size) {
-        return new ArrayAsSet<>(_uniqueValues(collection, size));
-    }
-
-    private static <T extends @Nullable Object> @Unmodifiable Set<T> __withNoTransform(final T @Unmodifiable [] collection,
-                                                                                       final int size) {
-        return new ArrayAsSet<>(_uniqueValues(collection, size));
-    }
-
-
-    private static <U extends @Nullable Object> @Unmodifiable Set<U> __with0Argument(final int size,
-                                                                                     final Supplier<? extends U> transform) {
-        return new ArrayAsSet<>(_values(size, transform));
-    }
-
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Set<U> __with1Argument(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                                                 final int size,
-                                                                                                                 final Function<? super T, ? extends U> transform) {
-        return new ArrayAsSet<>(_values(collection, size, transform));
-    }
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Set<U> __with1Argument(final T @Unmodifiable [] collection,
-                                                                                                                 final int size,
-                                                                                                                 final Function<? super T, ? extends U> transform) {
-        return new ArrayAsSet<>(_values(collection, size, transform));
-    }
-
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Set<U> __with2Argument(final MinimalistCollectionHolder<? extends T> collection,
-                                                                                                                 final int size,
-                                                                                                                 final ObjIntFunction<? super T, ? extends U> transform) {
-        return new ArrayAsSet<>(_values(collection, size, transform));
-    }
-
-    private static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Set<U> __with2Argument(final T @Unmodifiable [] collection,
-                                                                                                                 final int size,
-                                                                                                                 final ObjIntFunction<? super T, ? extends U> transform) {
-        return new ArrayAsSet<>(_values(collection, size, transform));
-    }
-
-    //#endregion -------------------- Loop methods --------------------
 
 }
