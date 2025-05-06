06617ae76a61da8bae2dc9abd3738cd0efa7ea4b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-05-06 15:14:43 -0400

    [Java] Addition of a class to imitate the BlockingQueue as an immutable variant

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ArrayAsBlockingQueue.java
@@ -0,0 +1,64 @@
+package joookiwi.collection.java.extended;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Range;
+
+import static java.lang.Integer.MAX_VALUE;
+
+/// A bare-bone implementation of a [java Collection][java.util.concurrent.BlockingQueue]
+/// with the [immutability][org.jetbrains.annotations.Unmodifiable] in place.
+/// During its creation, it <u>implies</u> that the array received has no duplicate.
+///
+/// Note that `null` is permitted in this instance.
+/// It is up to the implementor to specify it.
+///
+/// The instance uses the [methods][joookiwi.collection.java.method]
+/// to give similar implementation to the [joookiwi.collection.java.CollectionHolder]
+/// when possible.
+///
+/// @param <T> The type
+@NotNullByDefault
+public class ArrayAsBlockingQueue<T>
+        extends AbstractArrayAsBlockingQueue<T> {
+
+    //#region -------------------- Fields --------------------
+
+    private final T[] __reference;
+
+    private final int __size;
+    private final boolean __isEmpty;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
+    /// Create an instance of a [java.util.concurrent.BlockingQueue] and allowing `null` in the `reference`
+    ///
+    /// @param reference The array to be the internal structure
+    public ArrayAsBlockingQueue(final T[] reference) {
+        super();
+        if (__isEmpty = (__size = (__reference = reference).length) == 0)
+            return;
+        _hashCode = 0;
+    }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Methods --------------------
+
+    /// The internal reference passed through the constructor
+    @Override protected T[] _reference() { return __reference; }
+
+    @Contract(pure = true)
+    @Override public @Range(from = 0, to = MAX_VALUE) int size() { return __size; }
+
+    @Contract(pure = true)
+    @Override public boolean isEmpty() { return __isEmpty; }
+
+    @Contract(pure = true)
+    @Override public @Range(from = 0, to = 0) int remainingCapacity() { return 0; }
+
+    @Override public ArrayAsBlockingQueue<T> clone() { return new ArrayAsBlockingQueue<>(_reference().clone()); }
+
+    //#endregion -------------------- Methods --------------------
+
+}
