8c872073d977216a135f41063d6a88e90ee7d4df
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-05-06 15:11:48 -0400

    [Java] Addition of a class to imitate the Queue, but with possible null and as immutable

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/AbstractArrayAsQueue.java
@@ -0,0 +1,77 @@
+package joookiwi.collection.java.extended;
+
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import joookiwi.collection.java.exception.EmptyCollectionException;
+import joookiwi.collection.java.method.GetFirst;
+import joookiwi.collection.java.method.GetFirstOrNull;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_1;
+
+/// A definition of an immutable [Queue] to have a common ancestor.
+/// This class is similar to [java.util.AbstractQueue] but for array specifically.
+///
+/// @param <T> The type
+/// @see ArrayAsQueue
+@NotNullByDefault
+public abstract class AbstractArrayAsQueue<T extends @Nullable Object>
+        extends AbstractArrayAsCollection<T>
+        implements Queue<T> {
+
+    //#region -------------------- Constructor --------------------
+
+    protected AbstractArrayAsQueue() { super(); }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Supported methods --------------------
+
+    //#region -------------------- Get methods --------------------
+
+    @Contract(pure = true)
+    @Override public T element() {
+        if (isEmpty())
+            throw new NoSuchElementException(new EmptyCollectionException());
+        return GetFirst.getFirst(_reference());
+    }
+
+    @Contract(pure = true)
+    @Override public @Nullable T peek() { return GetFirstOrNull.getFirstOrNull(_reference()); }
+
+    //#endregion -------------------- Get methods --------------------
+
+    @Override public abstract AbstractArrayAsQueue<T> clone();
+
+    //#endregion -------------------- Supported methods --------------------
+    //#region -------------------- Unsupported methods --------------------
+
+    /// Fail to add a `value` to the current [AbstractArrayAsQueue]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public boolean offer(final @Nullable T value) { throw new UnsupportedOperationException("The method “offer” is not supported in an immutable Queue."); }
+
+    /// Fail to remove and retrieve the head of the current [AbstractArrayAsQueue]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T poll() { throw new UnsupportedOperationException("The method “poll” is not supported in an immutable Queue."); }
+
+
+    /// Fail to remove the head of the current [AbstractArrayAsQueue]
+    ///
+    /// @throws UnsupportedOperationException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T remove() { throw new UnsupportedOperationException("The method “remove” is not supported in an immutable Queue."); }
+
+    //#endregion -------------------- Unsupported methods --------------------
+
+    //#endregion -------------------- Methods --------------------
+
+}
