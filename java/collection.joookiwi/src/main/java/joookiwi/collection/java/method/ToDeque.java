94632242c0e4951d882a687b4b7486df65be5e2d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-05-06 23:15:41 -0400

    [Java] Addition of "toDeque" as an extension method

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToDeque.java
@@ -0,0 +1,252 @@
+package joookiwi.collection.java.method;
+
+import java.util.Deque;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.annotation.ExtensionFunction;
+import joookiwi.collection.java.callback.ObjIntFunction;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsDeque;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CollectionConstants.emptyDeque;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class ToDeque
+        extends UtilityWithTable {
+
+    @Contract(ALWAYS_FAIL_0)
+    private ToDeque() { throw new ImpossibleConstructionException("The utility class “ToDeque” cannot be constructed.", ToDeque.class); }
+
+    //#region -------------------- Facade methods --------------------
+
+    //#region -------------------- ∅ --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable Deque<T> toDeque(final @Nullable MinimalistCollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable Deque<T> toDeque(final @Nullable CollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return emptyDeque();
+        if (collection.isEmpty())
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, collection.size()));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    public static <T extends @Nullable Object> @Unmodifiable Deque<T> toDeque(final T @Nullable @Unmodifiable [] collection) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size));
+    }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- (T, int) → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                          final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size, transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                          final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+        if (collection.isEmpty())
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, collection.size(), transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final T @Nullable @Unmodifiable [] collection,
+                                                                                                          final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size, transform));
+    }
+
+    //#endregion -------------------- (T, int) → U --------------------
+    //#region -------------------- (T) → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                          final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size, transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                          final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+        if (collection.isEmpty())
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, collection.size(), transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T extends @Nullable Object, U extends @Nullable Object> @Unmodifiable Deque<U> toDeque(final T @Nullable @Unmodifiable [] collection,
+                                                                                                          final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection, size, transform));
+    }
+
+    //#endregion -------------------- (T) → U --------------------
+    //#region -------------------- () → U --------------------
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T, U> @Unmodifiable Deque<U> toDeque(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                        final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.size();
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(size, transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T, U> @Unmodifiable Deque<U> toDeque(final @Nullable CollectionHolder<? extends T> collection,
+                                                        final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+        if (collection.isEmpty())
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(collection.size(), transform));
+    }
+
+    /// Convert the `collection` to an [immutable][Unmodifiable] [Deque]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    public static <T, U> @Unmodifiable Deque<U> toDeque(final T @Nullable @Unmodifiable [] collection,
+                                                        final Supplier<? extends U> transform) {
+        if (collection == null)
+            return emptyDeque();
+
+        final var size = collection.length;
+        if (size == 0)
+            return emptyDeque();
+        return new ArrayAsDeque<>(_values(size, transform));
+    }
+
+    //#endregion -------------------- () → U --------------------
+
+    //#endregion -------------------- Facade methods --------------------
+
+}
