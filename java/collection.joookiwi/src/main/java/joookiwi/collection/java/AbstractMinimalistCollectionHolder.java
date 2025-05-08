64315a814c58b90bc96b8a16c04c8b930dda7e5b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-05-08 16:23:58 -0400

    [Java] Addition of the AbstractMinimalistCollectionHolder

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/AbstractMinimalistCollectionHolder.java
@@ -0,0 +1,15 @@
+package joookiwi.collection.java;
+
+import org.jetbrains.annotations.Nullable;
+
+/// A definition of a [MinimalistCollectionHolder] to have a common ancestor
+///
+/// @param <T> The type
+/// @see EmptyCollectionHolder
+/// @see GenericMinimalistCollectionHolder
+public abstract class AbstractMinimalistCollectionHolder<T extends @Nullable Object>
+        implements MinimalistCollectionHolder<T> {
+
+    protected AbstractMinimalistCollectionHolder() { super(); }
+
+}
