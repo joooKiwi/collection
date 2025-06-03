bd34c6f0b5c90274f255ce07927cbebfa5c49dbe
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-06-03 12:20:15 -0400

    [Java] Addition of the missing methods in the extended SequencedSet

--- java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/AbstractArrayAsSequencedSet.java
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/AbstractArrayAsSequencedSet.java
@@ -32,9 +32,23 @@ public abstract class AbstractArrayAsSequencedSet<T extends @Nullable Object>
 
     //#region -------------------- Supported methods --------------------
 
+    //#region -------------------- Get methods --------------------
+
+    @Override public T getFirst() { return UtilityForArray.getFirst(_reference()); }
+
+    @Override public T getLast() { return UtilityForArray.getLast(_reference()); }
+
+    //#endregion -------------------- Get methods --------------------
+    //#region -------------------- As reverse methods --------------------
+
     @Override public @UnmodifiableView SequencedSet<T> reversed() { return UtilityForArray.asReversed(this, _reference()); }
 
+    //#endregion -------------------- As reverse methods --------------------
+    //#region -------------------- Clone methods --------------------
+
     @Override public abstract AbstractArrayAsSequencedSet<T> clone();
+
+    //#endregion -------------------- Clone methods --------------------
 
     //#endregion -------------------- Supported methods --------------------
     //#region -------------------- Unsupported methods --------------------
@@ -45,6 +59,20 @@ public abstract class AbstractArrayAsSequencedSet<T extends @Nullable Object>
     /// @throws UnsupportedMethodException The method is not supported
     @Contract(ALWAYS_FAIL_1)
     @Override public boolean add(final @Nullable T value) { throw new UnsupportedMethodException("The method “add” is not supported in an immutable SequencedSet."); }
+
+    /// Fail to add a `value` to the current [instance][SequencedSet]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedMethodException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public void addFirst(final @Nullable T value) { throw new UnsupportedMethodException("The method “addFirst” is not supported in an immutable empty SequencedSet."); }
+
+    /// Fail to add a `value` to the current [instance][SequencedSet]
+    ///
+    /// @param value The (_never used_) element to add
+    /// @throws UnsupportedMethodException The method is not supported
+    @Contract(ALWAYS_FAIL_1)
+    @Override public void addLast(final @Nullable T value) { throw new UnsupportedMethodException("The method “addLast” is not supported in an immutable empty SequencedSet."); }
 
     /// Fail to add the `values` in the current [instance][SequencedSet]
     ///
@@ -67,6 +95,18 @@ public abstract class AbstractArrayAsSequencedSet<T extends @Nullable Object>
     /// @throws UnsupportedMethodException The method is not supported
     @Contract(ALWAYS_FAIL_1)
     @Override public boolean remove(final @Nullable Object value) { throw new UnsupportedMethodException("The method “remove” is not supported in an immutable SequencedSet."); }
+
+    /// Fail to remove the 1<sup>st</sup> value in the current [instance][SequencedSet]
+    ///
+    /// @throws UnsupportedMethodException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T removeFirst() { throw new UnsupportedMethodException("The method “removeFirst” is not supported in an immutable empty SequencedSet."); }
+
+    /// Fail to remove the last value in the current [instance][SequencedSet]
+    ///
+    /// @throws UnsupportedMethodException The method is not supported
+    @Contract(ALWAYS_FAIL_0)
+    @Override public T removeLast() { throw new UnsupportedMethodException("The method “removeLast” is not supported in an immutable empty SequencedSet."); }
 
     /// Fail to remove the `values` in the current [instance][SequencedSet]
     ///
