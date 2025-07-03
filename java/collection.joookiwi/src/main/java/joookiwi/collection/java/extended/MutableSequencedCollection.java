b7fee0a6aa89c30e7d61abb13dd3a5b96a67f35e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-07-02 22:26:05 -0400

    [Java] Addition of a class to imitate the Collection, but with possible null and as mutable

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/MutableSequencedCollection.java
@@ -0,0 +1,75 @@
+package joookiwi.collection.java.extended;
+
+import java.util.NoSuchElementException;
+import java.util.SequencedCollection;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+@NotNullByDefault
+public interface MutableSequencedCollection<T extends @Nullable Object>
+        extends MutableCollection<T>,
+                SequencedCollection<T> {
+
+    //#region -------------------- Get methods --------------------
+
+    /// Get the first element in the current [instance][MutableSequencedCollection]
+    ///
+    /// @throws NoSuchElementException The current [instance][MutableSequencedCollection] [is empty][#isEmpty]
+    /// @see java.util.SequencedCollection#getFirst()
+    /// @see java.util.SequencedSet#getFirst()
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/first.html">Kotlin first()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.first">C# First()</a>
+    @Override T getFirst();
+
+    /// Get the last element in the current [instance][MutableSequencedCollection]
+    ///
+    /// @throws NoSuchElementException The current [instance][MutableSequencedCollection] [is empty][#isEmpty]
+    /// @see java.util.SequencedCollection#getLast()
+    /// @see java.util.SequencedSet#getLast()
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/last.html">Kotlin last()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.last">C# Last()</a>
+    @Override T getLast();
+
+    //#endregion -------------------- Get methods --------------------
+    //#region -------------------- Add methods --------------------
+
+    @Contract(mutates = "this")
+    @Override void addFirst(T value);
+
+    @Contract(mutates = "this")
+    @Override void addLast(T value);
+
+    //#endregion -------------------- Add methods --------------------
+    //#region -------------------- Remove methods --------------------
+
+    @Contract(mutates = "this")
+    @Override T removeFirst();
+
+    @Contract(mutates = "this")
+    @Override T removeLast();
+
+    //#endregion -------------------- Remove methods --------------------
+    //#region -------------------- As reverse methods --------------------
+
+    /// Give a reversed-view of the current [instance][MutableSequencedCollection]
+    ///
+    /// @see java.util.SequencedCollection#reversed()
+    /// @see java.util.List#reversed()
+    /// @see java.util.SequencedSet#reversed()
+    /// @see java.util.Deque#reversed()
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse">Javascript MutableArray.reverse()</a>
+    /// @see <a href="https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/toReversed">Javascript Array.toReversed()</a>
+    /// @see <a href="https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/reverse.html">Kotlin reverse()</a>
+    /// @see <a href="https://learn.microsoft.com/dotnet/api/system.linq.enumerable.reverse">C# Reverse()</a>
+    @Override MutableSequencedCollection<T> reversed();
+
+    //#endregion -------------------- As reverse methods --------------------
+    //#region -------------------- Clone methods --------------------
+
+    @Override MutableSequencedCollection<T> clone();
+
+    //#endregion -------------------- Clone methods --------------------
+
+}
---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/ReversedArrayAsMutableSequencedCollection.java
@@ -0,0 +1,74 @@
+package joookiwi.collection.java.extended;
+
+import java.util.SequencedCollection;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_NEW_0;
+
+/// An implementation of a reversed-order [SequencedCollection] similar to the [ArrayAsMutableSequencedCollection] in its behaviour.
+///
+/// Note that `null` is permitted in this instance.
+/// It is up to the implementor to specify it.
+///
+/// The instance uses the [methods][joookiwi.collection.java.method]
+/// to give similar implementation to the [joookiwi.collection.java.CollectionHolder]
+/// when possible.
+///
+/// @param <T>               The type
+/// @param <SOURCE>          The original source of the instance
+///                          (generally a [ArrayAsMutableSequencedCollection] or [ReversedArrayAsMutableSequencedCollection])
+/// @param <REVERSED_ARRAY>> The array that should contain the new reference
+/// @see ArrayAsMutableSequencedCollection
+@NotNullByDefault
+public class ReversedArrayAsMutableSequencedCollection<T extends @Nullable Object,
+        SOURCE extends SequencedCollection<? super T>,
+        REVERSED_ARRAY extends ReversedMutableArray<T>>
+        extends AbstractArrayAsMutableSequencedCollection<T> {
+
+    //#region -------------------- Fields --------------------
+
+    private final SOURCE __source;
+    private final REVERSED_ARRAY __reversedArray;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
+    public ReversedArrayAsMutableSequencedCollection(final SOURCE source,
+                                                     final REVERSED_ARRAY reversedArray) {
+        __source = source;
+        __reversedArray = reversedArray;
+    }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Getter / setter methods --------------------
+
+    /// The source passed through the constructor
+    @Contract(pure = true)
+    protected SOURCE _source() { return __source; }
+
+    /// The [ReversedMutableArray] passed through the constructor
+    @Contract(pure = true)
+    protected REVERSED_ARRAY _reversedArray() { return __reversedArray; }
+
+    /// The internal referenced given from the [reversed-array][#_reversedArray] [source][ReversedMutableArray#reversedSource()]
+    @Contract(ALWAYS_NEW_0)
+    @Override protected T[] _reference() { return _reversedArray().reversedSource(); }
+
+    /// Change the internal reference in the [reversed-array][#_reversedArray] [source][ReversedMutableArray#reversedSource(Object\[\])]
+    ///
+    /// @param value The value to reverse and put in replacement on internal reference
+    @Override protected void _reference(final T @Unmodifiable [] value) { _reversedArray().reversedSource(value); }
+
+    //#endregion -------------------- Getter / setter methods --------------------
+    //#region -------------------- Methods --------------------
+
+    @MustBeInvokedByOverriders
+    @Override public ReversedArrayAsMutableSequencedCollection<T, SOURCE, REVERSED_ARRAY> clone() { return (ReversedArrayAsMutableSequencedCollection<T, SOURCE, REVERSED_ARRAY>) super.clone(); }
+
+    //#endregion -------------------- Methods --------------------
+
+}
