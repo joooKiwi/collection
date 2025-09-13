972a09c576001cd6f042218f73f679352c7e8859
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-09-12 23:45:21 -0400

    [Java] Addition of "toMutableCollection" as an extension method

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/ToMutableCollection.java
@@ -0,0 +1,265 @@
+package joookiwi.collection.java.method;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.annotation.ExtensionFunction;
+import joookiwi.collection.java.callback.ObjIntFunction;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsMutableCollection;
+import joookiwi.collection.java.extended.MutableCollection;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_NEW_1;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_NEW_2;
+
+@NotNullByDefault
+public final class ToMutableCollection
+        extends UtilityWithTable {
+
+    @Contract(ALWAYS_FAIL_0)
+    private ToMutableCollection() { throw new ImpossibleConstructionException("The utility class “ToMutableCollection” cannot be constructed.", ToMutableCollection.class); }
+
+    //#region -------------------- Facade methods --------------------
+
+    //#region -------------------- ∅ --------------------
+
+    /// Convert the `collection` to a [MutableCollection]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_1)
+    public static <T extends @Nullable Object> MutableCollection<T> toMutableCollection(final @Nullable MinimalistCollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.size();
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_1)
+    public static <T extends @Nullable Object> MutableCollection<T> toMutableCollection(final @Nullable CollectionHolder<? extends T> collection) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+        if (collection.isEmpty())
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, collection.size()));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param <T>        The `collection` type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_1)
+    public static <T extends @Nullable Object> MutableCollection<T> toMutableCollection(final T @Nullable @Unmodifiable [] collection) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.length;
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size));
+    }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- (T, int) → U --------------------
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                    final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.size();
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size, transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                    final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+        if (collection.isEmpty())
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, collection.size(), transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final T @Nullable @Unmodifiable [] collection,
+                                                                                                                    final ObjIntFunction<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.length;
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size, transform));
+    }
+
+    //#endregion -------------------- (T, int) → U --------------------
+    //#region -------------------- (T) → U --------------------
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                    final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.size();
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size, transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                    final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+        if (collection.isEmpty())
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, collection.size(), transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final T @Nullable @Unmodifiable [] collection,
+                                                                                                                    final Function<? super T, ? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.length;
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection, size, transform));
+    }
+
+    //#endregion -------------------- (T) → U --------------------
+    //#region -------------------- () → U --------------------
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][MinimalistCollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable MinimalistCollectionHolder<? extends T> collection,
+                                                                                                                    final Supplier<? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.size();
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(size, transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] [collection][CollectionHolder]
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final @Nullable CollectionHolder<? extends T> collection,
+                                                                                                                    final Supplier<? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+        if (collection.isEmpty())
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(collection.size(), transform));
+    }
+
+    /// Convert the `collection` to a [MutableCollection]
+    /// applying a transformation
+    ///
+    /// @param collection The [nullable][Nullable] collection
+    /// @param transform  The given transform
+    /// @param <T>        The `collection` type
+    /// @param <U>        The new type
+    @ExtensionFunction
+    @Contract(ALWAYS_NEW_2)
+    public static <T extends @Nullable Object, U extends @Nullable Object> MutableCollection<U> toMutableCollection(final T @Nullable @Unmodifiable [] collection,
+                                                                                                                    final Supplier<? extends U> transform) {
+        if (collection == null)
+            return new ArrayAsMutableCollection<>();
+
+        final var size = collection.length;
+        if (size == 0)
+            return new ArrayAsMutableCollection<>();
+        return new ArrayAsMutableCollection<>(_values(size, transform));
+    }
+
+    //#endregion -------------------- () → U --------------------
+
+    //#endregion -------------------- Facade methods --------------------
+
+}
