453d61ee811bf4398e7229844a24284fcf95b0cc
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-10-09 16:58:29 -0400

    [Java] Creation of an GenericMinimalistCollectionHolder test instance

---
+++ java/collection.joookiwi/src/test/java/instance/CollectionHolder_ByMinimalistCollection.java
@@ -0,0 +1,48 @@
+package instance;
+
+import joookiwi.collection.java.GenericMinimalistCollectionHolder;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.NumericConstants.MAX_INT_VALUE;
+
+/// A class to test the functionality of a [GenericMinimalistCollectionHolder]
+/// for both [GenericMinimalistCollectionHolder#size]
+/// and [GenericMinimalistCollectionHolder#get]
+///
+/// The remaining methods are from the extension methods for a [joookiwi.collection.java.MinimalistCollectionHolder]
+///
+/// @param <T> The type
+@NotNullByDefault
+public final class CollectionHolder_ByMinimalistCollection<T extends @Nullable Object>
+        extends AbstractMinimalistCollectionHolderForTest<T, CollectionHolder_ByMinimalistCollection<T>> {
+
+    /// Tell that the instance is of the type [MinimalistCollectionHolder][joookiwi.collection.java.MinimalistCollectionHolder] directly
+    public static final boolean IS_MINIMALIST = true;
+    /// Tell that the instance is **not** based on the extension methods directly
+    public static final boolean IS_EXTENSION = false;
+    /// The simplified named to be used in the test “ParameterizedTest” or “ParameterizedClass”
+    public static final String SIMPLIFIED_NAME = "minimalist";
+
+    /// The internal instance that is tested
+    public final GenericMinimalistCollectionHolder<T> instance;
+
+    public CollectionHolder_ByMinimalistCollection(final T @Unmodifiable [] array) {
+        super(array);
+        instance = new GenericMinimalistCollectionHolder<>(array) {
+
+            @Override public T get(final int index) {
+                amountOfCall++;
+                return super.get(index);
+            }
+
+        };
+    }
+
+    @Override public @Range(from = 0, to = MAX_INT_VALUE) int size() { return instance.size(); }
+
+    @Override public T get(final int index) { return instance.get(index); }
+
+}
