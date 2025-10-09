512acf4db19fbacb64c4a3037a6f6780e773d626
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-10-09 16:52:37 -0400

    [Java] Creation of general instances for the test similar to the JavaScript instances

---
+++ java/collection.joookiwi/src/test/java/instance/StraightCollectionHolderForTest.java
@@ -0,0 +1,36 @@
+package instance;
+
+import joookiwi.collection.java.CollectionHolder;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
+
+import java.util.function.Consumer;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_THIS_1;
+import static joookiwi.collection.java.NumericConstants.MAX_INT_VALUE;
+
+/// A [CollectionHolder] that has the minimum utility methods for a test
+/// and is watching the amount of time a specific method is being called for the instance
+///
+///  @param <T>       The type
+/// @param <INSTANCE> The current instance
+@NotNullByDefault
+public interface StraightCollectionHolderForTest<T extends @Nullable Object,
+        INSTANCE extends StraightCollectionHolderForTest<T, INSTANCE>>
+        extends CollectionHolder<T> {
+
+    /// The amount of time a specific method (as non-aliased method) has been called
+    @Range(from = 0, to = MAX_INT_VALUE)
+    int getAmountOfCall();
+
+    /// Do an action on the current [instance][StraightCollectionHolderForTest]
+    /// and return itself afterward
+    ///
+    /// @param action The action to do
+    @Contract(ALWAYS_THIS_1)
+    @SuppressWarnings("Unchecked cast")
+    INSTANCE execute(Consumer<INSTANCE> action);
+
+}
