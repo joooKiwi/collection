0081f4f8f99bfe8e206e09e9ef87cf694b375b46
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-10-15 17:22:53 -0400

    [Java] Simple formatting

--- java/collection.joookiwi/src/test/java/instance/StraightCollectionHolderForTest.java
+++ java/collection.joookiwi/src/test/java/instance/StraightCollectionHolderForTest.java
@@ -1,12 +1,11 @@
 package instance;
 
+import java.util.function.Consumer;
 import joookiwi.collection.java.CollectionHolder;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Range;
-
-import java.util.function.Consumer;
 
 import static joookiwi.collection.java.CommonContracts.ALWAYS_THIS_1;
 import static joookiwi.collection.java.NumericConstants.MAX_INT_VALUE;
@@ -22,15 +21,13 @@ public interface StraightCollectionHolderForTest<T extends @Nullable Object,
         extends CollectionHolder<T> {
 
     /// The amount of time a specific method (as non-aliased method) has been called
-    @Range(from = 0, to = MAX_INT_VALUE)
-    int getAmountOfCall();
+    @Range(from = 0, to = MAX_INT_VALUE) int getAmountOfCall();
 
     /// Do an action on the current [instance][StraightCollectionHolderForTest]
     /// and return itself afterward
     ///
     /// @param action The action to do
     @Contract(ALWAYS_THIS_1)
-    @SuppressWarnings("Unchecked cast")
     INSTANCE execute(Consumer<INSTANCE> action);
 
 }
