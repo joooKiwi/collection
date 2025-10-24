22fae50440fdcf9085736a51591ffd046abcb7ed
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-10-23 21:31:36 -0400

    [Java] Addition of test instances about the value

---
+++ java/collection.joookiwi/src/test/java/instance/GenericCollectionHolder_GetLastAlias.java
@@ -0,0 +1,36 @@
+package instance;
+
+import java.util.function.Consumer;
+import joookiwi.collection.java.GenericCollectionHolder;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Range;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_THIS_1;
+import static joookiwi.collection.java.NumericConstants.MAX_INT_VALUE;
+import static value.Arrays.ABCD;
+
+@NotNullByDefault
+public final class GenericCollectionHolder_GetLastAlias
+        extends GenericCollectionHolder<String>
+        implements StraightCollectionHolderForTest<String, GenericCollectionHolder_GetLastAlias> {
+
+    /// The field that tell the amount of time the method `getLast` has been called
+    public @Range(from = 0, to = MAX_INT_VALUE) int amountOfCall = 0;
+
+    public GenericCollectionHolder_GetLastAlias() { super(ABCD); }
+
+    @Override public @Range(from = 0, to = MAX_INT_VALUE) int getAmountOfCall() { return amountOfCall; }
+
+    @Contract(ALWAYS_THIS_1)
+    @Override public GenericCollectionHolder_GetLastAlias execute(final Consumer<GenericCollectionHolder_GetLastAlias> action) {
+        action.accept(this);
+        return this;
+    }
+
+    @Override public String getLast() {
+        amountOfCall++;
+        return super.getLast();
+    }
+
+}
