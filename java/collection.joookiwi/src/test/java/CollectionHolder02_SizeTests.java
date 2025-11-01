9062c3cd0009817929896181e94437faae7556f5
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-11-01 00:33:58 -0400

    [Java] Update to the size tests to include the methods and null tests

--- java/collection.joookiwi/src/test/java/CollectionHolder02_SizeTests.java
+++ java/collection.joookiwi/src/test/java/CollectionHolder02_SizeTests.java
@@ -9,9 +9,12 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.Simple;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedClass;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.FieldSource;
+import test.AbstractInstancesTests;
+import test.AbstractMethodsTests;
 import value.SizeValueInstance;
 
 import static helper.JoinAsArguments.getStaticField;
@@ -19,13 +22,14 @@ import static helper.JoinAsArguments.joinAsArguments;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static value.Instances.everyCollectionInstances;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static value.Instances.everyExtensionMethodInstancesAsArguments;
+import static value.Instances.everyInstances;
 import static value.Sizes.SIZE_VALUES;
 
 @NotNullByDefault
 @DisplayNameGeneration(Simple.class)
-@DisplayName("CollectionHolder tests (size)")
-class CollectionHolder02_SizeTests {
+@DisplayName("CollectionHolder tests (size)") @TestInstance(PER_CLASS) class CollectionHolder02_SizeTests {
 
     @Nested class EmptyCollectionHolder {
         @Test void size()       { assertEquals(0, new EmptyCollectionHolderForTest<>().size()); }
@@ -41,12 +45,28 @@ class CollectionHolder02_SizeTests {
     }
 
     @FieldSource("values")
+    @ParameterizedClass(name = "{0}")/* @TestInstance(PER_CLASS)*/ @Nested class methods extends AbstractMethodsTests {
+
+        //#region -------------------- Required test configuration --------------------
+
+        static final Arguments[] values = everyExtensionMethodInstancesAsArguments;
+
+        public methods(final CollectionHolderForTest<?, ?> instance) { super(instance); }
+
+        //#endregion -------------------- Required test configuration --------------------
+
+        @Test void isEmpty() { assertTrue(getInstance().isEmpty()); }
+        @Test void isNotEmpty() { assertFalse(getInstance().isNotEmpty()); }
+
+    }
+
+    @FieldSource("values")
 //    @TestInstance(PER_CLASS)
     @ParameterizedClass(name = "{0} {1}") @Nested class instances extends AbstractInstancesTests {
 
         //#region -------------------- Required test configuration --------------------
 
-        static final List<Arguments> values = joinAsArguments(everyCollectionInstances, getStaticField("SIMPLIFIED_NAME"), SIZE_VALUES, it -> String.valueOf(it.size()));
+        static final List<Arguments> values = joinAsArguments(everyInstances, getStaticField("SIMPLIFIED_NAME"), SIZE_VALUES, it -> String.valueOf(it.size()));
 
         final Object[] array;
         final int size;
