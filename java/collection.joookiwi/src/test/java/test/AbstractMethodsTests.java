d1e0ecdc5278643a8d1a9b6ca060270938c2cdda
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-11-01 00:25:28 -0400

    [Java] Relocation of the AbstractInstanceTests to be inside an independent package with the methods included

---
+++ java/collection.joookiwi/src/test/java/test/AbstractMethodsTests.java
@@ -0,0 +1,62 @@
+package test;
+
+import instance.CollectionHolderForTest;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.exception.EmptyCollectionException;
+import joookiwi.collection.java.exception.NullCollectionException;
+import org.intellij.lang.annotations.MagicConstant;
+import org.jetbrains.annotations.Nullable;
+
+public class AbstractMethodsTests {
+
+    //#region -------------------- Fields --------------------
+
+    /// The [CollectionHolderForTest] that should be used for the tests
+    public final CollectionHolderForTest<?, ?> instance;
+    /// The reusable class that contains all the necessary elements
+    public final InstanceClassUtil util;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
+    @SuppressWarnings("unchecked cast")
+    protected AbstractMethodsTests(final CollectionHolderForTest<?, ?> instance) { util = InstanceClassUtil.get((Class<? extends CollectionHolderForTest<?, ?>>) (this.instance = instance).getClass()); }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Getter method --------------------
+
+    /// The [EmptyCollectionException] class (either [EmptyCollectionException] or [NullCollectionException])
+    public Class<? extends EmptyCollectionException> emptyExceptionClass() { return util.emptyExceptionClass(); }
+
+    /// The type of [CollectionHolderForTest] type that is used.
+    ///
+    /// If it has "extension" in it, then it directly uses the extension methods.
+    /// Otherwise, it is on an already defined instance.
+    @MagicConstant(stringValues = {"normal", "minimalist", "normal extension", "minimalist extension", "array extension", "null normal extension", "null minimalist extension", "null array extension",}) public String type() { return util.type(); }
+
+    /// Tell if the [#instanceClass] is of the type `null`
+    public boolean isNull() { return util.isNull(); }
+
+    /// Tell if the [#instanceClass] is of the type [GenericCollectionHolder][joookiwi.collection.java.GenericCollectionHolder]
+    public boolean isNormal() { return util.isNormal(); }
+
+    /// Tell if the [#instanceClass] is of the type [MinimalistCollectionHolder][joookiwi.collection.java.MinimalistCollectionHolder] directly
+    public boolean isMinimalist() { return util.isMinimalist(); }
+
+    /// Tell if the [#instanceClass] is based on the extension methods directly
+    public boolean isExtension() { return util.isExtension(); }
+
+    //#endregion -------------------- Getter method --------------------
+    //#region -------------------- Method --------------------
+
+    /// Get the [#instance] cast to the type specified
+    ///
+    /// @param <T> The instance type
+    @SuppressWarnings("unchecked cast")
+    public <T extends @Nullable Object> CollectionHolder<T> getInstance() {
+        return (CollectionHolder<T>) instance;
+    }
+
+    //#endregion -------------------- Method --------------------
+
+}
---
+++ java/collection.joookiwi/src/test/java/test/InstanceClassUtil.java
@@ -0,0 +1,118 @@
+package test;
+
+import instance.CollectionHolderForTest;
+import joookiwi.collection.java.exception.EmptyCollectionException;
+import joookiwi.collection.java.exception.NullCollectionException;
+import org.intellij.lang.annotations.MagicConstant;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.params.ParameterizedClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static helper.GetField.getStaticBooleanField;
+import static helper.GetField.getStaticStringField;
+
+/// A reusable class that is used in [AbstractInstancesTests] and [AbstractMethodsTests] construction since [ParameterizedClass]
+/// and a [per-class lifecycle][org.junit.jupiter.api.TestInstance.Lifecycle#PER_CLASS] is not supported at the moment
+/// with the reason `Constructor injection is not supported for @ParameterizedClass classes with @TestInstance(Lifecycle.PER_CLASS)`
+public final class InstanceClassUtil {
+
+    //#region -------------------- Fields --------------------
+
+    public static final Map<Class<? extends CollectionHolderForTest<?, ?>>, InstanceClassUtil> bag = new HashMap<>(8);
+
+
+    public final Class<? extends CollectionHolderForTest<?, ?>> instanceClass;
+
+    public @Nullable Class<? extends EmptyCollectionException> __emptyExceptionClass;
+
+    private @Nullable String __type;
+
+    private boolean __isNull;
+    private boolean __isNullInitialized = false;
+
+    private boolean __isNormal;
+    private boolean __isNormalInitialized = false;
+
+    private boolean __isMinimalist;
+    private boolean __isMinimalistInitialized = false;
+
+    private boolean __isExtension;
+    private boolean __isExtensionInitialized = false;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructor --------------------
+
+    private InstanceClassUtil(final Class<? extends CollectionHolderForTest<?, ?>> instanceClass) { this.instanceClass = instanceClass; }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Getter method --------------------
+
+    public Class<? extends EmptyCollectionException> emptyExceptionClass() {
+        final var value = __emptyExceptionClass;
+        if (value != null)
+            return value;
+
+        if (isNull())
+            return __emptyExceptionClass = NullCollectionException.class;
+        return __emptyExceptionClass = EmptyCollectionException.class;
+    }
+
+
+    @MagicConstant(stringValues = {"normal", "minimalist", "normal extension", "minimalist extension", "array extension", "null normal extension", "null minimalist extension", "null array extension",}) public String type() {
+        final var value = __type;
+        if (value != null)
+            return value;
+        return __type = getStaticStringField(instanceClass, "SIMPLIFIED_NAME");
+    }
+
+    public boolean isNull() {
+        if (__isNullInitialized)
+            return __isNull;
+
+        final var value = __isNull = getStaticBooleanField(instanceClass, "IS_NULL");
+        __isNullInitialized = true;
+        return value;
+    }
+
+    public boolean isNormal() {
+        if (__isNormalInitialized)
+            return __isNormal;
+
+        final var value = __isNormal = type().equals("normal");
+        __isNormalInitialized = true;
+        return value;
+    }
+
+    public boolean isMinimalist() {
+        if (__isMinimalistInitialized)
+            return __isMinimalist;
+
+        final var value = __isMinimalist = getStaticBooleanField(instanceClass, "IS_MINIMALIST");
+        __isMinimalistInitialized = true;
+        return value;
+    }
+
+    public boolean isExtension() {
+        if (__isExtensionInitialized)
+            return __isExtension;
+
+        final var value = __isExtension = getStaticBooleanField(instanceClass, "IS_EXTENSION");
+        __isExtensionInitialized = true;
+        return value;
+    }
+
+    //#endregion -------------------- Getter method --------------------
+    //#region -------------------- Method --------------------
+
+    public static InstanceClassUtil get(final Class<? extends CollectionHolderForTest<?, ?>> instanceClass) {
+        final var bag = InstanceClassUtil.bag;
+        if (!bag.containsKey(instanceClass))
+            bag.put(instanceClass, new InstanceClassUtil(instanceClass));
+        return bag.get(instanceClass);
+    }
+
+    //#endregion -------------------- Method --------------------
+
+}
