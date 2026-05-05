31dc6b05c5b92e483a13042c76d9a28207da7479
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-05-05 12:17:36 -0400

    [JavaScript] Test alignment

--- javascript/test/CollectionHolder.01 - constructor.test.ts
+++ javascript/test/CollectionHolder.01 - constructor.test.ts
@@ -20,24 +20,24 @@ describe("CollectionHolderTest (constructor)", () => {
 
     describe("GenericMinimalistCollectionHolder", () => {
     describe.each(everyIterableInstances,)("%s", ({value: iterable,},) => {
-        const newCollection = <const T, >(values: readonly T[],) => new GenericMinimalistCollectionHolder_ConstructorTest<T>(new iterable(values,),).array
+        const newCollection = <const T, >(values: readonly T[],) => new GenericMinimalistCollectionHolder_ConstructorTest(new iterable(values,),).array
 
         test("empty",    () => expect(newCollection(EMPTY,),).toBeEmpty(),)
-        test("1 field",  () => expect(newCollection(A,),).toEqual(A,),)
-        test("2 fields", () => expect(newCollection(AB,),).toEqual(AB,),)
-        test("3 fields", () => expect(newCollection(ABC,),).toEqual(ABC,),)
-        test("4 fields", () => expect(newCollection(ABCD,),).toEqual(ABCD,),)
+        test("1 field",  () => expect(newCollection(A,),)    .toEqual(A,),)
+        test("2 fields", () => expect(newCollection(AB,),)   .toEqual(AB,),)
+        test("3 fields", () => expect(newCollection(ABC,),)  .toEqual(ABC,),)
+        test("4 fields", () => expect(newCollection(ABCD,),) .toEqual(ABCD,),)
 
     },)},)
     describe("GenericCollectionHolder", () => {
     describe.each(everyIterableInstances,)("%s", ({value: iterable,},) => {
-        const newCollection = <const T, >(values: readonly T[],) => new GenericCollectionHolder_ConstructorTest<T>(new iterable(values,),)
+        const newCollection = <const T, >(values: readonly T[],) => new GenericCollectionHolder_ConstructorTest(new iterable(values,),)
 
         test("empty",    () => expect(newCollection(EMPTY,),).toBeEmpty(),)
-        test("1 field",  () => expect(newCollection(A,),).toContainAllValues(A,),)
-        test("2 fields", () => expect(newCollection(AB,),).toContainAllValues(AB,),)
-        test("3 fields", () => expect(newCollection(ABC,),).toContainAllValues(ABC,),)
-        test("4 fields", () => expect(newCollection(ABCD,),).toContainAllValues(ABCD,),)
+        test("1 field",  () => expect(newCollection(A,),)    .toContainAllValues(A,),)
+        test("2 fields", () => expect(newCollection(AB,),)   .toContainAllValues(AB,),)
+        test("3 fields", () => expect(newCollection(ABC,),)  .toContainAllValues(ABC,),)
+        test("4 fields", () => expect(newCollection(ABCD,),) .toContainAllValues(ABCD,),)
 
     },)},)
     describe("LazyCollectionHolder", () => {
@@ -45,10 +45,10 @@ describe("CollectionHolderTest (constructor)", () => {
         const newCollection = <const T, >(values: readonly T[],) => new LazyCollectionHolder_ConstructorTest(new iterable(values,),)
 
         test("empty",    () => expect(newCollection(EMPTY,),).toBeEmpty(),)
-        test("1 field",  () => expect(newCollection(A,),).toContainAllValues(A,),)
-        test("2 fields", () => expect(newCollection(AB,),).toContainAllValues(AB,),)
-        test("3 fields", () => expect(newCollection(ABC,),).toContainAllValues(ABC,),)
-        test("4 fields", () => expect(newCollection(ABCD,),).toContainAllValues(ABCD,),)
+        test("1 field",  () => expect(newCollection(A,),)    .toContainAllValues(A,),)
+        test("2 fields", () => expect(newCollection(AB,),)   .toContainAllValues(AB,),)
+        test("3 fields", () => expect(newCollection(ABC,),)  .toContainAllValues(ABC,),)
+        test("4 fields", () => expect(newCollection(ABCD,),) .toContainAllValues(ABCD,),)
 
     },)},)
 
