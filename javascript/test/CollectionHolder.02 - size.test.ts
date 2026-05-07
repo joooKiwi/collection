317d9d43abb33e18e926f8ba722c4f9cbd2024ed
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-05-07 00:46:20 -0400

    [JavaScript] Test alignement

--- javascript/test/CollectionHolder.02 - size.test.ts
+++ javascript/test/CollectionHolder.02 - size.test.ts
@@ -49,105 +49,105 @@ describe("CollectionHolderTest (size)", () => {
     describe("aliases", () => {
         describe("ArrayAsCollectionHolder", () => {
             test("length", () => expect(new ArrayAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new ArrayAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new ArrayAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("ArrayOf1AsCollectionHolder", () => {
             test("length", () => expect(new ArrayOf1AsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new ArrayOf1AsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new ArrayOf1AsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("ArrayOf2AsCollectionHolder", () => {
             test("length", () => expect(new ArrayOf2AsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new ArrayOf2AsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new ArrayOf2AsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("CollectionHolderOf1", () => {
             test("length", () => expect(new CollectionHolderOf1_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new CollectionHolderOf1_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new CollectionHolderOf1_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("CollectionHolderOf2", () => {
             test("length", () => expect(new CollectionHolderOf2_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new CollectionHolderOf2_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new CollectionHolderOf2_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("CollectionViewer", () => {
             test("length", () => expect(new CollectionViewer_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new CollectionViewer_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new CollectionViewer_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("GenericCollectionHolder", () => {
             test("length", () => expect(new GenericCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new GenericCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new GenericCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("IteratorAsCollectionHolder", () => {
             test("length", () => expect(new IteratorAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new IteratorAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new IteratorAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("JsIterableAsCollectionHolder", () => {
             test("length", () => expect(new JsIterableAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new JsIterableAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new JsIterableAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("JsIteratorAsCollectionHolder", () => {
             test("length", () => expect(new JsIteratorAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new JsIteratorAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new JsIteratorAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolder", () => {
             test("length", () => expect(new LazyCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolderOf0Or1", () => {
             test("length", () => expect(new LazyCollectionHolderOf0Or1_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolderOf0Or1_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolderOf0Or1_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolderOf0Or1Or2", () => {
             test("length", () => expect(new LazyCollectionHolderOf0Or1Or2_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolderOf0Or1Or2_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolderOf0Or1Or2_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolderOf1", () => {
             test("length", () => expect(new LazyCollectionHolderOf1_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolderOf1_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolderOf1_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolderOf1Or2", () => {
             test("length", () => expect(new LazyCollectionHolderOf1Or2_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolderOf1Or2_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolderOf1Or2_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("LazyCollectionHolderOf2", () => {
             test("length", () => expect(new LazyCollectionHolderOf2_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new LazyCollectionHolderOf2_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new LazyCollectionHolderOf2_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("MinimalistAsCollectionHolder", () => {
             test("length", () => expect(new MinimalistAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new MinimalistAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new MinimalistAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("SetAsCollectionHolder", () => {
             test("length", () => expect(new SetAsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new SetAsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new SetAsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("SetOf1AsCollectionHolder", () => {
             test("length", () => expect(new SetOf1AsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new SetOf1AsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new SetOf1AsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
         describe("SetOf2AsCollectionHolder", () => {
             test("length", () => expect(new SetOf2AsCollectionHolder_SizeAlias().execute(it => it.length,).amountOfCall,).toBe(1,),)
-            test("count",  () => expect(new SetOf2AsCollectionHolder_SizeAlias().execute(it => it.count,).amountOfCall,).toBe(1,),)
+            test("count",  () => expect(new SetOf2AsCollectionHolder_SizeAlias().execute(it => it.count,) .amountOfCall,).toBe(1,),)
         },)
     },)
 
     describe("methods", () => {
     describe.each(everyExtensionMethodInstances,)("%s", ({value: {instance,},},) => {
-        test("isEmpty", () => expect(instance.isEmpty,).toBeTrue(),)
+        test("isEmpty",    () => expect(instance.isEmpty,)   .toBeTrue(),)
         test("isNotEmpty", () => expect(instance.isNotEmpty,).toBeFalse(),)
     },)},)
 
     describe("instances", () => {
         describe("empty", () => {
             describe.each(every0Instances,)("%s", ({value: {newInstance,},},) => {
-                test("size", () => expect(newInstance().size,).toBe(0,),)
-                test("isEmpty", () => expect(newInstance().isEmpty,).toBeTrue(),)
+                test("size",       () => expect(newInstance().size,)      .toBe(0,),)
+                test("isEmpty",    () => expect(newInstance().isEmpty,)   .toBeTrue(),)
                 test("isNotEmpty", () => expect(newInstance().isNotEmpty,).toBeFalse(),)
             },)
         },)
         describe("1 field", () => {
             const value = Symbol("1 field: value",)
             describe.each(every1Instances,)("%s", ({value: {newInstance,},},) => {
-                test("size", () => expect(newInstance(value,).size,).toBe(1,),)
-                test("isEmpty", () => expect(newInstance(value,).isEmpty,).toBeFalse(),)
+                test("size",       () => expect(newInstance(value,).size,)      .toBe(1,),)
+                test("isEmpty",    () => expect(newInstance(value,).isEmpty,)   .toBeFalse(),)
                 test("isNotEmpty", () => expect(newInstance(value,).isNotEmpty,).toBeTrue(),)
             },)
         },)
@@ -155,8 +155,8 @@ describe("CollectionHolderTest (size)", () => {
             const value2 = Symbol("2 fields: 1st value",)
             const value1 = Symbol("2 fields: 2nd value",)
             describe.each(every2Instances,)("%s", ({value: {newInstance,},},) => {
-                test("size", () => expect(newInstance(value1, value2,).size,).toBe(2,),)
-                test("isEmpty", () => expect(newInstance(value1, value2,).isEmpty,).toBeFalse(),)
+                test("size",       () => expect(newInstance(value1, value2,).size,)      .toBe(2,),)
+                test("isEmpty",    () => expect(newInstance(value1, value2,).isEmpty,)   .toBeFalse(),)
                 test("isNotEmpty", () => expect(newInstance(value1, value2,).isNotEmpty,).toBeTrue(),)
             },)
         },)
@@ -168,8 +168,8 @@ describe("CollectionHolderTest (size)", () => {
                 Symbol("4 fields: 4th value",),
             ]
             describe.each(everyNInstances,)("%s", ({value: {instance,},},) => {
-                test("size", () => expect(new instance(values,).size,).toBe(4,),)
-                test("isEmpty", () => expect(new instance(values,).isEmpty,).toBeFalse(),)
+                test("size",       () => expect(new instance(values,).size,)      .toBe(4,),)
+                test("isEmpty",    () => expect(new instance(values,).isEmpty,)   .toBeFalse(),)
                 test("isNotEmpty", () => expect(new instance(values,).isNotEmpty,).toBeTrue(),)
             },)
         },)
