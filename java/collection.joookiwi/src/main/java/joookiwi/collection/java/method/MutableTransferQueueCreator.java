356e3895e51eb6cab7257c77fcfcbbaacebe19df
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-07-29 15:56:18 -0400

    [Java] Addition of immutable|mutable transferDeque creator

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/MutableTransferQueueCreator.java
@@ -0,0 +1,47 @@
+package joookiwi.collection.java.method;
+
+import java.util.Collection;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsMutableTransferQueue;
+import joookiwi.collection.java.extended.MutableTransferQueue;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class MutableTransferQueueCreator
+        extends CreatorUtility {
+
+    @Contract(ALWAYS_FAIL_0)
+    private MutableTransferQueueCreator() { throw new ImpossibleConstructionException("The utility class “MutableTransferQueueCreator” cannot be constructed.", MutableTransferQueueCreator.class); }
+
+    //#region -------------------- ∅ --------------------
+
+    public static <T> MutableTransferQueue<T> MutableTransferQueue() { return new ArrayAsMutableTransferQueue<>(); }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- values --------------------
+
+    public static <T> MutableTransferQueue<T> MutableTransferQueue(final @Nullable MinimalistCollectionHolder<? extends T> values) { return ToMutableTransferQueue.toMutableTransferQueue(values); }
+
+    public static <T> MutableTransferQueue<T> MutableTransferQueue(final @Nullable CollectionHolder<? extends T> values) { return ToMutableTransferQueue.toMutableTransferQueue(values); }
+
+    public static <T> MutableTransferQueue<T> MutableTransferQueue(final T @Nullable @Unmodifiable [] values) { return ToMutableTransferQueue.toMutableTransferQueue(values); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> MutableTransferQueue<T> MutableTransferQueue(final @Nullable @Unmodifiable Collection<? extends T> values) {
+        if (values == null)
+            return new ArrayAsMutableTransferQueue<>();
+        if (values.isEmpty())
+            return new ArrayAsMutableTransferQueue<>();
+        return new ArrayAsMutableTransferQueue<>((T[]) values.toArray());
+    }
+
+    //#endregion -------------------- values --------------------
+
+}
---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/TransferQueueCreator.java
@@ -0,0 +1,50 @@
+package joookiwi.collection.java.method;
+
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsImmutableTransferQueue;
+import joookiwi.collection.java.extended.EmptyTransferQueue;
+import joookiwi.collection.java.extended.ImmutableTransferQueue;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Collection;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class TransferQueueCreator
+        extends CreatorUtility {
+
+    @Contract(ALWAYS_FAIL_0)
+    private TransferQueueCreator() { throw new ImpossibleConstructionException("The utility class “TransferQueueCreator” cannot be constructed.", TransferQueueCreator.class); }
+
+    //#region -------------------- ∅ --------------------
+
+    /// An [EmptyTransferQueue] instance
+    public static <T> EmptyTransferQueue<T> TransferQueue() { return EmptyTransferQueue.getInstance(); }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- values --------------------
+
+    public static <T> ImmutableTransferQueue<T> TransferQueue(final @Nullable MinimalistCollectionHolder<? extends T> values) { return ToTransferQueue.toTransferQueue(values); }
+
+    public static <T> ImmutableTransferQueue<T> TransferQueue(final @Nullable CollectionHolder<? extends T> values) { return ToTransferQueue.toTransferQueue(values); }
+
+    public static <T> ImmutableTransferQueue<T> TransferQueue(final T @Nullable @Unmodifiable [] values) { return ToTransferQueue.toTransferQueue(values); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> ImmutableTransferQueue<T> TransferQueue(final @Nullable @Unmodifiable Collection<? extends T> values) {
+        if (values == null)
+            return EmptyTransferQueue.getInstance();
+        if (values.isEmpty())
+            return EmptyTransferQueue.getInstance();
+        return new ArrayAsImmutableTransferQueue<>((T[]) values.toArray());
+    }
+
+    //#endregion -------------------- values --------------------
+
+}
