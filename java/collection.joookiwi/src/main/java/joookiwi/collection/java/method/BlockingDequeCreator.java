c6b97d0668070d5f835b8ab23efdbce2ac2fe8d6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-07-29 15:54:26 -0400

    [Java] Addition of immutable|mutable blockingDeque creator

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/BlockingDequeCreator.java
@@ -0,0 +1,49 @@
+package joookiwi.collection.java.method;
+
+import java.util.Collection;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsImmutableBlockingDeque;
+import joookiwi.collection.java.extended.EmptyBlockingDeque;
+import joookiwi.collection.java.extended.ImmutableBlockingDeque;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class BlockingDequeCreator
+        extends CreatorUtility {
+
+    @Contract(ALWAYS_FAIL_0)
+    private BlockingDequeCreator() { throw new ImpossibleConstructionException("The utility class “BlockingDequeCreator” cannot be constructed.", BlockingDequeCreator.class); }
+
+    //#region -------------------- ∅ --------------------
+
+    /// An [EmptyBlockingDeque] instance
+    public static <T> EmptyBlockingDeque<T> BlockingDeque() { return EmptyBlockingDeque.getInstance(); }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- values --------------------
+
+    public static <T> ImmutableBlockingDeque<T> BlockingDeque(final @Nullable MinimalistCollectionHolder<? extends T> values) { return ToBlockingDeque.toBlockingDeque(values); }
+
+    public static <T> ImmutableBlockingDeque<T> BlockingDeque(final @Nullable CollectionHolder<? extends T> values) { return ToBlockingDeque.toBlockingDeque(values); }
+
+    public static <T> ImmutableBlockingDeque<T> BlockingDeque(final T @Nullable @Unmodifiable [] values) { return ToBlockingDeque.toBlockingDeque(values); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> ImmutableBlockingDeque<T> BlockingDeque(final @Nullable @Unmodifiable Collection<? extends T> values) {
+        if (values == null)
+            return EmptyBlockingDeque.getInstance();
+        if (values.isEmpty())
+            return EmptyBlockingDeque.getInstance();
+        return new ArrayAsImmutableBlockingDeque<>((T[]) values.toArray());
+    }
+
+    //#endregion -------------------- values --------------------
+
+}
---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/method/MutableBlockingDequeCreator.java
@@ -0,0 +1,47 @@
+package joookiwi.collection.java.method;
+
+import java.util.Collection;
+import joookiwi.collection.java.CollectionHolder;
+import joookiwi.collection.java.MinimalistCollectionHolder;
+import joookiwi.collection.java.exception.ImpossibleConstructionException;
+import joookiwi.collection.java.extended.ArrayAsMutableBlockingDeque;
+import joookiwi.collection.java.extended.MutableBlockingDeque;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_FAIL_0;
+
+@NotNullByDefault
+public final class MutableBlockingDequeCreator
+        extends CreatorUtility {
+
+    @Contract(ALWAYS_FAIL_0)
+    private MutableBlockingDequeCreator() { throw new ImpossibleConstructionException("The utility class “MutableBlockingDequeCreator” cannot be constructed.", MutableBlockingDequeCreator.class); }
+
+    //#region -------------------- ∅ --------------------
+
+    public static <T> MutableBlockingDeque<T> MutableBlockingDeque() { return new ArrayAsMutableBlockingDeque<>(); }
+
+    //#endregion -------------------- ∅ --------------------
+    //#region -------------------- values --------------------
+
+    public static <T> MutableBlockingDeque<T> MutableBlockingDeque(final @Nullable MinimalistCollectionHolder<? extends T> values) { return ToMutableBlockingDeque.toMutableBlockingDeque(values); }
+
+    public static <T> MutableBlockingDeque<T> MutableBlockingDeque(final @Nullable CollectionHolder<? extends T> values) { return ToMutableBlockingDeque.toMutableBlockingDeque(values); }
+
+    public static <T> MutableBlockingDeque<T> MutableBlockingDeque(final T @Nullable @Unmodifiable [] values) { return ToMutableBlockingDeque.toMutableBlockingDeque(values); }
+
+    @SuppressWarnings("unchecked cast")
+    public static <T> MutableBlockingDeque<T> MutableBlockingDeque(final @Nullable @Unmodifiable Collection<? extends T> values) {
+        if (values == null)
+            return new ArrayAsMutableBlockingDeque<>();
+        if (values.isEmpty())
+            return new ArrayAsMutableBlockingDeque<>();
+        return new ArrayAsMutableBlockingDeque<>((T[]) values.toArray());
+    }
+
+    //#endregion -------------------- values --------------------
+
+}
