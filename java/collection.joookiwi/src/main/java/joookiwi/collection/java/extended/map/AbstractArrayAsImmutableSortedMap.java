2e7b6d90d66b4154230fe997b9379c32202d9fb2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-07-30 23:03:07 -0400

    [Java] Addition of immutable SortedMap instances

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/map/AbstractArrayAsImmutableSortedMap.java
@@ -0,0 +1,66 @@
+package joookiwi.collection.java.extended.map;
+
+import joookiwi.collection.java.extended.ImmutableSortedCollection;
+import joookiwi.collection.java.extended.UtilityForMap;
+import joookiwi.collection.java.extended.set.ImmutableSortedSet;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+import static joookiwi.collection.java.CommonContracts.ALWAYS_NEW_0;
+
+@NotNullByDefault
+public abstract class AbstractArrayAsImmutableSortedMap<K extends @Nullable Object,
+        V extends @Nullable Object>
+        extends AbstractArrayAsImmutableSequencedMap<K, V>
+        implements ImmutableSortedMap<K, V> {
+
+    //#region -------------------- Constructor --------------------
+
+    protected AbstractArrayAsImmutableSortedMap() { super(); }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Methods --------------------
+
+    //#region -------------------- Get methods --------------------
+
+    @Override public K firstKey() { return UtilityForMap.getFirstKey(_reference()); }
+
+    @Override public K lastKey() { return UtilityForMap.getLastKey(_reference()); }
+
+    //#endregion -------------------- Get methods --------------------
+    //#region -------------------- As subdivided methods --------------------
+
+    @Override public ImmutableSortedMap<K, V> subMap(final K from, final K to) { return UtilityForMap.asSubdivided(this, _reference(), from, to); }
+
+    @Override public ImmutableSortedMap<K, V> tailMap(final K from) { return UtilityForMap.asTailSubdivided(this, _reference(), from); }
+
+    @Override public ImmutableSortedMap<K, V> headMap(final K to) { return UtilityForMap.asHeadSubdivided(this, _reference(), to); }
+
+    //#endregion -------------------- As subdivided methods --------------------
+    //#region -------------------- As reverse methods --------------------
+
+    @Override public ImmutableSortedMap<K, V> reversed() { return UtilityForMap.asReversed(this, _reference()); }
+
+    //#endregion -------------------- As reverse methods --------------------
+    //#region -------------------- Values methods --------------------
+
+    @Override public ImmutableSortedSet<Entry<K, V>> entrySet() { return UtilityForMap.entries3(_reference()); }
+
+    @Override public ImmutableSortedSet<K> keySet() { return UtilityForMap.keys3(_reference()); }
+
+    @Override public ImmutableSortedCollection<V> values() { return UtilityForMap.values3(_reference()); }
+
+    //#endregion -------------------- Values methods --------------------
+    //#region -------------------- Clone methods --------------------
+
+    @MustBeInvokedByOverriders
+    @Contract(ALWAYS_NEW_0)
+    @Override public AbstractArrayAsImmutableSortedMap<K, V> clone() { return (AbstractArrayAsImmutableSortedMap<K, V>) super.clone(); }
+
+    //#endregion -------------------- Clone methods --------------------
+
+    //#endregion -------------------- Methods --------------------
+
+}
