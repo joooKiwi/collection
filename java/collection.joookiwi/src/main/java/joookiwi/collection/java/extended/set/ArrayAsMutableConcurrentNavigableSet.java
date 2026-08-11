da96e16faa414aa55831d160d9fb9935843ceb15
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-08-11 19:54:30 -0400

    [Java] Addition of the arrays as the extended collections

---
+++ java/collection.joookiwi/src/main/java/joookiwi/collection/java/extended/set/ArrayAsMutableConcurrentNavigableSet.java
@@ -0,0 +1,111 @@
+package joookiwi.collection.java.extended.set;
+
+import joookiwi.collection.java.helper.ComparatorHelper;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Comparator;
+
+import static joookiwi.collection.java.CollectionConstants.emptyArray;
+import static joookiwi.collection.java.CommonContracts.ALWAYS_NEW_0;
+
+/// A bare-bone implementation of a [MutableConcurrentNavigableSet]
+/// with the mutability in place.
+///
+/// Note that `null` is permitted in this instance.
+/// It is up to the implementor to specify it.
+///
+/// The instance uses the [methods][joookiwi.collection.java.method] and [ComparatorHelper]
+/// to give similar implementation to the [joookiwi.collection.java.CollectionHolder]
+/// when possible.
+///
+/// @param <T> The type
+/// @see SubdividedArrayAsMutableConcurrentNavigableSet
+/// @see ReversedArrayAsMutableConcurrentNavigableSet
+@NotNullByDefault
+public class ArrayAsMutableConcurrentNavigableSet<T extends @Nullable Object>
+        extends AbstractArrayAsMutableConcurrentNavigableSet<T> {
+
+    //#region -------------------- Fields --------------------
+
+    private T[] __reference;
+    private final @Nullable Comparator<? super T> __comparator;
+
+    //#endregion -------------------- Fields --------------------
+    //#region -------------------- Constructors --------------------
+
+    /// Create an instance of a [MutableConcurrentNavigableSet] from an [empty array][joookiwi.collection.java.CollectionConstants#emptyArray]
+    public ArrayAsMutableConcurrentNavigableSet() {
+        super();
+        __reference = emptyArray();
+        __comparator = null;
+    }
+
+    /// Create an instance of a [MutableConcurrentNavigableSet] from the `reference`
+    /// using a `comparator` to compare its values
+    ///
+    /// @param comparator The [Comparator] to use on its comparisons
+    /// @throws ClassCastException If the `comparator` was `null`, at least one value was not [Comparable]
+    /// @implNote If the `comparator` is `null`, then use a [Comparable] type on [T] to avoid [ClassCastException]
+    public ArrayAsMutableConcurrentNavigableSet(final @Nullable Comparator<? super T> comparator) {
+        super();
+        __reference = emptyArray();
+        __comparator = comparator;
+    }
+
+    /// Create an instance of a [MutableConcurrentNavigableSet] from the `reference`
+    /// using its natural ordering to compare its values
+    ///
+    /// @param reference The array to be the internal structure
+    /// @throws RuntimeException   A duplicate value exists in the `reference`
+    /// @throws RuntimeException   A value was not ordered in the `reference`
+    /// @throws ClassCastException At least one value was not [Comparable]
+    /// @implNote Use a [Comparable] type on [T] to avoid [ClassCastException]
+    public ArrayAsMutableConcurrentNavigableSet(final T[] reference) {
+        super();
+        _validateValues(reference);
+        __reference = reference;
+        __comparator = null;
+    }
+
+    /// Create an instance of a [MutableConcurrentNavigableSet] from the `reference`
+    /// using a `comparator` to compare its values
+    ///
+    /// @param reference  The array to be the internal structure
+    /// @param comparator The [Comparator] to use on its comparisons
+    /// @throws RuntimeException   A duplicate value exists in the `reference`
+    /// @throws RuntimeException   A value was not ordered in the `reference`
+    /// @throws ClassCastException If the `comparator` was `null`, at least one value was not [Comparable]
+    /// @implNote If the `comparator` is `null`, then use a [Comparable] type on [T] to avoid [ClassCastException]
+    public ArrayAsMutableConcurrentNavigableSet(final T[] reference, final @Nullable Comparator<? super T> comparator) {
+        super();
+        _validateValues(reference, comparator);
+        __reference = reference;
+        __comparator = comparator;
+    }
+
+    //#endregion -------------------- Constructor --------------------
+    //#region -------------------- Getter methods --------------------
+
+    /// The internal reference passed through the constructor
+    @Contract(pure = true)
+    @Override protected T[] _reference() { return __reference; }
+
+    @Contract(mutates = "this")
+    @Override protected void _reference(final T[] value) { __reference = value; }
+
+    @Contract(pure = true)
+    @Override public @Nullable Comparator<? super T> comparator() { return __comparator; }
+
+    //#endregion -------------------- Getter methods --------------------
+    //#region -------------------- Methods --------------------
+
+    @MustBeInvokedByOverriders
+    @Contract(ALWAYS_NEW_0)
+    @Override public ArrayAsMutableConcurrentNavigableSet<T> clone() { return (ArrayAsMutableConcurrentNavigableSet<T>) super.clone(); }
+
+    //#endregion -------------------- Methods --------------------
+
+}
